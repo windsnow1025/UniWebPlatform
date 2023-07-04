f85f5772edf6345ae5b3df597e68baae0265a537
windsnow1025 <windsnow1025@gmail.com>
2023-07-04 05:41:06 +0000

    bookmarks updates

--- node/public/js/bookmarks.js
+++ node/public/js/bookmarks.js
@@ -9,47 +9,26 @@ console.log(isAdmin ? "Admin" : "User");
 // Fetch bookmarks from the server
 axios.get('/api/bookmarks-api/')
     .then(res => {
-        // TODO: Display bookmarks
-        console.log(res.data);
+        // Display bookmarks
+        displayBookmarks(res.data);
     })
 
-function RowShow(NumberInRow) {
-    //Count the amount of divs in row
-    var RowDocument = document.getElementById("Row");
-    var RowTotal = RowDocument.getElementsByTagName('div').length;
-    for (let i = 1; i <= RowTotal; i++) {
-        var ChangeDivID = "Column" + i.toString();
-        var ChangeDivDocument = document.getElementById(ChangeDivID);
-        if (i != NumberInRow) {
-            //Hide other divs in column
-            ChangeDivDocument.style.display = "none";
-            //Hide other divs in main
-            ColumnShow(i, 0);
-        }
-        else {
-            //Show the current div in column
-            ChangeDivDocument.style.display = "block";
-            //Show the first div in main corresponding to the current div in column
-            ColumnShow(i, 1);
-        }
-    }
-}
+function displayBookmarks(bookmarks) {
+    const column1 = document.getElementById("Column1");
+    const column2 = document.getElementById("Column2");
+    const column3 = document.getElementById("Column3");
 
-function ColumnShow(Row, NumberInColumn) {
-    //Count the amount of divs in column
-    ColumnDocument = document.getElementById("Column" + Row.toString());
-    ColumnTotal = ColumnDocument.getElementsByTagName('div').length;
-    for (let i = 1; i <= ColumnTotal; i++) {
-        var ChangeDivID = "Main" + Row.toString() + "-" + i.toString();
-        var ChangeDivDocument = document.getElementById(ChangeDivID);
-        if (i != NumberInColumn) {
-            //Hide divs in main
-            ChangeDivDocument.style.display = "none";
-        }
-        else {
-            //Show the current div in main
-            ChangeDivDocument.style.display = "block";
-        }
-    }
-}
+    bookmarks.forEach((bookmark, index) => {
+        const div = document.createElement('div');
+        div.textContent = `${bookmark.firstTitle} - ${bookmark.secondTitle}`;
+        div.onclick = () => window.open(bookmark.url, '_blank');
 
+        if (index % 3 === 0) {
+            column1.appendChild(div);
+        } else if (index % 3 === 1) {
+            column2.appendChild(div);
+        } else {
+            column3.appendChild(div);
+        }
+    });
+}
