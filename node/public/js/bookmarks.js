eb321c38aafe8a195668d504e916837b3b220542
windsnow1025 <windsnow1025@gmail.com>
2023-07-07 06:33:14 +0000

    clear

--- node/public/js/bookmarks.js
+++ node/public/js/bookmarks.js
@@ -14,21 +14,5 @@ axios.get('/api/bookmarks-api/')
     })
 
 function displayBookmarks(bookmarks) {
-    const column1 = document.getElementById("Column1");
-    const column2 = document.getElementById("Column2");
-    const column3 = document.getElementById("Column3");
 
-    bookmarks.forEach((bookmark, index) => {
-        const div = document.createElement('div');
-        div.textContent = `${bookmark.firstTitle} - ${bookmark.secondTitle}`;
-        div.onclick = () => window.open(bookmark.url, '_blank');
-
-        if (index % 3 === 0) {
-            column1.appendChild(div);
-        } else if (index % 3 === 1) {
-            column2.appendChild(div);
-        } else {
-            column3.appendChild(div);
-        }
-    });
 }
