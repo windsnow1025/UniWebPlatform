95277de356f3e3632b797b4c75ba85ac181af586
windsnow1025 <windsnow1025@gmail.com>
2023-07-09 14:35:05 +0000

    Show on load. Add form

--- node/public/js/bookmarks.js
+++ node/public/js/bookmarks.js
@@ -1,8 +1,9 @@
-import { getUsername } from "./auth";
+import {getUsername} from "./auth";
 import axios from 'axios';
 
 // Theme
-import { initializeTheme } from './theme.js';
+import {initializeTheme} from './theme';
+
 initializeTheme();
 
 let username = getUsername();
@@ -14,10 +15,29 @@ function displayBookmarks(bookmarks) {
     const container = document.querySelector('div');
     bookmarks.forEach(bookmark => {
         const div = document.createElement('div');
-        div.textContent = `${bookmark.firstTitle} - ${bookmark.secondTitle} - ${bookmark.url} - ${bookmark.comment}`;
+        div.textContent = `${bookmark.first_title} - ${bookmark.second_title} - ${bookmark.url} - ${bookmark.comment}`;
+
+        const editButton = document.createElement('button');
+        editButton.textContent = 'Edit';
+        editButton.addEventListener('click', () => {
+            // code to edit this bookmark
+        });
+
+        const deleteButton = document.createElement('button');
+        deleteButton.textContent = 'Delete';
+        deleteButton.addEventListener('click', () => {
+            deleteBookmark(bookmark.id).then(() => {
+                // reload bookmarks
+                axios.get('/api/bookmark-api/').then(res => displayBookmarks(res.data));
+            });
+        });
+
+        div.appendChild(editButton);
+        div.appendChild(deleteButton);
         container.appendChild(div);
     });
 }
+
 
 function addBookmark(bookmark) {
     return axios.post('/api/bookmark-api/', bookmark);
@@ -31,7 +51,8 @@ function deleteBookmark(id) {
     return axios.delete(`/api/bookmark-api/${id}`);
 }
 
-document.querySelector('#addBookmarkForm').addEventListener('submit', function(event) {
+
+document.querySelector('#addBookmarkForm').addEventListener('submit', function (event) {
     event.preventDefault();
     const form = event.target;
     const bookmark = {
@@ -43,6 +64,19 @@ document.querySelector('#addBookmarkForm').addEventListener('submit', function(e
     addBookmark(bookmark).then(() => {
         form.reset();
         // reload bookmarks
-        axios.get('/api/bookmarks-api/').then(res => displayBookmarks(res.data));
+        axios.get('/api/bookmark-api/').then(res => displayBookmarks(res.data));
+    }).catch(error => {
+        console.error('Error adding bookmark:', error);
     });
 });
+
+
+window.onload = function () {
+    // Fetch bookmarks from the server on window load
+    axios.get('/api/bookmark-api/').then(res => {
+        // Display bookmarks
+        displayBookmarks(res.data);
+    }).catch(error => {
+        console.error('Error fetching bookmarks:', error);
+    });
+};
