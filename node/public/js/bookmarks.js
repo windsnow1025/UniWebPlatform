e50943cfc3aa8b0c7ad93312fe2eb862864dd0d8
windsnow1025 <windsnow1025@gmail.com>
2023-07-10 02:13:34 +0000

    Bug Fixes for bookmarks

--- node/public/js/bookmarks.js
+++ node/public/js/bookmarks.js
@@ -7,62 +7,64 @@ if (username == "windsnow1025@gmail.com") {
     console.log("User");
 }
 
-function RowShow(NumberInRow) {
-    //Count the amount of divs in row
-    var RowDocument = document.getElementById("Row");
-    var RowTotal = RowDocument.getElementsByTagName('div').length;
-    for (let i = 1; i <= RowTotal; i++) {
-        var ChangeDivID = "Column" + i.toString();
-        var ChangeDivDocument = document.getElementById(ChangeDivID);
-        if (i != NumberInRow) {
-            //Hide other divs in column
-            ChangeDivDocument.style.display = "none";
-            //Hide other divs in main
-            ColumnShow(i, 0);
+function displayRow(rowIndex) {
+    // Count the amount of divs in row
+    const rowDiv = document.getElementById("Row");
+    const rowNumber = rowDiv.getElementsByTagName('div').length;
+    for (let i = 1; i <= rowNumber; i++) {
+        var currentDivId = "Column" + i.toString();
+        var currentDiv = document.getElementById(currentDivId);
+        if (i != rowIndex) {
+            // Hide div in column
+            currentDiv.style.display = "none";
+            // Hide div in main
+            displayColumn(i, 0);
         } else {
-            //Show the current div in column
-            ChangeDivDocument.style.display = "block";
-            //Show the first div in main corresponding to the current div in column
-            ColumnShow(i, 1);
+            // Show div in column
+            currentDiv.style.display = "block";
+            // Show the first div in main corresponding to the current div in column
+            displayColumn(i, 1);
         }
     }
 }
 
-function ColumnShow(Row, NumberInColumn) {
-    //Count the amount of divs in column
-    var ColumnDocument = document.getElementById("Column" + Row.toString());
-    var ColumnTotal = ColumnDocument.getElementsByTagName('div').length;
-    for (let i = 1; i <= ColumnTotal; i++) {
-        var ChangeDivID = "Main" + Row.toString() + "-" + i.toString();
-        var ChangeDivDocument = document.getElementById(ChangeDivID);
-        if (i != NumberInColumn) {
-            //Hide divs in main
-            ChangeDivDocument.style.display = "none";
+function displayColumn(rowIndex, columnIndex) {
+    // Count the amount of divs in column
+    const columnDiv = document.getElementById("Column" + rowIndex.toString());
+    const columnNumber = columnDiv.getElementsByTagName('div').length;
+    for (let i = 1; i <= columnNumber; i++) {
+        let currentDivId = "Main" + rowIndex.toString() + "-" + i.toString();
+        var currentDiv = document.getElementById(currentDivId);
+        if (i != columnIndex) {
+            // Hide in main
+            currentDiv.style.display = "none";
         } else {
-            //Show the current div in main
-            ChangeDivDocument.style.display = "block";
+            // Show in main
+            currentDiv.style.display = "block";
         }
     }
 }
 
 // Add event listeners for Row
-var RowDocument = document.getElementById("Row");
-var divs = RowDocument.getElementsByTagName('div');
+let rowDiv = document.getElementById("Row");
+let divs = rowDiv.getElementsByTagName('div');
 for (let i = 0; i < divs.length; i++) {
     divs[i].onclick = function () {
-        RowShow(i + 1);
+        displayRow(i + 1);
     }
 }
 
 // Add event listeners for Column
-var item2 = document.getElementsByClassName("item2")[0];
-var columns = item2.getElementsByTagName('div');
+let item2 = document.getElementsByClassName("item2")[0];
+let columns = item2.getElementsByTagName('div');
 for (let i = 0; i < columns.length; i++) {
     if (columns[i].id.includes("Column")) {
-        var divs = columns[i].getElementsByTagName('div');
+        let divs = columns[i].getElementsByTagName('div');
         for (let j = 0; j < divs.length; j++) {
             divs[j].onclick = function () {
-                ColumnShow(i + 1, j + 1);
+                let currentRowIndex = Math.floor(i / rowDiv.getElementsByTagName('div').length) + 1;
+                let currentColumnIndex = i - (currentRowIndex - 1) * rowDiv.getElementsByTagName('div').length + 1;
+                displayColumn(currentColumnIndex, j + 1);
             }
         }
     }
