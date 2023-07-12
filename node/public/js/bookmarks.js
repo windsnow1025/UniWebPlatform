e5d417e605e07f0a8dddbd4bd56a8d5f01bc07a3
windsnow1025 <windsnow1025@gmail.com>
2023-07-12 01:12:31 +0000

    feat: bookmarks
    Class

--- node/public/js/bookmarks.js
+++ node/public/js/bookmarks.js
@@ -12,112 +12,121 @@ import {initializeTheme} from './theme';
 
 initializeTheme();
 
-function displayBookmarks(bookmarks) {
-    const tableBody = document.querySelector('#bookmarksTable tbody');
-    // Remove all rows except the first one (the form)
-    while (tableBody.children.length > 1) {
-        tableBody.removeChild(tableBody.lastChild);
+class Bookmarks {
+    constructor() {
+        this.bookmarks = [];
     }
-    bookmarks.forEach(bookmark => {
-        const tr = document.createElement('tr');
 
-        // Create table data for each attribute
-        const firstTitleTd = document.createElement('td');
-        firstTitleTd.textContent = bookmark.first_title;
-        tr.appendChild(firstTitleTd);
+    displayBookmarks(bookmarks) {
+        const tableBody = document.querySelector('#bookmarksTable tbody');
+        // Remove all rows except the first one (the form)
+        while (tableBody.children.length > 1) {
+            tableBody.removeChild(tableBody.lastChild);
+        }
+        bookmarks.forEach(bookmark => {
+            const tr = document.createElement('tr');
 
-        const secondTitleTd = document.createElement('td');
-        secondTitleTd.textContent = bookmark.second_title;
-        tr.appendChild(secondTitleTd);
+            // Create table data for each attribute
+            const firstTitleTd = document.createElement('td');
+            firstTitleTd.textContent = bookmark.first_title;
+            tr.appendChild(firstTitleTd);
 
-        const urlTd = document.createElement('td');
-        urlTd.textContent = bookmark.url;
-        tr.appendChild(urlTd);
+            const secondTitleTd = document.createElement('td');
+            secondTitleTd.textContent = bookmark.second_title;
+            tr.appendChild(secondTitleTd);
 
-        const commentTd = document.createElement('td');
-        commentTd.textContent = bookmark.comment;
-        tr.appendChild(commentTd);
+            const urlTd = document.createElement('td');
+            urlTd.textContent = bookmark.url;
+            tr.appendChild(urlTd);
 
-        const urlCommentTd = document.createElement('td');
-        const a = document.createElement('a');
-        a.href = bookmark.url;
-        a.textContent = bookmark.comment;
-        urlCommentTd.appendChild(a);
-        tr.appendChild(urlCommentTd);
+            const commentTd = document.createElement('td');
+            commentTd.textContent = bookmark.comment;
+            tr.appendChild(commentTd);
 
-        const buttonTd = document.createElement('td');
+            const urlCommentTd = document.createElement('td');
+            const a = document.createElement('a');
+            a.href = bookmark.url;
+            a.textContent = bookmark.comment;
+            urlCommentTd.appendChild(a);
+            tr.appendChild(urlCommentTd);
 
-        const editButton = document.createElement('button');
-        editButton.textContent = 'Edit';
-        editButton.addEventListener('click', () => {
-            if (!isAdmin) {
-                alert('Only admins can edit bookmarks.');
-                return;
-            }
-            if (editButton.textContent === 'Edit') {
-                // Make the fields editable
-                firstTitleTd.contentEditable = true;
-                secondTitleTd.contentEditable = true;
-                urlTd.contentEditable = true;
-                commentTd.contentEditable = true;
+            const buttonTd = document.createElement('td');
 
-                // Change the button text to 'Submit'
-                editButton.textContent = 'Submit';
-            } else {
-                // Make the fields non-editable
-                firstTitleTd.contentEditable = false;
-                secondTitleTd.contentEditable = false;
-                urlTd.contentEditable = false;
-                commentTd.contentEditable = false;
+            const editButton = document.createElement('button');
+            editButton.textContent = 'Edit';
+            editButton.addEventListener('click', () => {
+                if (!isAdmin) {
+                    alert('Only admins can edit bookmarks.');
+                    return;
+                }
+                if (editButton.textContent === 'Edit') {
+                    // Make the fields editable
+                    firstTitleTd.contentEditable = "plaintext-only";
+                    secondTitleTd.contentEditable = "plaintext-only";
+                    urlTd.contentEditable = "plaintext-only";
+                    commentTd.contentEditable = "plaintext-only";
 
-                // Change the button text back to 'Edit'
-                editButton.textContent = 'Edit';
+                    // Change the button text to 'Submit'
+                    editButton.textContent = 'Submit';
+                } else {
+                    // Make the fields non-editable
+                    firstTitleTd.contentEditable = "false";
+                    secondTitleTd.contentEditable = "false";
+                    urlTd.contentEditable = "false";
+                    commentTd.contentEditable = "false";
 
-                // Update the bookmark
-                const updatedBookmark = {
-                    firstTitle: firstTitleTd.textContent,
-                    secondTitle: secondTitleTd.textContent,
-                    url: urlTd.textContent,
-                    comment: commentTd.textContent
-                };
-                editBookmark(bookmark.id, updatedBookmark).then(() => {
+                    // Change the button text back to 'Edit'
+                    editButton.textContent = 'Edit';
+
+                    // Update the bookmark
+                    const updatedBookmark = {
+                        firstTitle: firstTitleTd.textContent,
+                        secondTitle: secondTitleTd.textContent,
+                        url: urlTd.textContent,
+                        comment: commentTd.textContent
+                    };
+                    this.editBookmark(bookmark.id, updatedBookmark).then(() => {
+                        // reload bookmarks
+                        axios.get('/api/bookmark-api/').then(res => this.displayBookmarks(res.data));
+                    });
+                }
+            });
+            buttonTd.appendChild(editButton);
+
+            const deleteButton = document.createElement('button');
+            deleteButton.textContent = 'Delete';
+            deleteButton.addEventListener('click', () => {
+                if (!isAdmin) {
+                    alert('Only admins can delete bookmarks.');
+                    return;
+                }
+                this.deleteBookmark(bookmark.id).then(() => {
                     // reload bookmarks
                     axios.get('/api/bookmark-api/').then(res => displayBookmarks(res.data));
                 });
-            }
-        });
-        buttonTd.appendChild(editButton);
-
-        const deleteButton = document.createElement('button');
-        deleteButton.textContent = 'Delete';
-        deleteButton.addEventListener('click', () => {
-            if (!isAdmin) {
-                alert('Only admins can delete bookmarks.');
-                return;
-            }
-            deleteBookmark(bookmark.id).then(() => {
-                // reload bookmarks
-                axios.get('/api/bookmark-api/').then(res => displayBookmarks(res.data));
             });
+            buttonTd.appendChild(deleteButton);
+            tr.appendChild(buttonTd);
+
+            tableBody.appendChild(tr);
         });
-        buttonTd.appendChild(deleteButton);
-        tr.appendChild(buttonTd);
+    }
 
-        tableBody.appendChild(tr);
-    });
+    addBookmark(bookmark) {
+        return axios.post('/api/bookmark-api/', bookmark);
+    }
+
+    editBookmark(id, bookmark) {
+        return axios.put(`/api/bookmark-api/${id}`, bookmark);
+    }
+
+    deleteBookmark(id) {
+        return axios.delete(`/api/bookmark-api/${id}`);
+    }
+
 }
 
-function addBookmark(bookmark) {
-    return axios.post('/api/bookmark-api/', bookmark);
-}
-
-function editBookmark(id, bookmark) {
-    return axios.put(`/api/bookmark-api/${id}`, bookmark);
-}
-
-function deleteBookmark(id) {
-    return axios.delete(`/api/bookmark-api/${id}`);
-}
+const bookmarks = new Bookmarks();
 
 document.querySelector('#addButton').addEventListener('click', function () {
     if (!isAdmin) {
@@ -131,13 +140,13 @@ document.querySelector('#addButton').addEventListener('click', function () {
         url: row.children[2].textContent,
         comment: row.children[3].textContent
     };
-    addBookmark(bookmark).then(() => {
+    bookmarks.addBookmark(bookmark).then(() => {
         // Clear the input fields
         for (let i = 0; i < 4; i++) {
             row.children[i].textContent = '';
         }
         // Reload bookmarks
-        axios.get('/api/bookmark-api/').then(res => displayBookmarks(res.data));
+        axios.get('/api/bookmark-api/').then(res => bookmarks.displayBookmarks(res.data));
     }).catch(error => {
         console.error('Error adding bookmark:', error);
     });
@@ -147,7 +156,7 @@ window.onload = function () {
     // Fetch bookmarks from the server on window load
     axios.get('/api/bookmark-api/').then(res => {
         // Display bookmarks
-        displayBookmarks(res.data);
+        bookmarks.displayBookmarks(res.data);
     }).catch(error => {
         console.error('Error fetching bookmarks:', error);
     });
