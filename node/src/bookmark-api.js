4e6dac838e60094eaa624597cc001af1799bd06f
windsnow1025 <windsnow1025@gmail.com>
2023-07-14 03:18:55 +0000

    feat(bookmarks): auth in header

--- node/src/bookmark-api.js
+++ node/src/bookmark-api.js
@@ -3,6 +3,33 @@ const router = express.Router();
 const BookmarkSQL = require("./bookmark-sql");
 const jwt = require('jsonwebtoken');
 
+router.use((req, res, next) => {
+
+    const authHeader = req.headers.authorization;
+
+    if (authHeader) {
+        const token = authHeader.split(' ')[1];
+
+        jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+            if (err) {
+                return res.sendStatus(403);
+            }
+
+            req.user = user;
+
+            console.log(req.user.username);
+
+            if (req.user.username != "windsnow1025@gmail.com") {
+                return res.sendStatus(403);
+            }
+
+            next();
+        });
+    } else {
+        res.sendStatus(401);
+    }
+});
+
 router.get('/', async (req, res, next) => {
     try {
         let bookmarks = await BookmarkSQL.Show();
