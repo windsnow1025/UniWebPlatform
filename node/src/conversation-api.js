3e5063e94e4e606c8d79643b21e67bcad06870af
windsnow1025 <windsnow1025@gmail.com>
2023-07-21 02:54:06 +0000

    feat(conversation): backend

---
+++ node/src/conversation-api.js
@@ -0,0 +1,103 @@
+const express = require('express');
+
+const router = express.Router();
+const UserSQL = require("./conversation-sql");
+
+const jwt = require('jsonwebtoken');
+
+// Data Processing
+router.use((req, res, next) => {
+
+    const authHeader = req.headers.authorization;
+
+    if (authHeader) {
+        const token = authHeader.split(' ')[1];
+
+        jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+            if (err) {
+                return res.sendStatus(403);
+            }
+
+            req.user = user;
+
+            next();
+        });
+    } else {
+        res.sendStatus(401);
+    }
+});
+
+router.get('/', async (req, res, next) => {
+    try {
+        let data = req.body.data;
+
+        // User can only access their own data
+        if (req.user.sub != data.username) {
+            return res.sendStatus(403);
+        }
+
+        let conversations = await UserSQL.Show();
+        res.json(conversations);
+    } catch (err) {
+        console.error("Error in GET /:", err);
+        res.status(500).send("Error occurred while fetching data.");
+        next(err);
+    }
+});
+
+router.post('/', async (req, res, next) => {
+    try {
+        let data = req.body.data;
+
+        // User can only access their own data
+        if (req.user.sub != data.username) {
+            return res.sendStatus(403);
+        }
+
+        await UserSQL.Store(data);
+        res.status(200).send(true);
+    } catch (err) {
+        console.error("Error in POST /:", err);
+        res.status(500).send("Error occurred while storing data.");
+        next(err);
+    }
+});
+
+router.put('/', async (req, res, next) => {
+    try {
+        let data = req.body.data;
+
+        // User can only access their own data
+        if (req.user.sub != data.username) {
+            return res.sendStatus(403);
+        }
+
+        await UserSQL.Update(data);
+        res.status(200).send(true);
+    } catch (err) {
+        console.error("Error in PUT /:", err);
+        res.status(500).send("Error occurred while updating data.");
+        next(err);
+    }
+});
+
+router.delete('/:id', async (req, res, next) => {
+    try {
+        let data = req.body.data;
+        let id = req.params.id;
+
+        // User can only access their own data
+        if (req.user.sub != data.username) {
+            return res.sendStatus(403);
+        }
+
+        await UserSQL.Delete(id);
+        res.status(200).send(true);
+    } catch (err) {
+        console.error("Error in DELETE /:", err);
+        res.status(500).send("Error occurred while deleting data.");
+        next(err);
+    }
+});
+
+module.exports = router;
---
+++ node/src/conversation-sql.js
@@ -0,0 +1,78 @@
+const mysql = require("mysql");
+const util = require("util");
+
+// MySQL Connection Pool
+const pool = mysql.createPool({
+    host: "sql",
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE
+});
+
+// MySQL Promisify
+const poolQuery = util.promisify(pool.query).bind(pool);
+
+// MySQL Connection Test
+async function testConnection() {
+    let attempts = 0;
+    let maxAttempts = 5;
+    let delay = 5000;
+
+    await new Promise(resolve => setTimeout(resolve, delay));
+
+    while (true) {
+        try {
+            await poolQuery('SELECT * FROM conversations');
+            console.log('ConversationSQL Connected!');
+            break;
+        } catch (err) {
+            console.error('Error connecting to ConversationSQL:', err);
+            attempts++;
+            if (attempts < maxAttempts) {
+                console.log(`Attempts ${attempts + 1},Retrying in ${delay / 1000} seconds...`);
+                await new Promise(resolve => setTimeout(resolve, delay));
+            } else {
+                console.error('Max attempts reached. ConversationSQL connection failed.');
+                throw err;
+            }
+        }
+    }
+}
+
+testConnection();
+
+// MySQL Functions
+
+async function Show() {
+    const sql = "SELECT * FROM conversations";
+    const result = await poolQuery(sql);
+    return result;
+}
+
+async function Store(data) {
+    const sql = "INSERT INTO conversations (user, conversation) VALUES (?)";
+    const sqlParams = [data.user, data.conversation];
+    await poolQuery(sql, sqlParams);
+    console.log("1 record inserted");
+}
+
+async function Update(data) {
+    const sql = "UPDATE conversations SET conversation WHERE id = ?";
+    const sqlParams = [data.conversation];
+    await poolQuery(sql, sqlParams);
+    console.log("1 record updated");
+}
+
+async function Delete(id) {
+    const sql = "DELETE FROM conversations WHERE id = ?";
+    const sqlParams = [id];
+    await poolQuery(sql, sqlParams);
+    console.log("1 record deleted");
+}
+
+module.exports = {
+    Show: Show,
+    Store: Store,
+    Update: Update,
+    Delete: Delete
+};
