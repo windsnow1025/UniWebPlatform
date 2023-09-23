370fc4485ce6890691b71c9823577a660c657814
windsnow1025 <windsnow1025@gmail.com>
2023-09-23 14:17:53 +0000

    feat(react): WIP on react feature: stage 5-0

--- react/public/js/Select.js
+++ react/public/js/Select.js
@@ -4,7 +4,7 @@ import TextField from '@mui/material/TextField';
 import DeleteIcon from '@mui/icons-material/Delete';
 import '../css/react-style.css';
 
-function Select({ options, handleDelete }) {
+function Select({ options, label, handleOptionClick, handleDelete }) {
     const handleDeleteClick = (event, index) => {
         event.stopPropagation();
         handleDelete(index);
@@ -15,10 +15,10 @@ function Select({ options, handleDelete }) {
             options={options}
             getOptionLabel={(option) => option.label}
             renderInput={(params) => (
-                <TextField {...params} label="Select an option" variant="outlined" style={{ width: '300px' }} />
+                <TextField {...params} label={label} variant="outlined" style={{ width: '300px' }} />
             )}
             renderOption={(props, option, state) => (
-                <li {...props}>
+                <li {...props} onClick={() => handleOptionClick(state.index)}>
                     {option.label}
                     <DeleteIcon className="deleteIcon" onClick={(event) => handleDeleteClick(event, state.index)} />
                 </li>
