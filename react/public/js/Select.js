dce363315974f896b00efda0543122c226547978
windsnow1025 <windsnow1025@gmail.com>
2023-09-24 04:56:41 +0000

    Revert "feat(react): WIP on react feature: stage 5-1"
    This reverts commit 85934675b8b7b5762d6a32894e4d46b74facff21.

--- react/public/js/Select.js
+++ react/public/js/Select.js
@@ -2,9 +2,7 @@ import React from 'react';
 import Autocomplete from '@mui/material/Autocomplete';
 import TextField from '@mui/material/TextField';
 import DeleteIcon from '@mui/icons-material/Delete';
-import Paper from '@mui/material/Paper';
 import '../css/react-style.css';
-import {Button} from "@mui/material";
 
 function Select({ options, label, handleOptionClick, handleDelete }) {
     const handleDeleteClick = (event, index) => {
@@ -12,36 +10,18 @@ function Select({ options, label, handleOptionClick, handleDelete }) {
         handleDelete(index);
     };
 
-    const renderOption = (props, option, state) => (
-        <li {...props} onClick={() => handleOptionClick(state.index)}>
-            {option.label}
-            <DeleteIcon
-                className="deleteIcon"
-                onClick={(event) => handleDeleteClick(event, state.index)}
-            />
-        </li>
-    );
-
     return (
         <Autocomplete
             options={options}
             getOptionLabel={(option) => option.label}
             renderInput={(params) => (
-                <TextField
-                    {...params}
-                    label={label}
-                    variant="outlined"
-                    style={{ width: '300px' }}
-                />
+                <TextField {...params} label={label} variant="outlined" style={{ width: '300px' }} />
             )}
-            renderOption={renderOption}
-            PaperComponent={(props) => (
-                <Paper {...props}>
-                    {props.children}
-                    <Button>
-                        + Add New
-                    </Button>
-                </Paper>
+            renderOption={(props, option, state) => (
+                <li {...props} onClick={() => handleOptionClick(state.index)}>
+                    {option.label}
+                    <DeleteIcon className="deleteIcon" onClick={(event) => handleDeleteClick(event, state.index)} />
+                </li>
             )}
         />
     );
