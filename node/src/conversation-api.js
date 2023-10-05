17b2c6b703080f3ceb25d48f1490bae4136860ac
windsnow1025 <windsnow1025@gmail.com>
2023-10-05 10:37:47 +0000

    chore(nodejs backend): multiple code optimizations

--- node/src/conversation-api.js
+++ node/src/conversation-api.js
@@ -1,8 +1,6 @@
 const express = require('express');
-
 const router = express.Router();
-const UserSQL = require("./conversation-sql");
-
+const ConversationSQL = require("./conversation-sql");
 const jwt = require('jsonwebtoken');
 const axios = require('axios');
 
@@ -19,13 +17,10 @@ router.use(async (req, res, next) => {
             const username = await jwt.verify(token, process.env.JWT_SECRET).sub;
 
             // Get user_id from username
-            const user = await axios.post("http://localhost:3000/user", {
-                data: { username: username }
-            });
+            const user = await axios.get("http://localhost:3000/user?username=" + username);
 
             // Set req.user_id
             req.user_id = user.data.id;
-
             next();
         } catch (err) {
             res.sendStatus(403);
@@ -38,14 +33,13 @@ router.use(async (req, res, next) => {
 
 router.get('/', async (req, res, next) => {
     try {
-        let conversations = await UserSQL.Show({
+        let conversations = await ConversationSQL.Show({
             user_id: req.user_id
         });
         res.status(200).json(conversations);
     } catch (err) {
         console.error("Error in GET /:", err);
         res.status(500).send("Error occurred while fetching data.");
-        next(err);
     }
 });
 
@@ -61,12 +55,11 @@ router.post('/', async (req, res, next) => {
             conversation: conversation
         }
 
-        await UserSQL.Store(sqlData);
-        res.status(200).send(true);
+        await ConversationSQL.Store(sqlData);
+        res.status(201).send(true);
     } catch (err) {
         console.error("Error in POST /:", err);
         res.status(500).send("Error occurred while storing data.");
-        next(err);
     }
 });
 
@@ -84,12 +77,11 @@ router.put('/', async (req, res, next) => {
             id: id
         }
 
-        await UserSQL.Update(sqlData);
+        await ConversationSQL.Update(sqlData);
         res.status(200).send(true);
     } catch (err) {
         console.error("Error in PUT /:", err);
         res.status(500).send("Error occurred while updating data.");
-        next(err);
     }
 });
 
@@ -105,12 +97,11 @@ router.put('/name', async (req, res, next) => {
             id: id
         }
 
-        await UserSQL.UpdateName(sqlData);
+        await ConversationSQL.UpdateName(sqlData);
         res.status(200).send(true);
     } catch (err) {
-        console.error("Error in PUT /:", err);
+        console.error("Error in PUT /name:", err);
         res.status(500).send("Error occurred while updating data.");
-        next(err);
     }
 });
 
@@ -123,12 +114,11 @@ router.delete('/:id', async (req, res, next) => {
             id: id
         }
 
-        await UserSQL.Delete(sqlData);
+        await ConversationSQL.Delete(sqlData);
         res.status(200).send(true);
     } catch (err) {
-        console.error("Error in DELETE /:", err);
+        console.error("Error in DELETE /:id:", err);
         res.status(500).send("Error occurred while deleting data.");
-        next(err);
     }
 });
 
