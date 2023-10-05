02f0ce31aa0d2ce8aa73c927566048a30518353a
windsnow1025 <windsnow1025@gmail.com>
2023-10-05 07:00:32 +0000

    chore(sql): update table names

--- node/src/message-sql.js
+++ node/src/message-sql.js
@@ -22,7 +22,7 @@ async function testConnection() {
 
     while (true) {
         try {
-            await poolQuery('SELECT * FROM messages');
+            await poolQuery('SELECT * FROM message');
             console.log('MessageSQL Connected!');
             break;
         } catch (err) {
@@ -44,27 +44,27 @@ testConnection();
 // MySQL Functions
 
 async function Show() {
-    const sql = "SELECT * FROM messages";
+    const sql = "SELECT * FROM message";
     const result = await poolQuery(sql);
     return result;
 }
 
 async function Store(data) {
-    const sql = "INSERT INTO messages (message) VALUES (?)";
+    const sql = "INSERT INTO message (message) VALUES (?)";
     const sqlParams = [data.message];
     await poolQuery(sql, sqlParams);
     console.log("1 message inserted");
 }
 
 async function Delete(id) {
-    const sql = "DELETE FROM messages WHERE id = ?";
+    const sql = "DELETE FROM message WHERE id = ?";
     const sqlParams = [id];
     await poolQuery(sql, sqlParams);
     console.log("1 message deleted");
 }
 
 async function DeleteAll() {
-    const sql = "DELETE FROM messages";
+    const sql = "DELETE FROM message";
     const result = await poolQuery(sql);
     console.log("Number of messages deleted: " + result.affectedRows);
 }
