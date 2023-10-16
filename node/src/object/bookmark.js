d57d793a0d276b98cef04755ad2eb2d595837637
windsnow1025 <windsnow1025@gmail.com>
2023-10-16 12:29:01 +0000

    chore(bookmark): use bookmark object

---
+++ node/src/object/bookmark.js
@@ -0,0 +1,8 @@
+class Bookmark {
+    constructor(firstTitle, secondTitle, url, comment) {
+        this.firstTitle = firstTitle;
+        this.secondTitle = secondTitle;
+        this.url = url;
+        this.comment = comment;
+    }
+}
