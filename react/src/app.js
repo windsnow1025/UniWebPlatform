68975392a550d11c0ae28755d7706a9f93cc8ecd
windsnow1025 <windsnow1025@gmail.com>
2023-10-20 14:07:45 +0000

    chore(node): update packages

--- react/src/app.js
+++ react/src/app.js
@@ -1,17 +1,11 @@
 const express = require('express');
-const http = require("http");
 
 const app = express();
-
-
-// HTTP
 const port = 3000;
-http.createServer(app.listen(port, () => {
-    console.log(`Server listening at port ${port}...`);
-}));
 
-
-// Serve Static Files
 app.use(express.static('public'));
 app.use(express.static('dist'));
 
+app.listen(port, () => {
+    console.log(`Server listening at port ${port}...`);
+});
