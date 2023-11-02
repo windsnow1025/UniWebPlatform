c55f9923fa5e035addc5236f25662d3bccc6da81
windsnow1025 <windsnow1025@gmail.com>
2023-11-02 07:26:13 +0000

    feat(react): update index markdown

--- react/src/index.js
+++ react/src/index.js
@@ -14,3 +14,17 @@ theme_div.render(
         <ThemeSelect />
     </React.StrictMode>
 );
+
+// Markdown List
+import axios from "axios";
+const markdown_list = document.querySelector('#markdown-list');
+const res = await axios.get('/api/markdown/');
+const data = res.data;
+data.forEach(markdown => {
+    const li = document.createElement('li');
+    const a = document.createElement('a');
+    a.href = `/html/markdown.html?id=${markdown.id}`;
+    a.innerText = markdown.title;
+    li.appendChild(a);
+    markdown_list.appendChild(li);
+});
