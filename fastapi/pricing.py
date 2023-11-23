e55b919a5e4d09e80396b1e0d443f971d7ff0c0b
windsnow1025 <windsnow1025@gmail.com>
2023-11-23 02:53:49 +0000

    feat(fastapi): pricing

---
+++ fastapi/pricing.py
@@ -0,0 +1,31 @@
+model_pricing = {
+    "open_ai": {
+        "gpt-3.5-turbo": {},
+        "gpt-3.5-turbo-0301": {"input": 0.0015, "output": 0.0020},
+        "gpt-3.5-turbo-0613": {"input": 0.0015, "output": 0.0020},
+        "gpt-3.5-turbo-1106": {},
+        "gpt-3.5-turbo-16k": {"input": 0.0030, "output": 0.0040},
+        "gpt-3.5-turbo-16k-0613": {},
+        "gpt-4": {"input": 0.03, "output": 0.06},
+        "gpt-4-0314": {"input": 0.03, "output": 0.06},
+        "gpt-4-0613": {"input": 0.03, "output": 0.06},
+        "gpt-4-1106-preview": {"input": 0.01, "output": 0.03},
+        "gpt-4-vision-preview": {"input": 0.01, "output": 0.03}
+    },
+    "azure": {
+        "gpt-35-turbo": {"input": 0.0015, "output": 0.0020},
+        "gpt-35-turbo-16k": {"input": 0.0030, "output": 0.0040},
+        "gpt-4": {"input": 0.01, "output": 0.03},
+        "gpt-4-32k": {"input": 0.06, "output": 0.12}
+    }
+}
+
+
+def calculate_cost(api_type, model, prompt_tokens, completion_tokens):
+    pricing_info = model_pricing[api_type][model]
+
+    input_cost = pricing_info["input"] * (prompt_tokens / 1000)
+    output_cost = pricing_info["output"] * (completion_tokens / 1000)
+    total_cost = input_cost + output_cost
+
+    return total_cost
