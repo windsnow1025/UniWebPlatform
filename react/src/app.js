9874c3f19f149ac80f79fd76cae0a1b8c5998aa3
windsnow1025 <windsnow1025@gmail.com>
2023-12-10 07:50:48 +0000

    react router

--- react/src/app.js
+++ react/src/app.js
@@ -1,10 +1,16 @@
 import './App.css';
 
-import MarkdownList from './components/MarkdownList';
+import MarkdownList from './component/MarkdownList';
+import ThemeSelect from "./component/ThemeSelect";
+import AuthDiv from "./component/AuthDiv";
 
 function App() {
   return (
     <div className="App">
+      <div className="Flex-space-around" style={{margin: "16px"}}>
+        <AuthDiv />
+        <ThemeSelect />
+      </div>
       <div className="Flex-space-around" style={{margin: "16px"}}>
         <a href="./html/bookmarks.html">Bookmarks</a>
         <a href="./html/message-transmitter.html">Message Transmitter</a>
