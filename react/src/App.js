cb5ddaca5f821cc02e287ce496463da6bf0cf081
windsnow1025 <windsnow1025@gmail.com>
2023-12-11 09:48:22 +0000

    bookmark init

--- react/src/App.js
+++ react/src/App.js
@@ -12,7 +12,7 @@ function App() {
         <ThemeSelect />
       </div>
       <div className="Flex-space-around" style={{margin: "16px"}}>
-        <a href="./html/bookmarks.html">Bookmarks</a>
+        <a href="/bookmark">Bookmarks</a>
         <a href="./html/message-transmitter.html">Message Transmitter</a>
         <a href="./html/gpt.html">GPT</a>
       </div>
