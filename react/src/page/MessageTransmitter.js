db6a0baa66a54a388a5f7f3434e4fcbd7f36af74
windsnow1025 <windsnow1025@gmail.com>
2023-12-12 02:55:08 +0000

    message div init

--- react/src/page/MessageTransmitter.js
+++ react/src/page/MessageTransmitter.js
@@ -1,15 +1,17 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect } from 'react';
 import { MessageService } from "../service/MessageService";
 import { AuthManager } from "../manager/AuthManager";
 import AuthDiv from '../component/AuthDiv';
 import ThemeSelect from '../component/ThemeSelect';
+import MessageDiv from '../component/MessageDiv';
 
 function MessageTransmitter() {
   const [messages, setMessages] = useState([]);
   const [username, setUsername] = useState(null);
+  const [newMessage, setNewMessage] = useState({ role: '', content: '' });
   const messageService = new MessageService();
+
   const authManager = new AuthManager();
-  const messageInputRef = useRef(null);
 
   useEffect(() => {
     fetchMessages();
@@ -18,7 +20,7 @@ function MessageTransmitter() {
 
   const fetchMessages = async () => {
     const fetchedMessages = await messageService.fetchMessages();
-    setMessages(fetchedMessages);
+    setMessages(fetchedMessages.map(msg => ({ ...msg, editing: false })));
   };
 
   const fetchUsername = async () => {
@@ -27,17 +29,24 @@ function MessageTransmitter() {
   };
 
   const handleSendMessage = async () => {
-    if (messageInputRef.current) {
-      const message = messageInputRef.current.textContent;
-      await messageService.sendMessage(username, message);
-      messageInputRef.current.textContent = '';
-      fetchMessages();
-    }
+    // Send the message stored in newMessage
+    await messageService.sendMessage(newMessage.role, newMessage.content); // Replace 'username' with actual username
+    setNewMessage({ role: '', content: '' }); // Clear the new message
+    fetchMessages();
   };
 
   const handleClearMessages = async () => {
     await messageService.deleteMessages();
     fetchMessages();
+  };
+
+  // Handler for when the content of the new message is changed
+  const onNewMessageRoleChange = (role) => {
+    setNewMessage(prev => ({ ...prev, role }));
+  }
+
+  const onNewMessageContentChange = (content) => {
+    setNewMessage(prev => ({ ...prev, content }));
   };
 
   return (
@@ -50,11 +59,15 @@ function MessageTransmitter() {
       <div>
         <h2 className="center">Receive Messages</h2>
         <div className="rounded-border-container">
-          <div className="margin">
-            {messages.map((msg, index) => (
-              <div key={index} className="message_div">{msg.content}</div>
-            ))}
-          </div>
+          {messages.map(msg => (
+            <MessageDiv
+              key={msg.id}
+              roleInitial={msg.role}
+              contentInitial={msg.content}
+              onRoleChange={() => {}} // Role change handler if needed
+              onContentChange={() => {}} // Content change handler if needed
+            />
+          ))}
         </div>
         <div className="center">
           <button type="button" onClick={fetchMessages}>Receive</button>
@@ -63,18 +76,14 @@ function MessageTransmitter() {
       </div>
       <div>
         <h2 className="center">Send Messages</h2>
-        <div className="message_div">
-          <div className="Flex-space-between">
-            <div
-              ref={messageInputRef}
-              className="inFlex-FillSpace markdown-body"
-              style={{ margin: '8px', padding: '8px', minHeight: '24px' }}
-              contentEditable="plaintext-only"
-            ></div>
-            <div className="button inFlex-flex-end">
-              <button type="button" onClick={handleSendMessage} title="Ctrl + Enter">Send</button>
-            </div>
-          </div>
+        <MessageDiv
+          roleInitial={username}
+          contentInitial={newMessage.content}
+          onRoleChange={onNewMessageRoleChange}
+          onContentChange={onNewMessageContentChange}
+        />
+        <div className="center">
+          <button type="button" onClick={handleSendMessage}>Send</button>
         </div>
       </div>
     </div>
