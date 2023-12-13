289ecd21d097c3ab65e4cdcd7ff48665bc34e179
windsnow1025 <windsnow1025@gmail.com>
2023-12-13 16:02:34 +0000

    Update GPT.js

--- react/src/page/GPT.js
+++ react/src/page/GPT.js
@@ -1,6 +1,8 @@
 import React, {useState, useEffect} from 'react';
 import AuthDiv from '../component/AuthDiv';
 import ThemeSelect from '../component/ThemeSelect';
+import MessageDiv from "../component/MessageDiv";
+import axios from 'axios';
 
 function GPT() {
   return (
