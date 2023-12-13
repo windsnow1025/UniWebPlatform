65d781f55c6093c98689804d0f613817238bd7a6
windsnow1025 <windsnow1025@gmail.com>
2023-12-13 11:34:23 +0000

    separate message, revert dependency removal

---
+++ react/src/component/ContentDiv.js
@@ -0,0 +1,43 @@
+import {useEffect, useRef, useState} from "react";
+import {parseLaTeX, parseMarkdown} from "../util/MarkdownParser";
+
+function ContentDiv({ contentInitial, onContentChange }) {
+  const [content, setContent] = useState(contentInitial);
+  const [editing, setEditing] = useState(false);
+  const contentRef = useRef(null);
+
+  useEffect(() => {
+    setContent(contentInitial);
+  }, [contentInitial]);
+
+  useEffect(() => {
+    if (contentRef.current) {
+      if (!editing) {
+        contentRef.current.innerHTML = parseMarkdown(content);
+        parseLaTeX(contentRef.current);
+      } else {
+        contentRef.current.textContent = content;
+      }
+    }
+  }, [content, editing]);
+
+  const handleContentChange = () => {
+    const newContent = contentRef.current.textContent;
+    setContent(newContent);
+    onContentChange(newContent);
+    setEditing(false);
+  };
+
+  return (
+    <div
+      className="markdown-body"
+      style={{ margin: '8px', padding: '8px', minHeight: '24px' }}
+      contentEditable="plaintext-only"
+      ref={contentRef}
+      onFocus={() => setEditing(true)}
+      onBlur={handleContentChange}
+    ></div>
+  );
+}
+
+export default ContentDiv;
