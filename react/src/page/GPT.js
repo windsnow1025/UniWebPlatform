fa0b415cd48454d775015f883fae1b5e5d4b3a91
windsnow1025 <windsnow1025@gmail.com>
2023-12-13 16:15:42 +0000

    gpt wip 1

--- react/src/page/GPT.js
+++ react/src/page/GPT.js
@@ -5,12 +5,30 @@ import MessageDiv from "../component/MessageDiv";
 import axios from 'axios';
 
 function GPT() {
+  const [messages, setMessages] = useState([
+    {
+      "role": "system",
+      "content": "You are a helpful assistant."
+    }
+  ]);
+  const [api_type, setApi_type] = useState('open_ai');
+  const [model, setModel] = useState('gpt-3.5-turbo');
+  const [temperature, setTemperature] = useState(0);
+  const [stream, setStream] = useState(true);
+
   return (
     <div>
+      <h1 className="center">WindsnowGPT</h1>
       <div className="Flex-space-around">
         <AuthDiv/>
         <ThemeSelect/>
       </div>
+      <div className="Flex-space-around">
+        <a href="/markdown/view/gpt-documentation.md">Documentation</a>
+        <a href="/markdown/view/gpt-presets.md">System Presets</a>
+        <div>Credit</div>
+      </div>
+
     </div>
   )
 }
