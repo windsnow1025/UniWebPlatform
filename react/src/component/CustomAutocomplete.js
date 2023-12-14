0ede8b0a6314887c3fc4dc9cd0db020e165ef8f2
windsnow1025 <windsnow1025@gmail.com>
2023-12-14 17:27:00 +0000

    gpt wip 8

---
+++ react/src/component/CustomAutocomplete.js
@@ -0,0 +1,87 @@
+import React from 'react';
+import Autocomplete, { createFilterOptions } from '@mui/material/Autocomplete';
+import TextField from '@mui/material/TextField';
+import DeleteIcon from '@mui/icons-material/Delete';
+import UpdateIcon from '@mui/icons-material/Update';
+import {Tooltip} from "@mui/material";
+import '../asset/css/react-style.css';
+
+const filter = createFilterOptions();
+
+function AutocompleteComponent({ options, label, handleOptionClick, handleDelete, handleAdd, handleUpdate }) {
+
+  const handleDeleteClick = (event, index) => {
+    event.stopPropagation();
+    if (window.confirm('Are you sure you want to delete this item?')) {
+      handleDelete(index);
+    }
+  };
+
+  const handleUpdateClick = (event, index, label) => {
+    event.stopPropagation();
+    if (window.confirm('Are you sure you want to update this item?')) {
+      handleUpdate(index, label);
+    }
+  };
+
+  return (
+    <Autocomplete
+      filterOptions={(options, params) => {
+        const filtered = filter(options, params);
+
+        const { inputValue } = params;
+        const isExisting = options.some((option) => inputValue === option.label);
+        if (inputValue !== '' && !isExisting) {
+          filtered.push({
+            inputValue,
+            label: `Add "${inputValue}"`,
+            isNew: true,
+          });
+        }
+
+        return filtered;
+      }}
+      selectOnFocus
+      clearOnBlur
+      handleHomeEndKeys
+      options={options}
+      getOptionLabel={(option) => {
+        if (option.inputValue) {
+          return option.inputValue;
+        }
+        return option.label;
+      }}
+      renderOption={(props, option, state) => (
+        <li {...props} onClick={() => {
+          if (option.isNew) {
+            if (window.confirm('Are you sure you want to add this item?')) {
+              handleAdd(option.inputValue);
+            }
+          } else {
+            if (window.confirm('Are you sure you want to select this item?')) {
+              handleOptionClick(state.index);
+            }
+          }
+        }}>
+          {option.label}
+          {!option.isNew &&
+            <div className="iconContainer">
+              <Tooltip title="Update">
+                <UpdateIcon className="icon" onClick={(event) => handleUpdateClick(event, state.index, option.label)} />
+              </Tooltip>
+              <Tooltip title="Delete">
+                <DeleteIcon className="icon" onClick={(event) => handleDeleteClick(event, state.index)} />
+              </Tooltip>
+            </div>
+          }
+        </li>
+      )}
+      freeSolo
+      renderInput={(params) => (
+        <TextField {...params} label={label} variant="outlined" style={{ width: '300px' }} />
+      )}
+    />
+  );
+}
+
+export default AutocompleteComponent;
