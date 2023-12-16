d6086caa4e3bed6dfd4ed4127dd6ac54316f4924
windsnow1025 <windsnow1025@gmail.com>
2023-12-16 15:34:35 +0000

    feat(react): mui user-center

--- react/src/App.js
+++ react/src/App.js
@@ -3,23 +3,37 @@ import './asset/css/App.css';
 import MarkdownList from './component/MarkdownList';
 import ThemeSelect from "./component/ThemeSelect";
 import AuthDiv from "./component/AuthDiv";
+import {ThemeProvider} from "@mui/material/styles";
+import {useEffect, useState} from "react";
+import {getInitMUITheme} from "./logic/ThemeLogic";
 
 function App() {
+  const [theme, setTheme] = useState(getInitMUITheme());
+
+  useEffect(() => {
+    const handleThemeChange = (event) => {
+      setTheme(event.detail);
+    };
+    window.addEventListener('themeChanged', handleThemeChange);
+  }, []);
+
   return (
-    <div className="App">
-      <div className="Flex-space-around" style={{margin: "16px"}}>
-        <AuthDiv />
-        <ThemeSelect />
+    <ThemeProvider theme={theme}>
+      <div className="App">
+        <div className="Flex-space-around" style={{margin: "16px"}}>
+          <AuthDiv/>
+          <ThemeSelect/>
+        </div>
+        <div className="Flex-space-around" style={{margin: "16px"}}>
+          <a href="/bookmark" target="_blank" rel="noopener noreferrer">Bookmarks</a>
+          <a href="/message" target="_blank" rel="noopener noreferrer">Message Transmitter</a>
+          <a href="/gpt" target="_blank" rel="noopener noreferrer">GPT</a>
+        </div>
+        <div className="Flex-space-around" style={{margin: "16px"}}>
+          <MarkdownList/>
+        </div>
       </div>
-      <div className="Flex-space-around" style={{margin: "16px"}}>
-        <a href="/bookmark" target="_blank" rel="noopener noreferrer">Bookmarks</a>
-        <a href="/message" target="_blank" rel="noopener noreferrer">Message Transmitter</a>
-        <a href="/gpt" target="_blank" rel="noopener noreferrer">GPT</a>
-      </div>
-      <div className="Flex-space-around" style={{margin: "16px"}}>
-        <MarkdownList />
-      </div>
-    </div>
+    </ThemeProvider>
   );
 }
 
