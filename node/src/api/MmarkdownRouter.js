e72b4214497d99e9416de6f0e60ae6daceb1e451
windsnow1025 <windsnow1025@gmail.com>
2023-12-18 08:54:27 +0000

    refactor(node): uppercase refactor WIP 1

--- node/src/api/MmarkdownRouter.js
+++ node/src/api/MmarkdownRouter.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const MarkdownDAO = require("../db/markdownDAO");
+const MarkdownDAO = require("../db/MmarkdownDAO");
 const jwt = require('jsonwebtoken');
 
 
--- node/src/api/UuserRouter.js
+++ node/src/api/UuserRouter.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const UserDAO = require("../db/userDAO");
+const UserDAO = require("../db/UuserDAO");
 
 const jwt = require('jsonwebtoken');
 
