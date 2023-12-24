b2fb960c6b115b8687998a245ce8269673eebff2
windsnow1025 <windsnow1025@gmail.com>
2023-12-24 16:24:34 +0000

    feat(fastapi): singleton db connection class

---
+++ fastapi/app/util/db_util.py
@@ -0,0 +1,24 @@
+import os
+from mysql.connector.pooling import MySQLConnectionPool, PooledMySQLConnection
+
+
+class DatabaseConnection:
+    _instance = None
+
+    def __init__(self):
+        dbconfig = {
+            "host": os.getenv('MYSQL_HOST'),
+            "user": os.getenv('MYSQL_USER'),
+            "password": os.getenv('MYSQL_PASSWORD'),
+            "database": os.getenv('MYSQL_DATABASE')
+        }
+        self.connection_pool = MySQLConnectionPool(**dbconfig)
+
+    @staticmethod
+    def get_instance():
+        if DatabaseConnection._instance is None:
+            DatabaseConnection._instance = DatabaseConnection()
+        return DatabaseConnection._instance
+
+    def get_connection(self) -> PooledMySQLConnection:
+        return self.connection_pool.get_connection()
