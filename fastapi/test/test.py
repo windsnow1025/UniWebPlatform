4c02c966c89911839a9b5ed9bba07c6be543528c
windsnow1025 <windsnow1025@gmail.com>
2023-12-28 18:01:37 +0000

    test(fastapi): add assertions

--- fastapi/test/test.py
+++ fastapi/test/test.py
@@ -33,19 +33,30 @@ async def openai_test():
     prompt_tokens = sum(len(message["content"]) for message in messages)
     completion_tokens = 0
 
+    output = ""
     if stream:
         async for content in response.body_iterator:
+            output += content
             print(content, end='')
             completion_tokens += len(content)
         print()
     else:
+        output = response
         print(response)
         completion_tokens += len(response)
 
     cost = calculate_cost(api_type, model, prompt_tokens, completion_tokens)
     print(f"Cost: ${cost:.4f}")
 
+    # Test
+    assert output == "This is a test."
+    assert abs(cost - 0.0015) < 0.0001
 
-if __name__ == "__main__":
+
+def main():
     init_environment()
     asyncio.run(openai_test())
+
+
+if __name__ == "__main__":
+    main()
