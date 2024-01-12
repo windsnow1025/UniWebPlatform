f2ba73ceda2a9afa4126084706d9ed7a89f5fb27
windsnow1025 <windsnow1025@gmail.com>
2024-01-12 18:14:33 +0000

    refactor(next)!: next.js dev migration

---
+++ next/pages/markdown-add.js
@@ -0,0 +1,76 @@
+import '../src/asset/css/App.css';
+import '../src/asset/css/markdown.css';
+
+import React, {useState, useRef, useEffect} from 'react';
+import {ThemeProvider} from "@mui/material/styles";
+import {parseMarkdown} from "../src/util/MarkdownParser";
+import {parseLaTeX} from "../src/util/LaTeXParser";
+import { MarkdownLogic } from '../src/logic/MarkdownLogic';
+import AuthDiv from "../src/component/AuthDiv";
+import ThemeSelect from "../src/component/ThemeSelect";
+import {getInitMUITheme, getLightMUITheme} from "../src/logic/ThemeLogic";
+import {Button} from "@mui/material";
+
+function MarkdownAdd() {
+  const [theme, setTheme] = useState(getLightMUITheme());
+
+  useEffect(() => {
+    const handleThemeChange = (event) => {
+      setTheme(event.detail);
+    };
+    window.addEventListener('themeChanged', handleThemeChange);
+    setTheme(getInitMUITheme());
+  }, []);
+
+  const [content, setContent] = useState('');
+  const [isEditing, setIsEditing] = useState(false);
+  const markdownRef = useRef(null);
+  const markdownLogic = new MarkdownLogic();
+
+  useEffect(() => {
+    document.title = "Markdown Add";
+  }, []);
+
+  const handleEdit = () => {
+    markdownRef.current.innerHTML = content;
+    setIsEditing(true);
+  };
+
+  const handleConfirm = () => {
+    if (markdownRef.current) {
+      const content = markdownRef.current.innerHTML;
+      setContent(content);
+      markdownRef.current.innerHTML = parseMarkdown(content);
+      parseLaTeX(markdownRef.current);
+    }
+    setIsEditing(false);
+  };
+
+  const handleAdd = async () => {
+    const title = markdownLogic.getTitleFromContent(content);
+    await markdownLogic.addMarkdown(title, content);
+  };
+
+  return (
+    <ThemeProvider theme={theme}>
+      <h1 className="center">Markdown Add</h1>
+      <div className="Flex-space-around">
+        <AuthDiv/>
+        <ThemeSelect/>
+      </div>
+      <div
+        className="markdown-body"
+        ref={markdownRef}
+        style={{ margin: 8, padding: 8, minHeight: 24 }}
+        contentEditable={isEditing ? "plaintext-only" : "false"}
+      />
+      <div className="center">
+        {!isEditing && <Button variant="outlined" onClick={handleEdit} style={{margin: 4}}>Edit</Button>}
+        {isEditing && <Button variant="outlined" onClick={handleConfirm} style={{margin: 4}}>Confirm</Button>}
+        <Button variant="outlined" onClick={handleAdd} style={{margin: 4}}>Add</Button>
+      </div>
+    </ThemeProvider>
+  );
+}
+
+export default MarkdownAdd;
---
+++ next/src/logic/BookmarkLogic.js
@@ -0,0 +1,92 @@
+import BookmarkService from "../service/BookmarkService";
+
+export class BookmarkLogic {
+
+  constructor() {
+    this.bookmarkService = new BookmarkService();
+  }
+
+  async fetchBookmarks() {
+    try {
+      const bookmarks = await this.bookmarkService.fetchBookmarks();
+      return this.sortBookmarks(bookmarks);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  async addBookmark(newBookmark) {
+    // await this.bookmarkService.addBookmark(newBookmark);
+    try {
+      await this.bookmarkService.addBookmark(newBookmark);
+    } catch (error) {
+      if (error.response.status === 401) {
+        alert('Unauthorized');
+      } else if (error.response.status === 403) {
+        alert('Forbidden');
+      } else {
+        console.error(error);
+      }
+    }
+  }
+
+  async updateBookmark(id, updatedFields) {
+    try {
+      await this.bookmarkService.updateBookmark(id, updatedFields);
+    } catch (error) {
+      if (error.response.status === 401) {
+        alert('Unauthorized');
+      } else if (error.response.status === 403) {
+        alert('Forbidden');
+      } else {
+        console.error(error);
+      }
+    }
+  }
+
+  async deleteBookmark(id) {
+    try {
+      await this.bookmarkService.deleteBookmark(id);
+    } catch (error) {
+      if (error.response.status === 401) {
+        alert('Unauthorized');
+      } else if (error.response.status === 403) {
+        alert('Forbidden');
+      } else {
+        console.error(error);
+      }
+    }
+  }
+
+  sortBookmarks(bookmarks) {
+    bookmarks.sort((a, b) => {
+      if (a.first_title < b.first_title) return -1;
+      if (a.first_title > b.first_title) return 1;
+      if (a.second_title < b.second_title) return -1;
+      if (a.second_title > b.second_title) return 1;
+      if (a.comment < b.comment) return -1;
+      if (a.comment > b.comment) return 1;
+      return 0;
+    });
+    return bookmarks;
+  }
+
+  filterBookmarks(bookmarks, filters) {
+    const { searchGlobal, searchFirstTitle, searchSecondTitle, searchUrl, searchComment } = filters;
+
+    let filteredBookmarks = bookmarks.filter(bookmark =>
+      (searchGlobal === '' ||
+        bookmark.first_title.toLowerCase().includes(searchGlobal.toLowerCase()) ||
+        bookmark.second_title.toLowerCase().includes(searchGlobal.toLowerCase()) ||
+        bookmark.url.toLowerCase().includes(searchGlobal.toLowerCase()) ||
+        bookmark.comment.toLowerCase().includes(searchGlobal.toLowerCase())) &&
+      (searchFirstTitle === '' || bookmark.first_title.toLowerCase().includes(searchFirstTitle.toLowerCase())) &&
+      (searchSecondTitle === '' || bookmark.second_title.toLowerCase().includes(searchSecondTitle.toLowerCase())) &&
+      (searchUrl === '' || bookmark.url.toLowerCase().includes(searchUrl.toLowerCase())) &&
+      (searchComment === '' || bookmark.comment.toLowerCase().includes(searchComment.toLowerCase()))
+    );
+
+    return this.sortBookmarks(filteredBookmarks);
+  }
+
+}
