5d2d5179cf3fc1997184e7dd7430d105a616d831
windsnow1025 <windsnow1025@gmail.com>
2024-01-15 17:51:06 +0000

    feat: refactor bookmark with mui data grid

--- next/pages/bookmark.js
+++ next/pages/bookmark.js
@@ -1,16 +1,52 @@
 import '../src/asset/css/App.css';
 
 import React, {useState, useEffect} from 'react';
-import {ThemeProvider} from "@mui/material/styles";
+import {ThemeProvider} from '@mui/material/styles';
 import AuthDiv from '../src/component/AuthDiv';
 import ThemeSelect from '../src/component/ThemeSelect';
-import {BookmarkLogic} from "../src/logic/BookmarkLogic";
-import {getInitMUITheme, getLightMUITheme} from "../src/logic/ThemeLogic";
-import TextField from "@mui/material/TextField";
-import {Button} from "@mui/material";
+import {BookmarkLogic} from '../src/logic/BookmarkLogic';
+import {getInitMUITheme, getLightMUITheme} from '../src/logic/ThemeLogic';
+import {
+  GridRowModes,
+  DataGrid,
+  GridToolbarContainer,
+  GridActionsCellItem,
+} from '@mui/x-data-grid';
+import Button from '@mui/material/Button';
+import AddIcon from '@mui/icons-material/Add';
+import EditIcon from '@mui/icons-material/Edit';
+import DeleteIcon from '@mui/icons-material/Delete';
+import SaveIcon from '@mui/icons-material/Save';
+import CancelIcon from '@mui/icons-material/Close';
+
+function EditToolbar(props) {
+  const {setRows, setRowModesModel, rows} = props;
+
+  const handleClick = () => {
+    const id = Math.max(...rows.map((row) => row.id)) + 1;
+    console.log(id);
+    setRows((oldRows) => [{id, firstTitle: '', secondTitle: '', url: '', comment: '', isNew: true}, ...oldRows]);
+    setRowModesModel((oldModel) => ({
+      ...oldModel,
+      [id]: {mode: GridRowModes.Edit, fieldToFocus: 'firstTitle'},
+    }));
+  };
+
+  return (
+    <GridToolbarContainer>
+      <Button color="primary" startIcon={<AddIcon/>} onClick={handleClick}>
+        Add bookmark
+      </Button>
+    </GridToolbarContainer>
+  );
+}
 
 function Bookmark() {
   const [theme, setTheme] = useState(getLightMUITheme());
+  const [rows, setRows] = useState([]);
+  const [rowModesModel, setRowModesModel] = useState({});
+
+  const bookmarkLogic = new BookmarkLogic();
 
   useEffect(() => {
     const handleThemeChange = (event) => {
@@ -20,18 +56,6 @@ function Bookmark() {
     setTheme(getInitMUITheme());
   }, []);
 
-  const [bookmarks, setBookmarks] = useState([]);
-  const [newBookmark, setNewBookmark] = useState({firstTitle: '', secondTitle: '', url: '', comment: ''});
-  const [editStates, setEditStates] = useState({});
-  const [editableContents, setEditableContents] = useState({});
-  const [searchGlobal, setSearchGlobal] = useState('');
-  const [searchFirstTitle, setSearchFirstTitle] = useState('');
-  const [searchSecondTitle, setSearchSecondTitle] = useState('');
-  const [searchUrl, setSearchUrl] = useState('');
-  const [searchComment, setSearchComment] = useState('');
-
-  const bookmarkLogic = new BookmarkLogic();
-
   useEffect(() => {
     loadBookmarks();
     document.title = "Bookmark";
@@ -39,67 +63,83 @@ function Bookmark() {
 
   const loadBookmarks = async () => {
     const bookmarks = await bookmarkLogic.fetchBookmarks();
-
-    if (!bookmarks) {
-      return;
+    if (bookmarks) {
+      setRows(bookmarks);
     }
-
-    setBookmarks(bookmarks);
-    // Initialize edit states
-    let initEditStates = {};
-    bookmarks.forEach(bookmark => {
-      initEditStates[bookmark.id] = false;
-    });
-    setEditStates(initEditStates);
   };
 
-  const handleAddBookmark = async () => {
-    await bookmarkLogic.addBookmark(newBookmark);
+  const processRowUpdate = async (newRow) => {
+    const updatedRow = { ...newRow, isNew: false };
+    setRows(rows.map((row) => (row.id === newRow.id ? updatedRow : row)));
+    if (newRow.isNew) {
+      await bookmarkLogic.addBookmark(updatedRow);
+    } else {
+      await bookmarkLogic.updateBookmark(updatedRow.id, updatedRow);
+    }
     loadBookmarks();
-    setNewBookmark({firstTitle: '', secondTitle: '', url: '', comment: ''});
+    return updatedRow;
   };
 
-  const handleUpdateBookmark = async (id, updatedFields) => {
-    const updatedBookmark = {...bookmarks.find(bookmark => bookmark.id === id), ...updatedFields};
-    await bookmarkLogic.updateBookmark(id, updatedBookmark);
-    loadBookmarks();
-  };
-
-  const handleEditableContentChange = (id, field, value) => {
-    setEditableContents(prev => ({
-      ...prev,
-      [id]: {...prev[id], [field]: value}
-    }));
-  };
-
-  const handleDeleteBookmark = async (id) => {
+  const handleDeleteClick = async (id) => {
     await bookmarkLogic.deleteBookmark(id);
     loadBookmarks();
   };
 
-  const toggleEditState = (id) => {
-    setEditStates(prev => ({...prev, [id]: !prev[id]}));
-    if (!editStates[id]) {
-      // Entering edit mode
-      const bookmarkToEdit = bookmarks.find(bookmark => bookmark.id === id);
-      setEditableContents(prev => ({
-        ...prev,
-        [id]: {
-          firstTitle: bookmarkToEdit.first_title,
-          secondTitle: bookmarkToEdit.second_title,
-          url: bookmarkToEdit.url,
-          comment: bookmarkToEdit.comment
-        }
-      }));
-    } else {
-      // Leaving edit mode
-      handleUpdateBookmark(id, editableContents[id]);
+  const handleCancelClick = (id) => () => {
+    setRowModesModel({
+      ...rowModesModel,
+      [id]: { mode: GridRowModes.View, ignoreModifications: true },
+    });
+
+    const editedRow = rows.find((row) => row.id === id);
+    if (editedRow.isNew) {
+      setRows(rows.filter((row) => row.id !== id));
     }
   };
 
-  const filteredBookmarks = bookmarkLogic.filterBookmarks(bookmarks, {
-    searchGlobal, searchFirstTitle, searchSecondTitle, searchUrl, searchComment
-  });
+  const columns = [
+    {field: 'firstTitle', headerName: 'First Title', flex: 0.15, editable: true},
+    {field: 'secondTitle', headerName: 'Second Title', flex: 0.15, editable: true},
+    {field: 'url', headerName: 'URL', flex: 0.35, editable: true},
+    {field: 'comment', headerName: 'Comment', flex: 0.3, editable: true},
+    {
+      field: 'actions',
+      type: 'actions',
+      headerName: 'Actions',
+      flex: 0.2,
+      getActions: ({id}) => {
+        const isInEditMode = rowModesModel[id]?.mode === 'edit';
+
+        if (isInEditMode) {
+          return [
+            <GridActionsCellItem
+              icon={<SaveIcon/>}
+              label="Save"
+              onClick={() => setRowModesModel({...rowModesModel, [id]: {mode: GridRowModes.View}})}
+            />,
+            <GridActionsCellItem
+              icon={<CancelIcon/>}
+              label="Cancel"
+              onClick={handleCancelClick(id)}
+            />,
+          ];
+        }
+
+        return [
+          <GridActionsCellItem
+            icon={<EditIcon/>}
+            label="Edit"
+            onClick={() => setRowModesModel({...rowModesModel, [id]: {mode: GridRowModes.Edit}})}
+          />,
+          <GridActionsCellItem
+            icon={<DeleteIcon/>}
+            label="Delete"
+            onClick={() => handleDeleteClick(id)}
+          />,
+        ];
+      },
+    },
+  ];
 
   return (
     <ThemeProvider theme={theme}>
@@ -108,113 +148,21 @@ function Bookmark() {
         <AuthDiv/>
         <ThemeSelect/>
       </div>
-      <div className="center">
-        <TextField
-          label="Global Search..."
-          variant="outlined"
-          type="text"
-          value={searchGlobal}
-          onChange={(e) => setSearchGlobal(e.target.value)}
-          style={{margin: 8}}
+      <div>
+        <DataGrid
+          rows={rows}
+          columns={columns}
+          editMode="row"
+          rowModesModel={rowModesModel}
+          handleRowModelChange={(newModel) => setRowModesModel(newModel)}
+          processRowUpdate={processRowUpdate}
+          slots={{
+            toolbar: EditToolbar,
+          }}
+          slotProps={{
+            toolbar: {setRows, setRowModesModel, rows},
+          }}
         />
-        <TextField
-          label="Search by First Title..."
-          variant="outlined"
-          type="text"
-          value={searchFirstTitle}
-          onChange={(e) => setSearchFirstTitle(e.target.value)}
-          style={{margin: 8}}
-        />
-        <TextField
-          label="Search by Second Title..."
-          variant="outlined"
-          type="text"
-          value={searchSecondTitle}
-          onChange={(e) => setSearchSecondTitle(e.target.value)}
-          style={{margin: 8}}
-        />
-        <TextField
-          label="Search by URL..."
-          variant="outlined"
-          type="text"
-          value={searchUrl}
-          onChange={(e) => setSearchUrl(e.target.value)}
-          style={{margin: 8}}
-        />
-        <TextField
-          label="Search by Comment..."
-          variant="outlined"
-          type="text"
-          value={searchComment}
-          onChange={(e) => setSearchComment(e.target.value)}
-          style={{margin: 8}}
-        />
-      </div>
-      <div style={{padding: 16}}>
-        <table>
-          <thead>
-          <tr>
-            <th>First Title</th>
-            <th>Second Title</th>
-            <th>URL</th>
-            <th>Comment</th>
-            <th>Link</th>
-            <th>Actions</th>
-          </tr>
-          </thead>
-          <tbody>
-          {/* Add bookmark row */}
-          <tr>
-            <td contentEditable="plaintext-only"
-                onBlur={e => setNewBookmark({...newBookmark, firstTitle: e.target.textContent})}>
-              {newBookmark.firstTitle}</td>
-            <td contentEditable="plaintext-only"
-                onBlur={e => setNewBookmark({...newBookmark, secondTitle: e.target.textContent})}>
-              {newBookmark.secondTitle}</td>
-            <td contentEditable="plaintext-only"
-                onBlur={e => setNewBookmark({...newBookmark, url: e.target.textContent})}>
-              {newBookmark.url}</td>
-            <td contentEditable="plaintext-only"
-                onBlur={e => setNewBookmark({...newBookmark, comment: e.target.textContent})}>
-              {newBookmark.comment}</td>
-            <td></td>
-            <td>
-              <Button variant="outlined" size="small" onClick={handleAddBookmark}>
-                Add
-              </Button>
-            </td>
-          </tr>
-          {/* Bookmark rows */}
-          {filteredBookmarks.map(bookmark => (
-            <tr key={bookmark.id}>
-              <td contentEditable={editStates[bookmark.id]}
-                  onBlur={e => handleEditableContentChange(bookmark.id, 'firstTitle', e.target.textContent)}>
-                {bookmark.first_title}</td>
-              <td contentEditable={editStates[bookmark.id]}
-                  onBlur={e => handleEditableContentChange(bookmark.id, 'secondTitle', e.target.textContent)}>
-                {bookmark.second_title}</td>
-              <td contentEditable={editStates[bookmark.id]}
-                  onBlur={e => handleEditableContentChange(bookmark.id, 'url', e.target.textContent)}
-                  className="word-break">
-                {bookmark.url}</td>
-              <td contentEditable={editStates[bookmark.id]}
-                  onBlur={e => handleEditableContentChange(bookmark.id, 'comment', e.target.textContent)}>
-                {bookmark.comment}</td>
-              <td className="word-break">
-                <a href={bookmark.url} target="_blank" rel="noopener noreferrer">{bookmark.url}</a>
-              </td>
-              <td>
-                <Button variant="outlined" size="small" onClick={() => toggleEditState(bookmark.id)}>
-                  {editStates[bookmark.id] ? 'Submit' : 'Edit'}
-                </Button>
-                <Button variant="outlined" size="small" onClick={() => handleDeleteBookmark(bookmark.id)}>
-                  Delete
-                </Button>
-              </td>
-            </tr>
-          ))}
-          </tbody>
-        </table>
       </div>
     </ThemeProvider>
   );
