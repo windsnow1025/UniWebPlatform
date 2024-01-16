f261bd4d574afeb8630fb93d1b81aab3d8b58491
windsnow1025 <windsnow1025@gmail.com>
2024-01-16 07:41:52 +0000

    fix(next): auth div button margin

--- next/app/components/MessageDiv.js
+++ next/app/components/MessageDiv.js
@@ -44,7 +44,7 @@ function MessageDiv({
   };
 
   return (
-    <Paper elevation={4} className="message_div my-1 p-2 rounded-lg">
+    <Paper elevation={4} className="my-1 p-2 rounded-lg">
       {useRoleSelect ?
         <RoleSelect
           roleInitial={roleInitial}
