07d288bc01cfdc30642b6c3f903853ca6672048f
windsnow1025 <windsnow1025@gmail.com>
2024-01-17 13:33:35 +0000

    test(fastapi): update test

--- fastapi/test/test.py
+++ fastapi/test/test.py
@@ -2,7 +2,7 @@ import unittest
 
 from app.logic.completion import ChatCompletionFactory
 from app.config import init_environment
-from app.api.completion_router import stream_handler
+from app.api.completion_router import stream_handler, non_stream_handler, ChatRequest
 from app.util.pricing import calculate_cost
 
 
@@ -11,26 +11,37 @@ class TestCompletion(unittest.IsolatedAsyncioTestCase):
         init_environment()
 
     async def test_completion(self):
-        messages = [
-            {
-                "role": "user",
-                "content": "Say this is a test."
-            }
-        ]
-        model = "gpt-4"
-        api_type = "open_ai"
-        temperature = 0
-        stream = True
+        username = "test"
+        chat_request = ChatRequest(
+            messages=[
+                {
+                    "role": "user",
+                    "content": "Say this is a test."
+                }
+            ],
+            model="gpt-4",
+            api_type="open_ai",
+            temperature=0,
+            stream=True
+        )
 
-        factory = ChatCompletionFactory(messages, model, api_type, temperature, stream, stream_handler)
+        factory = ChatCompletionFactory(
+            messages=chat_request.messages,
+            model=chat_request.model,
+            api_type=chat_request.api_type,
+            temperature=chat_request.temperature,
+            stream=chat_request.stream,
+            stream_handler=lambda generator_function: stream_handler(generator_function, username, chat_request),
+            non_stream_handler=lambda content: non_stream_handler(content, username, chat_request)
+        )
         completion = factory.create_chat_completion()
         response = completion.process_request()
 
-        prompt_tokens = sum(len(message["content"]) for message in messages)
+        prompt_tokens = sum(len(message["content"]) for message in chat_request.messages)
         completion_tokens = 0
 
         output = ""
-        if stream:
+        if chat_request.stream:
             async for content in response.body_iterator:
                 output += content
                 completion_tokens += len(content)
@@ -38,7 +49,7 @@ class TestCompletion(unittest.IsolatedAsyncioTestCase):
             output = response
             completion_tokens += len(response)
 
-        cost = calculate_cost(api_type, model, prompt_tokens, completion_tokens)
+        cost = calculate_cost(chat_request.api_type, chat_request.model, prompt_tokens, completion_tokens)
 
         # Test
         self.assertEqual(output, "This is a test.")
