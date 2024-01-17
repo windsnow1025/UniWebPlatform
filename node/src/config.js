5e34433d3074609ae70cd8d3dad610474df3f693
windsnow1025 <windsnow1025@gmail.com>
2024-01-17 04:54:55 +0000

    build(node): use env instead of config

--- node/src/config.js
+++ node/src/config.js
@@ -1,12 +1,3 @@
-const fs = require('fs');
-const path = require('path');
-
-function loadConfigFromFile() {
-  const configPath = path.join(__dirname, 'config.json');
-  const configFile = fs.readFileSync(configPath, 'utf8');
-  return JSON.parse(configFile);
-}
-
 if (process.env.JWT_SECRET && process.env.MYSQL_ROOT_PASSWORD && process.env.MYSQL_USER && process.env.MYSQL_PASSWORD && process.env.MYSQL_DATABASE) {
   process.env.PORT = 3000;
   process.env.BASE_URL = "/api";
@@ -14,16 +5,11 @@ if (process.env.JWT_SECRET && process.env.MYSQL_ROOT_PASSWORD && process.env.MYS
 
   console.log("Using production setting.")
 } else {
-  const config = loadConfigFromFile();
+  require('dotenv').config();
 
   process.env.PORT = 3001;
   process.env.BASE_URL = "";
-  process.env.JWT_SECRET = config.JWT_SECRET;
   process.env.MYSQL_HOST = "localhost";
-  process.env.MYSQL_ROOT_PASSWORD = config.MYSQL_ROOT_PASSWORD;
-  process.env.MYSQL_USER = config.MYSQL_USER;
-  process.env.MYSQL_PASSWORD = config.MYSQL_PASSWORD;
-  process.env.MYSQL_DATABASE = config.MYSQL_DATABASE;
 
   console.log("Using development setting.")
 }
