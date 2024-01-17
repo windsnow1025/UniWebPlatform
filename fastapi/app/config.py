76f24030415d2f1c16fd5c76935624f2ea9d4b38
windsnow1025 <windsnow1025@gmail.com>
2024-01-17 04:38:43 +0000

    build(fastapi): use env instead of config

--- fastapi/app/config.py
+++ fastapi/app/config.py
@@ -1,37 +1,16 @@
 import os
-import json
 import logging
-
-
-def detect_environment_variables(environment_variables: list[str]):
-    return all(os.getenv(var) is not None for var in environment_variables)
-
-
-def set_environment_from_config(filepath: str):
-    with open(filepath) as config_file:
-        config = json.load(config_file)
-        for key, value in config.items():
-            os.environ[key] = value
+from dotenv import load_dotenv
 
 
 def init_environment():
-    environment_variables = [
-        "OPENAI_API_KEY",
-        "AZURE_API_KEY",
-        "AZURE_API_BASE",
-        "JWT_SECRET",
-        "MYSQL_ROOT_PASSWORD",
-        "MYSQL_USER",
-        "MYSQL_PASSWORD",
-        "MYSQL_DATABASE"
-    ]
 
-    if detect_environment_variables(environment_variables):
+    if os.getenv("OPENAI_API_KEY"):
         os.environ["MYSQL_HOST"] = "mysql"
 
         logging.info("Using production setting.")
     else:
+        load_dotenv()
         os.environ["MYSQL_HOST"] = "localhost"
-        set_environment_from_config(os.path.join(os.path.dirname(os.path.abspath(__file__)), "config.json"))
 
         logging.info("Using development setting.")
