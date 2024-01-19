f968bed7d6c75749abbec55a33d0e0b752832b77
windsnow1025 <windsnow1025@gmail.com>
2024-01-19 16:21:07 +0000

    feat(next): combine signin signup

--- next/pages/user-sign.js
+++ next/pages/user-sign.js
@@ -7,11 +7,11 @@ import {UserLogic} from "../src/logic/UserLogic";
 import ThemeSelect from '../app/components/ThemeSelect';
 import {getInitMUITheme, getLightMUITheme} from "../src/logic/ThemeLogic";
 import {ThemeProvider} from "@mui/material/styles";
-import {AppBar, Button, Toolbar} from "@mui/material";
+import {AppBar, Button} from "@mui/material";
 import TextField from "@mui/material/TextField";
-import AuthDiv from "../app/components/AuthDiv";
+import Snackbar from "@mui/material/Snackbar";
 
-function SignIn() {
+function UserSign() {
   const [theme, setTheme] = useState(getLightMUITheme());
 
   useEffect(() => {
@@ -22,19 +22,55 @@ function SignIn() {
     setTheme(getInitMUITheme());
   }, []);
 
+  const [action, setAction] = useState('');
+  const [title, setTitle] = useState('');
+
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   const router = useRouter();
   const userLogic = new UserLogic();
 
   useEffect(() => {
-    document.title = "Sign In";
-  }, []);
+    setAction(router.query.action);
+  }, [router.query.action]);
+
+  useEffect(() => {
+    if (action === 'signin') {
+      setTitle("Sign In");
+    }
+    if (action === 'signup') {
+      setTitle("Sign Up");
+    }
+  }, [action]);
+
+  const [alertOpen, setAlertOpen] = useState(false);
+  const [alertMessage, setAlertMessage] = useState('');
 
   const handleSignIn = async () => {
-    if (await userLogic.signIn(username, password)) {
-      let prevUrl = localStorage.getItem('prevUrl') || "/";
+    try {
+      await userLogic.signIn(username, password)
+      const prevUrl = localStorage.getItem('prevUrl') || "/";
       router.push(prevUrl);
+    } catch (e) {
+      setAlertMessage(e.message);
+      setAlertOpen(true);
+    }
+  };
+
+  const handleSignUp = async () => {
+    if (!userLogic.validateInput(username) || !userLogic.validateInput(password)) {
+      setAlertMessage("Username or Password contains invalid characters or has an invalid length.");
+      setAlertOpen(true);
+      return;
+    }
+    try {
+      await userLogic.signUp(username, password);
+      setAlertMessage("Sign up successful.");
+      setAlertOpen(true);
+      setAction('signin');
+    } catch (e) {
+      setAlertMessage(e.message);
+      setAlertOpen(true);
     }
   };
 
@@ -42,7 +78,7 @@ function SignIn() {
     <ThemeProvider theme={theme}>
       <AppBar position="static" color="secondary">
         <div className="Flex-space-around p-2">
-          <h1 className="grow">Sign In</h1>
+          <h1 className="grow">{title}</h1>
           <div className="m-1"><ThemeSelect/></div>
         </div>
       </AppBar>
@@ -69,12 +105,18 @@ function SignIn() {
             />
           </div>
           <div className="m-2">
-            <Button variant="contained" onClick={handleSignIn}>Sign In</Button>
+            <Button variant="contained" onClick={action === 'signin' ? handleSignIn : handleSignUp}>{title}</Button>
           </div>
         </div>
       </div>
+      <Snackbar
+        open={alertOpen}
+        autoHideDuration={6000}
+        onClose={() => setAlertOpen(false)}
+        message={alertMessage}
+      />
     </ThemeProvider>
   );
 }
 
-export default SignIn;
+export default UserSign;
