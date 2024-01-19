51b90a5acfa2d52ecf6880a0e78860000180bdc1
windsnow1025 <windsnow1025@gmail.com>
2024-01-19 14:06:45 +0000

    feat(next): confirm dialog for custom autocomplete

---
+++ next/app/components/ConfirmDialog.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import Button from '@mui/material/Button';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+
+export default function ConfirmDialog({ open, handleClose, title, content }) {
+  return (
+    <Dialog
+      open={open}
+      onClose={() => handleClose(false)}
+      aria-labelledby="confirm-dialog-title"
+      aria-describedby="confirm-dialog-description"
+    >
+      <DialogTitle id="confirm-dialog-title">
+        {title}
+      </DialogTitle>
+      <DialogContent>
+        <DialogContentText id="confirm-dialog-description">
+          {content}
+        </DialogContentText>
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={() => handleClose(false)}>Cancel</Button>
+        <Button onClick={() => handleClose(true)} color="primary" autoFocus>
+          Confirm
+        </Button>
+      </DialogActions>
+    </Dialog>
+  );
+}
