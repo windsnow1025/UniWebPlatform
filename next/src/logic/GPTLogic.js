247d72afcc24c0c525586d5bbdd1635596e3d532
windsnow1025 <windsnow1025@gmail.com>
2024-01-26 11:56:55 +0000

    feat(next): get gpt models from backend

--- next/src/logic/GPTLogic.js
+++ next/src/logic/GPTLogic.js
@@ -17,87 +17,26 @@ export class GPTLogic {
       }
     ];
 
-    this.models = {
-      "open_ai": [
-        "gpt-3.5-turbo",
-        "gpt-3.5-turbo-0301",
-        "gpt-3.5-turbo-0613",
-        "gpt-3.5-turbo-1106",
-        "gpt-3.5-turbo-0125",
-        "gpt-3.5-turbo-16k",
-        "gpt-3.5-turbo-16k-0613",
-        "gpt-4",
-        "gpt-4-0314",
-        "gpt-4-0613",
-        "gpt-4-32k-0314",
-        "gpt-4-turbo-preview",
-        "gpt-4-1106-preview",
-        "gpt-4-0125-preview",
-        "gpt-4-vision-preview"
-      ],
-      "azure": [
-        "gpt-35-turbo",
-        "gpt-35-turbo-16k",
-        "gpt-4",
-        "gpt-4-32k"
-      ]
-    }
-
     this.defaultApiType = "open_ai";
-    this.defaultModel = "gpt-4-turbo-preview";
+    this.defaultModel = [
+      {"api_type": "open_ai", "model": "gpt-4-turbo-preview"},
+      {"api_type": "azure", "model": "gpt-4"}
+    ]
   }
 
-
-  // Save the messages array as a JSON file
-  export(messages) {
-    const fileName = 'messages.json';
-    const data = JSON.stringify(messages);
-    const blob = new Blob([data], {type: 'application/json'});
-    const url = URL.createObjectURL(blob);
-
-    const a = document.createElement('a');
-    a.href = url;
-    a.download = fileName;
-    a.style.display = 'none';
-    document.body.appendChild(a);
-    a.click();
-    document.body.removeChild(a);
-
-    URL.revokeObjectURL(url);
+  async fetchModels() {
+    try {
+      return await this.gptService.fetchModels();
+    } catch (err) {
+      console.error("Error in GET /models:", err);
+      return "Error occurred while fetching models.";
+    }
   }
 
-  // Load the messages array from a JSON file
-  import() {
-    return new Promise((resolve, reject) => {
-      // Request a JSON file from the user
-      const input = document.createElement('input');
-      input.type = 'file';
-      input.accept = 'application/json';
-      input.onchange = e => {
-        // Get the file
-        const file = e.target.files[0];
-
-        // Read the file
-        const reader = new FileReader();
-        reader.readAsText(file, "UTF-8");
-        reader.onload = readerEvent => {
-          const content = readerEvent.target.result;
-
-          // Parse the JSON file
-          const messages = JSON.parse(content);
-
-          // Resolve the promise with the messages
-          resolve(messages);
-        }
-
-        // Reject the promise if there's an error
-        reader.onerror = error => reject(error);
-      }
-      input.click();
-
-      // Scroll to the bottom of the page
-      window.scrollTo(0, document.body.scrollHeight);
-    });
+  getModels(models, apiType) {
+    return models
+      .filter(model => model.api_type === apiType)
+      .map(model => model.model);
   }
 
   async nonStreamGenerate(messages, api_type, model, temperature, stream) {
@@ -166,5 +105,58 @@ export class GPTLogic {
     });
 
     return contentArray;
+  }
+
+
+  // Save the messages array as a JSON file
+  export(messages) {
+    const fileName = 'messages.json';
+    const data = JSON.stringify(messages);
+    const blob = new Blob([data], {type: 'application/json'});
+    const url = URL.createObjectURL(blob);
+
+    const a = document.createElement('a');
+    a.href = url;
+    a.download = fileName;
+    a.style.display = 'none';
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
+
+    URL.revokeObjectURL(url);
+  }
+
+  // Load the messages array from a JSON file
+  import() {
+    return new Promise((resolve, reject) => {
+      // Request a JSON file from the user
+      const input = document.createElement('input');
+      input.type = 'file';
+      input.accept = 'application/json';
+      input.onchange = e => {
+        // Get the file
+        const file = e.target.files[0];
+
+        // Read the file
+        const reader = new FileReader();
+        reader.readAsText(file, "UTF-8");
+        reader.onload = readerEvent => {
+          const content = readerEvent.target.result;
+
+          // Parse the JSON file
+          const messages = JSON.parse(content);
+
+          // Resolve the promise with the messages
+          resolve(messages);
+        }
+
+        // Reject the promise if there's an error
+        reader.onerror = error => reject(error);
+      }
+      input.click();
+
+      // Scroll to the bottom of the page
+      window.scrollTo(0, document.body.scrollHeight);
+    });
   }
 }
