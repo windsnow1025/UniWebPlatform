57b2cfdcabff643a29529f051b0a272973adcc74
windsnow1025 <windsnow1025@gmail.com>
2024-01-26 07:36:20 +0000

    feat: update gpt models

--- fastapi/app/util/pricing.py
+++ fastapi/app/util/pricing.py
@@ -4,16 +4,19 @@ ModelPricing = dict[str, dict[str, PricingDetails]]
 
 model_pricing: ModelPricing = {
     "open_ai": {
-        "gpt-3.5-turbo": {"input": 0.0010, "output": 0.0020},
+        "gpt-3.5-turbo": {"input": 0.0005, "output": 0.0015},
         "gpt-3.5-turbo-0301": {"input": 0.0015, "output": 0.0020},
         "gpt-3.5-turbo-0613": {"input": 0.0015, "output": 0.0020},
         "gpt-3.5-turbo-1106": {"input": 0.0010, "output": 0.0020},
+        "gpt-3.5-turbo-0125": {"input": 0.0005, "output": 0.0015},
         "gpt-3.5-turbo-16k": {"input": 0.0030, "output": 0.0040},
         "gpt-3.5-turbo-16k-0613": {"input": 0.0030, "output": 0.0040},
         "gpt-4": {"input": 0.03, "output": 0.06},
         "gpt-4-0314": {"input": 0.03, "output": 0.06},
         "gpt-4-0613": {"input": 0.03, "output": 0.06},
+        "gpt-4-turbo-preview": {"input": 0.01, "output": 0.03},
         "gpt-4-1106-preview": {"input": 0.01, "output": 0.03},
+        "gpt-4-0125-preview": {"input": 0.01, "output": 0.03},
         "gpt-4-vision-preview": {"input": 0.01, "output": 0.03}
     },
     "azure": {
