80c0dc413c73feee071c021c20fa4c2d09ac3124
windsnow1025 <windsnow1025@gmail.com>
2024-03-26 03:35:37 +0000

    test(selenium): path as class member

--- selenium/user.py
+++ selenium/user.py
@@ -1,6 +1,5 @@
-from selenium.common import NoSuchElementException
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.support.expected_conditions import *
 from selenium.webdriver.support.wait import WebDriverWait
 
 
@@ -8,28 +7,27 @@ class User:
     def __init__(self, webdriver):
         self.driver = webdriver
         self.wait = WebDriverWait(self.driver, timeout=1)
+        self.sign_in_button_path = "//button[normalize-space(text())='Sign In']"
+        self.user_center_link_path = "//a[@href='/user-center']"
+        self.username_input_path = "//label[text()='Username']/following-sibling::div//input"
+        self.password_input_path = "//label[text()='Password']/following-sibling::div//input"
+        self.sign_out_link_path = "//a[normalize-space(text())='Sign Out']"
 
     def is_signed_in(self):
         try:
-            self.wait.until(expected_conditions.presence_of_element_located((
-                By.XPATH, "//button[normalize-space(text())='Sign In']"
-            )))
+            self.wait.until(presence_of_element_located((By.XPATH, self.sign_in_button_path)))
             return False
         except NoSuchElementException:
-            self.driver.find_element(By.XPATH, "//a[@href='/user-center']")
+            self.driver.find_element(By.XPATH, self.user_center_link_path)
             return True
 
     def sign_in(self):
-        self.wait.until(expected_conditions.presence_of_element_located((
-            By.XPATH, "//button[normalize-space(text())='Sign In']"
-        ))).click()
-        username_input = self.wait.until(expected_conditions.presence_of_element_located((
-            By.XPATH, "//label[text()='Username']/following-sibling::div//input"
-        )))
+        self.wait.until(presence_of_element_located((By.XPATH, self.sign_in_button_path))).click()
+        username_input = self.wait.until(presence_of_element_located((By.XPATH, self.username_input_path)))
         username_input.send_keys("test")
-        password_input = self.driver.find_element(By.XPATH, "//label[text()='Password']/following-sibling::div//input")
+        password_input = self.driver.find_element(By.XPATH, self.password_input_path)
         password_input.send_keys("test")
-        self.driver.find_element(By.XPATH, "//button[normalize-space(text())='Sign In']").click()
+        self.driver.find_element(By.XPATH, self.sign_in_button_path).click()
 
     def sign_out(self):
-        self.driver.find_element(By.XPATH, "//a[normalize-space(text())='Sign Out']").click()
+        self.driver.find_element(By.XPATH, self.sign_out_link_path).click()
