56f91a55798d233d4bad20db317e35c23cf75bec
windsnow1025 <windsnow1025@gmail.com>
2024-03-27 21:40:26 -0700

    feat(fastapi): handler injection

--- fastapi/app/logic/completion/request_handler.py
+++ fastapi/app/logic/completion/request_handler.py
@@ -1,22 +1,12 @@
-import logging
+from typing import Callable
 
-from app.api.completion_router import ChatRequest
-from app.dao import user_dao
-from app.util import pricing
+from app.model.message import Message
 
 
 def handle_request(
-        username: str,
-        chat_request: ChatRequest
+        messages: list[Message],
+        reduce_credit: Callable[[int], None]
 ) -> None:
-    prompt_tokens = sum(len(message["content"]) for message in chat_request.messages)
-    user_dao.reduce_credit(
-        username=username,
-        cost=pricing.calculate_cost(
-            api_type=chat_request.api_type,
-            model=chat_request.model,
-            prompt_tokens=prompt_tokens,
-            completion_tokens=0)
-    )
-    logging.info(f"username: {username}, model: {chat_request.model}")
+    prompt_tokens = sum(len(message["content"]) for message in messages)
+    reduce_credit(prompt_tokens)
     return
