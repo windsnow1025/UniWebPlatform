0c8ea8966bfb5be8ed21f0cb585572432fd5d47e
windsnow1025 <windsnow1025@gmail.com>
2024-03-27 21:40:26 -0700

    feat(fastapi): handler injection

--- fastapi/app/logic/completion/response_handler.py
+++ fastapi/app/logic/completion/response_handler.py
@@ -3,47 +3,27 @@ from typing import Callable, Generator
 
 from fastapi.responses import StreamingResponse
 
-from app.api.completion_router import ChatRequest
-from app.dao import user_dao
-from app.util import pricing
-
 
 def non_stream_handler(
         content: str,
-        username: str,
-        chat_request: ChatRequest
+        reduce_credit: Callable[[str], None]
 ) -> str:
-    user_dao.reduce_credit(
-        username=username,
-        cost=pricing.calculate_cost(
-            api_type=chat_request.api_type,
-            model=chat_request.model,
-            prompt_tokens=0,
-            completion_tokens=len(content)
-        )
-    )
+    reduce_credit(content)
     logging.info(f"content: {content}")
     return content
 
 
 def stream_handler(
         generator_function: Callable[[], Generator[str, None, None]],
-        username: str,
-        chat_request: ChatRequest
+        reduce_credit: Callable[[int], None]
 ) -> StreamingResponse:
     def wrapper_generator() -> Generator[str, None, str]:
         content = ""
         for chunk in generator_function():
             content += chunk
             yield chunk
-        user_dao.reduce_credit(
-            username=username,
-            cost=pricing.calculate_cost(
-                api_type=chat_request.api_type,
-                model=chat_request.model,
-                prompt_tokens=0,
-                completion_tokens=len(content))
-        )
+        completion_tokens = len(content)
+        reduce_credit(completion_tokens)
         logging.info(f"content: {content}")
         return content
 
