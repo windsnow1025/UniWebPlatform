6e7f0a564909197395cb14b9f67880810528adaf
windsnow1025 <windsnow1025@gmail.com>
2024-04-02 13:46:00 +0000

    test(selenium): scraper

---
+++ selenium/scraper.py
@@ -0,0 +1,43 @@
+from selenium import webdriver
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.expected_conditions import *
+from selenium.webdriver.remote.webdriver import WebDriver
+from selenium.webdriver.remote.webelement import WebElement
+
+
+class Scraper:
+    def __init__(self, url: str):
+        self.driver = webdriver.Chrome()
+        self.driver.get(url)
+
+    def _wait(
+            self,
+            element: WebDriver | WebElement = None,
+            timeout: float = 2
+    ) -> WebDriverWait:
+        if element is None:
+            element = self.driver
+        return WebDriverWait(element, timeout=timeout)
+
+    def _wait_find(
+            self,
+            path: str,
+            element: WebDriver | WebElement = None,
+            find_all: bool = False
+    ) -> WebElement | list[WebElement]:
+        if element is None:
+            element = self.driver
+        if find_all is False:
+            return self._wait(element).until(presence_of_element_located((By.XPATH, path)))
+        if find_all is True:
+            return self._wait(element).until(presence_of_all_elements_located((By.XPATH, path)))
+
+    def _wait_for_staleness(
+            self,
+            element: WebDriver | WebElement = None,
+            timeout: float = 2
+    ):
+        if element is None:
+            element = self.driver
+        WebDriverWait(self.driver, timeout).until(staleness_of(element))
--- selenium/user.py
+++ selenium/user.py
@@ -1,33 +1,33 @@
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.expected_conditions import *
+from selenium.common import TimeoutException
 from selenium.webdriver.support.wait import WebDriverWait
 
+from scraper import Scraper
 
-class User:
-    def __init__(self, webdriver):
-        self.driver = webdriver
+
+class User(Scraper):
+    def __init__(self, url):
+        super().__init__(url)
+
         self.wait = WebDriverWait(self.driver, timeout=1)
         self.sign_in_button_path = "//button[normalize-space(text())='Sign In']"
-        self.user_center_link_path = "//a[@href='/user-center']"
+        self.user_account_link_path = "//a[@href='/user/account']"
         self.username_input_path = "//label[text()='Username']/following-sibling::div//input"
         self.password_input_path = "//label[text()='Password']/following-sibling::div//input"
         self.sign_out_link_path = "//a[normalize-space(text())='Sign Out']"
 
     def is_signed_in(self):
         try:
-            self.wait.until(presence_of_element_located((By.XPATH, self.sign_in_button_path)))
+            self._wait_find(self.sign_in_button_path)
             return False
-        except NoSuchElementException:
-            self.driver.find_element(By.XPATH, self.user_center_link_path)
+        except TimeoutException:
+            self._wait_find(self.user_account_link_path)
             return True
 
     def sign_in(self):
-        self.wait.until(presence_of_element_located((By.XPATH, self.sign_in_button_path))).click()
-        username_input = self.wait.until(presence_of_element_located((By.XPATH, self.username_input_path)))
-        username_input.send_keys("test")
-        password_input = self.driver.find_element(By.XPATH, self.password_input_path)
-        password_input.send_keys("test")
-        self.driver.find_element(By.XPATH, self.sign_in_button_path).click()
+        self._wait_find(self.sign_in_button_path).click()
+        self._wait_find(self.username_input_path).send_keys("test")
+        self._wait_find(self.password_input_path).send_keys("test")
+        self._wait_find(self.sign_in_button_path).click()
 
     def sign_out(self):
-        self.driver.find_element(By.XPATH, self.sign_out_link_path).click()
+        self._wait_find(self.sign_out_link_path).click()
