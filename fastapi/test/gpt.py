7d40f55cb2e24c4068cccc86b5a0bb900399931f
windsnow1025 <windsnow1025@gmail.com>
2024-04-03 04:59:30 +0000

    test(fastapi): test for stream & nonstream

--- fastapi/test/gpt.py
+++ fastapi/test/gpt.py
@@ -11,7 +11,7 @@ class TestChat(unittest.IsolatedAsyncioTestCase):
     def setUp(self):
         init_environment()
 
-    async def test_chat(self):
+    async def test_openai_stream(self):
         username = "test"
         messages = [
             {
@@ -55,13 +55,57 @@ class TestChat(unittest.IsolatedAsyncioTestCase):
         response = processor.process_request()
 
         output = ""
-        if stream:
-            async for content in response.body_iterator:
-                output += content
-                print(content, end="")
-        else:
-            output = response
-            print(response)
+        async for content in response.body_iterator:
+            output += content
+            print(content, end="")
 
         # Test
+        self.assertEqual(output, "This is a test.")
+
+    def test_openai_nonstream(self):
+        username = "test"
+        messages = [
+            {
+                "role": "user",
+                "content": "Say this is a test."
+            }
+        ]
+        model = "gpt-4"
+        api_type = "open_ai"
+        temperature = 0
+        stream = False
+
+        def reduce_credit(prompt_tokens: int, completion_tokens: int) -> float:
+            cost = pricing.calculate_cost(
+                api_type=api_type,
+                model=model,
+                prompt_tokens=prompt_tokens,
+                completion_tokens=completion_tokens
+            )
+            user_dao.reduce_credit(
+                username=username,
+                cost=cost
+            )
+            return cost
+
+        processor = create_chat_processor(
+            messages=messages,
+            model=model,
+            api_type=api_type,
+            temperature=temperature,
+            stream=stream,
+            non_stream_response_handler=lambda content: non_stream_handler(
+                content,
+                lambda completion_tokens: reduce_credit(prompt_tokens=0, completion_tokens=completion_tokens)
+            ),
+            stream_response_handler=lambda generator_function: stream_handler(
+                generator_function,
+                lambda completion_tokens: reduce_credit(prompt_tokens=0, completion_tokens=completion_tokens)
+            )
+        )
+        response = processor.process_request()
+
+        output = response
+        print(response)
+
         self.assertEqual(output, "This is a test.")
