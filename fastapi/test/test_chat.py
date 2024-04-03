cbf89fc320db6be38f18e2462125932c6eedd263
windsnow1025 <windsnow1025@gmail.com>
2024-04-03 06:12:47 +0000

    test(fastapi): azure test

--- fastapi/test/test_chat.py
+++ fastapi/test/test_chat.py
@@ -57,9 +57,7 @@ class TestChat(unittest.IsolatedAsyncioTestCase):
         output = ""
         async for content in response.body_iterator:
             output += content
-            print(content, end="")
 
-        # Test
         self.assertEqual(output, "This is a test.")
 
     def test_openai_nonstream(self):
@@ -106,6 +104,101 @@ class TestChat(unittest.IsolatedAsyncioTestCase):
         response = processor.process_request()
 
         output = response
-        print(response)
+
+        self.assertEqual(output, "This is a test.")
+
+    async def test_azure_stream(self):
+        username = "test"
+        messages = [
+            {
+                "role": "user",
+                "content": "Say this is a test."
+            }
+        ]
+        model = "gpt-4"
+        api_type = "azure"
+        temperature = 0
+        stream = True
+
+        def reduce_credit(prompt_tokens: int, completion_tokens: int) -> float:
+            cost = pricing.calculate_cost(
+                api_type=api_type,
+                model=model,
+                prompt_tokens=prompt_tokens,
+                completion_tokens=completion_tokens
+            )
+            user_dao.reduce_credit(
+                username=username,
+                cost=cost
+            )
+            return cost
+
+        processor = create_chat_processor(
+            messages=messages,
+            model=model,
+            api_type=api_type,
+            temperature=temperature,
+            stream=stream,
+            non_stream_response_handler=lambda content: non_stream_handler(
+                content,
+                lambda completion_tokens: reduce_credit(prompt_tokens=0, completion_tokens=completion_tokens)
+            ),
+            stream_response_handler=lambda generator_function: stream_handler(
+                generator_function,
+                lambda completion_tokens: reduce_credit(prompt_tokens=0, completion_tokens=completion_tokens)
+            )
+        )
+        response = processor.process_request()
+
+        output = ""
+        async for content in response.body_iterator:
+            output += content
+
+        self.assertEqual(output, "This is a test.")
+
+    def test_azure_nonstream(self):
+        username = "test"
+        messages = [
+            {
+                "role": "user",
+                "content": "Say this is a test."
+            }
+        ]
+        model = "gpt-4"
+        api_type = "azure"
+        temperature = 0
+        stream = False
+
+        def reduce_credit(prompt_tokens: int, completion_tokens: int) -> float:
+            cost = pricing.calculate_cost(
+                api_type=api_type,
+                model=model,
+                prompt_tokens=prompt_tokens,
+                completion_tokens=completion_tokens
+            )
+            user_dao.reduce_credit(
+                username=username,
+                cost=cost
+            )
+            return cost
+
+        processor = create_chat_processor(
+            messages=messages,
+            model=model,
+            api_type=api_type,
+            temperature=temperature,
+            stream=stream,
+            non_stream_response_handler=lambda content: non_stream_handler(
+                content,
+                lambda completion_tokens: reduce_credit(prompt_tokens=0, completion_tokens=completion_tokens)
+            ),
+            stream_response_handler=lambda generator_function: stream_handler(
+                generator_function,
+                lambda completion_tokens: reduce_credit(prompt_tokens=0, completion_tokens=completion_tokens)
+            )
+        )
+        response = processor.process_request()
+
+        output = response
 
         self.assertEqual(output, "This is a test.")
