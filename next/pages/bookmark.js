169bc6e01654a556e348535b1f772d76e1859301
windsnow1025 <windsnow1025@gmail.com>
2024-04-09 07:19:31 +0000

    refactor(next): refactor theme WIP2

--- next/pages/bookmark.js
+++ next/pages/bookmark.js
@@ -1,25 +1,38 @@
 import '../src/asset/css/index.css';
 
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import {ThemeProvider} from '@mui/material/styles';
 import HeaderAppBar from "../app/components/common/HeaderAppBar";
 import BookmarkDataGrid from "../app/components/BookmarkDataGrid";
-import {useTheme} from "../app/hooks/useTheme";
 import {CssBaseline} from "@mui/material";
+import {createMUITheme} from "../app/utils/Theme";
 
 function Bookmark() {
-  const theme = useTheme();
+  const [systemTheme, setSystemTheme] = useState();
+  const [muiTheme, setMuiTheme] = useState();
+
+  useEffect(() => {
+    setMuiTheme(createMUITheme(systemTheme));
+  }, [systemTheme]);
 
   useEffect(() => {
     document.title = "Bookmark";
   }, []);
 
   return (
-    <ThemeProvider theme={theme}>
-      <CssBaseline enableColorScheme />
-      <HeaderAppBar title="Bookmarks"/>
-      <BookmarkDataGrid/>
-    </ThemeProvider>
+    <>
+      {muiTheme &&
+        <ThemeProvider theme={muiTheme}>
+          <CssBaseline enableColorScheme />
+          <HeaderAppBar
+            title="Bookmarks"
+            systemTheme={systemTheme}
+            setSystemTheme={setSystemTheme}
+          />
+          <BookmarkDataGrid/>
+        </ThemeProvider>
+      }
+    </>
   );
 }
 
