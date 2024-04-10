91f4874e223182feadec95e66f02f95d8e7ad07b
windsnow1025 <windsnow1025@gmail.com>
2024-04-10 14:21:44 +0000

    feat(fastapi): gemini multi-turn conversation

--- fastapi/app/logic/chat/processor/gemini/gemini.py
+++ fastapi/app/logic/chat/processor/gemini/gemini.py
@@ -7,47 +7,51 @@ from app.config import init_env
 init_env()
 genai.configure(api_key=os.environ["GOOGLE_AI_STUDIO_API_KEY"])
 
-# Set up the model
+
 generation_config = {
-  "temperature": 0,
-  "top_p": 1,
-  "top_k": 1,
-  "max_output_tokens": 2048,
+    "temperature": 0,
+    "top_p": 1,
+    "top_k": 1,
+    "max_output_tokens": 2048,
 }
 
 safety_settings = [
-  {
-    "category": "HARM_CATEGORY_HARASSMENT",
-    "threshold": "BLOCK_ONLY_HIGH"
-  },
-  {
-    "category": "HARM_CATEGORY_HATE_SPEECH",
-    "threshold": "BLOCK_ONLY_HIGH"
-  },
-  {
-    "category": "HARM_CATEGORY_SEXUALLY_EXPLICIT",
-    "threshold": "BLOCK_ONLY_HIGH"
-  },
-  {
-    "category": "HARM_CATEGORY_DANGEROUS_CONTENT",
-    "threshold": "BLOCK_ONLY_HIGH"
-  },
+    {
+        "category": "HARM_CATEGORY_HARASSMENT",
+        "threshold": "BLOCK_NONE"
+    },
+    {
+        "category": "HARM_CATEGORY_HATE_SPEECH",
+        "threshold": "BLOCK_NONE"
+    },
+    {
+        "category": "HARM_CATEGORY_SEXUALLY_EXPLICIT",
+        "threshold": "BLOCK_NONE"
+    },
+    {
+        "category": "HARM_CATEGORY_DANGEROUS_CONTENT",
+        "threshold": "BLOCK_NONE"
+    },
 ]
 
 model = genai.GenerativeModel(model_name="models/gemini-1.5-pro-latest",
                               generation_config=generation_config,
                               safety_settings=safety_settings)
 
-convo = model.start_chat(history=[
-  {
-    "role": "user",
-    "parts": ["Hello"]
-  },
-  {
-    "role": "model",
-    "parts": ["Hello there! How can I assist you today?"]
-  },
-])
+messages = [
+    {
+        'role': 'user',
+        'parts': ["Briefly explain how a computer works to a young child."]
+    },
+    {
+        'role': 'model',
+        'parts': ["Imagine a computer like a really smart toy box! Inside, there are tiny switches that can be either on or off, like little light switches. These switches work together to follow instructions we give the computer, like showing pictures or playing music. We use a keyboard, mouse, or touchscreen to tell the computer what to do, and it uses those tiny switches to make it happen! It's like magic, but with switches! "]
+    },
+    {
+        'role': 'user',
+        'parts': ["Okay, how about a more detailed explanation to a high school student?"]
+    }
+]
 
-convo.send_message("How are you?")
-print(convo.last.text)
+response = model.generate_content(messages)
+print(response.text)
