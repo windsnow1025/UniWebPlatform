62687efb6273a77c6efd5984df09759865d8e22d
windsnow1025 <windsnow1025@gmail.com>
2024-04-12 07:15:48 -0700

    style(next): enable all tailwind, mui link

--- next/app/components/common/HeaderAppBar.js
+++ next/app/components/common/HeaderAppBar.js
@@ -1,4 +1,4 @@
-import {AppBar} from "@mui/material";
+import {AppBar, Typography} from "@mui/material";
 import AuthDiv from "./AuthDiv";
 import ThemeSelect from "./ThemeSelect";
 import React from "react";
@@ -7,9 +7,9 @@ function HeaderAppBar({ title, useAuthDiv = true, systemTheme, setSystemTheme}) 
     return (
         <AppBar position="static" color="secondary">
             <div className="flex-around p-2">
-                <h1 className="grow">
+                <Typography variant="h4" className="grow">
                   {title}
-                </h1>
+                </Typography>
                 {useAuthDiv &&
                   <div className="m-1">
                     <AuthDiv/>
