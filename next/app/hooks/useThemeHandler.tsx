44e3ccdbe2d876fc34db00b0ea48aae37765abba
windsnow1025 <windsnow1025@gmail.com>
2024-04-15 12:17:28 +0000

    feat(next): encapsulate all theme logic to hook

--- next/app/hooks/useThemeHandler.tsx
+++ next/app/hooks/useThemeHandler.tsx
@@ -1,20 +1,24 @@
-import {useEffect, useState} from 'react';
-import {applyTheme} from "@/app/utils/Theme";
+import { useEffect, useState } from 'react';
+import {applyTheme, createMUITheme} from "@/app/utils/Theme";
+import { Theme } from "@mui/material/styles";
 
 const useThemeHandler = () => {
-  const [systemTheme, setSystemTheme] = useState<string>("");
+  const [systemTheme, setSystemTheme] = useState<string>();
+  const [muiTheme, setMuiTheme] = useState<Theme>();
 
   useEffect(() => {
-    if (!["system", "light", "dark"].includes(systemTheme)) {
-      setSystemTheme(localStorage.getItem("theme") || "system");
+    const storedTheme = localStorage.getItem("theme") || "system";
+    if (!["system", "light", "dark"].includes(systemTheme!!)) {
+      setSystemTheme(storedTheme);
     }
   }, []);
 
   useEffect(() => {
-    applyTheme(systemTheme);
+    applyTheme(systemTheme!!);
+    setMuiTheme(createMUITheme(systemTheme!!));
   }, [systemTheme]);
 
-  return {systemTheme, setSystemTheme};
+  return { systemTheme, setSystemTheme, muiTheme };
 };
 
 export default useThemeHandler;
