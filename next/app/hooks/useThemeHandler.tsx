29de74459b9c37801456bf92d8878afc0eb860e4
windsnow1025 <windsnow1025@gmail.com>
2024-04-15 12:11:43 +0000

    feat(next): improve useThemeHandler hook

--- next/app/hooks/useThemeHandler.tsx
+++ next/app/hooks/useThemeHandler.tsx
@@ -1,20 +1,20 @@
-import { useEffect } from 'react';
-import { applyTheme } from "@/app/utils/Theme";
+import {useEffect, useState} from 'react';
+import {applyTheme} from "@/app/utils/Theme";
 
-interface UseThemeHandlerProps {
-    systemTheme: string | undefined;
-    setSystemTheme: (theme: string) => void;
-}
+const useThemeHandler = () => {
+  const [systemTheme, setSystemTheme] = useState<string>("");
 
-const useThemeHandler = ({ systemTheme, setSystemTheme }: UseThemeHandlerProps) => {
-    useEffect(() => {
-        let currentSystemTheme = systemTheme;
-        if (systemTheme !== "system" && systemTheme !== "light" && systemTheme !== "dark") {
-            currentSystemTheme = localStorage.getItem("theme") || "system";
-            setSystemTheme(currentSystemTheme);
-        }
-        applyTheme(currentSystemTheme!!);
-    }, [systemTheme, setSystemTheme]);
+  useEffect(() => {
+    if (!["system", "light", "dark"].includes(systemTheme)) {
+      setSystemTheme(localStorage.getItem("theme") || "system");
+    }
+  }, []);
+
+  useEffect(() => {
+    applyTheme(systemTheme);
+  }, [systemTheme]);
+
+  return {systemTheme, setSystemTheme};
 };
 
 export default useThemeHandler;
