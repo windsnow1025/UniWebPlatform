8dca3cc5ad4a3fc70450e61384b7963385e79c17
windsnow1025 <windsnow1025@gmail.com>
2024-04-15 13:03:14 +0000

    refactor(next): ts markdown parser

--- next/src/util/MarkdownParser.test.js
+++ next/src/util/MarkdownParser.test.js
@@ -1,30 +1,30 @@
 import {parseMarkdown} from "./MarkdownParser";
 
 describe('parseMarkdown', () => {
-  it('should parse plain text correctly', () => {
+  it('should parse plain text correctly', async () => {
     const input = '&lt;div&gt;&amp;&lt;/div&gt;';
-    const output = parseMarkdown(input);
+    const output = await parseMarkdown(input);
     const expectedOutput = '<p>&lt;div&gt;&amp;&lt;/div&gt;</p>\n';
     expect(output).toBe(expectedOutput);
   });
 
-  it('should parse inline code correctly', () => {
+  it('should parse inline code correctly', async () => {
     const input = '`&lt;div&gt;&amp;&lt;/div&gt;`';
-    const output = parseMarkdown(input);
+    const output = await parseMarkdown(input);
     const expectedOutput = '<p><code>&lt;div&gt;&amp;&lt;/div&gt;</code></p>\n';
     expect(output).toBe(expectedOutput);
   });
 
-  it('should parse code block correctly', () => {
+  it('should parse code block correctly', async () => {
     const input = '```\n&lt;div&gt;&amp;&lt;/div&gt;\n```';
-    const output = parseMarkdown(input);
+    const output = await parseMarkdown(input);
     const expectedOutput = '<pre><code>&lt;div&gt;&amp;&lt;/div&gt;\n</code></pre>';
     expect(output).toBe(expectedOutput);
   });
 
-  it('should parse HTML code block correctly', () => {
+  it('should parse HTML code block correctly', async () => {
     const input = '```html\n&lt;div&gt;&amp;&lt;/div&gt;\n```';
-    const output = parseMarkdown(input);
+    const output = await parseMarkdown(input);
     const expectedOutput = '<pre><code class="hljs language-html"><span class="hljs-symbol">&lt;</span>div<span class="hljs-symbol">&gt;</span><span class="hljs-symbol">&amp;</span><span class="hljs-symbol">&lt;</span>/div<span class="hljs-symbol">&gt;</span>\n</code></pre>';
     expect(output).toBe(expectedOutput);
   });
--- next/src/util/MarkdownParser.ts
+++ next/src/util/MarkdownParser.ts
@@ -14,9 +14,9 @@ const marked = new Marked(
 );
 
 
-export function parseMarkdown(content, sanitize=true) {
+export async function parseMarkdown(content: string, sanitize = true) {
   if (sanitize) {
-    const addLaTeXEscape = (text) => {
+    const addLaTeXEscape = (text: string) => {
       return text
         .replace(/\\\[/g, '\\\\\[')
         .replace(/\\\]/g, '\\\\\]')
@@ -24,26 +24,30 @@ export function parseMarkdown(content, sanitize=true) {
         .replace(/\\\)/g, '\\\\\)');
     };
     const escapedContent = addLaTeXEscape(content);
-    const decodeEntitiesInParsedCode = function (html) {
+    const decodeEntitiesInParsedCode = function (text: string) {
       // Use "\S\s" instead of "." to match newlines
-      return html.replace(/<code([^>]*?)>([\S\s]*?)<\/code>/g, function (match, p1, p2) {
+      return text.replace(/<code([^>]*?)>([\S\s]*?)<\/code>/g, function (match, p1, p2) {
         return `<code${p1}>${p2.replace(/&amp;/g, "&")}</code>`;
       });
     };
 
-    const parsedContent = marked.parse(escapedContent);
+    const parsedContent = await marked.parse(escapedContent);
     const decodedContent = decodeEntitiesInParsedCode(parsedContent);
     return decodedContent;
   } else {
-    const deSanitize = function (html) {
-      const map = {
+    const deSanitize = function (text: string) {
+      const deSanitizeMap = {
         '&amp;': '&',
         '&lt;': '<',
         '&gt;': '>',
         '&quot;': '"',
         '&#039;': "'"
       };
-      return html.replace(/&amp;|&lt;|&gt;|&quot;|&#039;/g, function(m) { return map[m]; });
+      return text.replace(
+        /&amp;|&lt;|&gt;|&quot;|&#039;/g,
+        (matchedEntity) => {
+          return deSanitizeMap[matchedEntity as keyof typeof deSanitizeMap];
+        });
     }
 
     const deSanitizedContent = deSanitize(content);
