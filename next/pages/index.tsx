68c836e062e7566b1a20a3324fb716ab548c4701
windsnow1025 <windsnow1025@gmail.com>
2024-04-15 11:58:46 +0000

    feat(next): useThemeHandler

--- next/pages/index.tsx
+++ next/pages/index.tsx
@@ -9,14 +9,15 @@ import LinkIcon from "@mui/icons-material/Link";
 import HeaderAppBar from "../app/components/common/HeaderAppBar";
 import {CssBaseline, Link} from "@mui/material";
 import {createMUITheme} from "@/app/utils/Theme";
-import ThemeHandler from "@/app/components/common/ThemeHandler";
+import useThemeHandler from "@/app/hooks/useThemeHandler";
 
 function Index() {
   const [systemTheme, setSystemTheme] = useState<string>();
+  useThemeHandler({systemTheme, setSystemTheme});
   const [muiTheme, setMuiTheme] = useState<Theme>();
 
   useEffect(() => {
-    if (!systemTheme) {
+    if (systemTheme === undefined) {
       return;
     }
     const muiTheme = createMUITheme(systemTheme);
@@ -29,51 +30,44 @@ function Index() {
 
   return (
     <>
-      {
-        muiTheme ? (
-            <ThemeProvider theme={muiTheme}>
-              <CssBaseline enableColorScheme />
-              <HeaderAppBar
-                  title="Windsnow1025"
-                  systemTheme={systemTheme!!}
-                  setSystemTheme={setSystemTheme}
-              />
-              <div className="flex-around m-4">
-                <Link href="/bookmark" target="_blank" rel="noopener noreferrer">
-                  <div className="flex-center">
-                    Bookmarks
-                    <LinkIcon/>
-                  </div>
-                </Link>
-                <Link href="/message" target="_blank" rel="noopener noreferrer">
-                  <div className="flex-center">
-                    Message Transmitter
-                    <LinkIcon/>
-                  </div>
-                </Link>
-                <Link href="/password" target="_blank" rel="noopener noreferrer">
-                  <div className="flex-center">
-                    Password Generator
-                    <LinkIcon/>
-                  </div>
-                </Link>
-                <Link href="/chat" target="_blank" rel="noopener noreferrer">
-                  <div className="flex-center">
-                    AI Chat
-                    <LinkIcon/>
-                  </div>
-                </Link>
+      {muiTheme &&
+        <ThemeProvider theme={muiTheme!!}>
+          <CssBaseline enableColorScheme/>
+          <HeaderAppBar
+            title="Windsnow1025"
+            systemTheme={systemTheme!!}
+            setSystemTheme={setSystemTheme}
+          />
+          <div className="flex-around m-4">
+            <Link href="/bookmark" target="_blank" rel="noopener noreferrer">
+              <div className="flex-center">
+                Bookmarks
+                <LinkIcon/>
               </div>
-              <div className="mx-16">
-                <MarkdownList/>
+            </Link>
+            <Link href="/message" target="_blank" rel="noopener noreferrer">
+              <div className="flex-center">
+                Message Transmitter
+                <LinkIcon/>
               </div>
-            </ThemeProvider>
-        ) : (
-            <ThemeHandler
-                systemTheme={systemTheme!!}
-                setSystemTheme={setSystemTheme}
-            />
-        )
+            </Link>
+            <Link href="/password" target="_blank" rel="noopener noreferrer">
+              <div className="flex-center">
+                Password Generator
+                <LinkIcon/>
+              </div>
+            </Link>
+            <Link href="/chat" target="_blank" rel="noopener noreferrer">
+              <div className="flex-center">
+                AI Chat
+                <LinkIcon/>
+              </div>
+            </Link>
+          </div>
+          <div className="mx-16">
+            <MarkdownList/>
+          </div>
+        </ThemeProvider>
       }
     </>
   );
