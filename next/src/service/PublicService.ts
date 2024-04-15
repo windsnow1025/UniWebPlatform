28a11e771beb91fbdd3119944cb91edb898be0ab
windsnow1025 <windsnow1025@gmail.com>
2024-04-15 13:28:50 +0000

    feat(next): public service ts

--- next/src/service/PublicService.ts
+++ next/src/service/PublicService.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export default class PublicService {
-  async fetchMarkdown(filename) {
+  async fetchMarkdown(filename: string): Promise<string> {
     const response = await axios.get(`/markdown/${filename}`);
     return response.data;
   }
