8ff03f608dac4703ec046b13edb73f59b3da4fa5
windsnow1025 <windsnow1025@gmail.com>
2024-04-15 12:26:00 +0000

    style(next): complete theme refactor

--- next/pages/bookmark.js
+++ next/pages/bookmark.js
@@ -1,19 +1,14 @@
 import '../src/asset/css/index.css';
 
-import React, {useEffect, useState} from 'react';
+import React, {useEffect} from 'react';
 import {ThemeProvider} from '@mui/material/styles';
 import HeaderAppBar from "../app/components/common/HeaderAppBar";
 import BookmarkDataGrid from "../app/components/BookmarkDataGrid";
 import {CssBaseline} from "@mui/material";
-import {createMUITheme} from "../app/utils/Theme";
+import useThemeHandler from "../app/hooks/useThemeHandler";
 
 function Bookmark() {
-  const [systemTheme, setSystemTheme] = useState();
-  const [muiTheme, setMuiTheme] = useState();
-
-  useEffect(() => {
-    setMuiTheme(createMUITheme(systemTheme));
-  }, [systemTheme]);
+  const {systemTheme, setSystemTheme, muiTheme} = useThemeHandler();
 
   useEffect(() => {
     document.title = "Bookmark";
