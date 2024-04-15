a58965b8fe6f0034751785633da840e597b8e971
windsnow1025 <windsnow1025@gmail.com>
2024-04-15 00:52:16 -0700

    feat(next): ts theme

---
+++ next/pages/index.tsx
@@ -0,0 +1,82 @@
+'use client';
+
+import '../src/asset/css/index.css';
+
+import MarkdownList from '../app/components/MarkdownList';
+import {Theme, ThemeProvider} from "@mui/material/styles";
+import React, {useEffect, useState} from "react";
+import LinkIcon from "@mui/icons-material/Link";
+import HeaderAppBar from "../app/components/common/HeaderAppBar";
+import {CssBaseline, Link} from "@mui/material";
+import {createMUITheme} from "@/app/utils/Theme";
+import ThemeHandler from "@/app/components/common/ThemeHandler";
+
+function Index() {
+  const [systemTheme, setSystemTheme] = useState<string>();
+  const [muiTheme, setMuiTheme] = useState<Theme>();
+
+  useEffect(() => {
+    if (!systemTheme) {
+      return;
+    }
+    const muiTheme = createMUITheme(systemTheme);
+    setMuiTheme(muiTheme);
+  }, [systemTheme]);
+
+  useEffect(() => {
+    document.title = "Windsnow1025";
+  }, []);
+
+  return (
+    <>
+      {
+        muiTheme ? (
+            <ThemeProvider theme={muiTheme}>
+              <CssBaseline enableColorScheme />
+              <HeaderAppBar
+                  title="Windsnow1025"
+                  systemTheme={systemTheme!!}
+                  setSystemTheme={setSystemTheme}
+              />
+              <div className="flex-around m-4">
+                <Link href="/bookmark" target="_blank" rel="noopener noreferrer">
+                  <div className="flex-center">
+                    Bookmarks
+                    <LinkIcon/>
+                  </div>
+                </Link>
+                <Link href="/message" target="_blank" rel="noopener noreferrer">
+                  <div className="flex-center">
+                    Message Transmitter
+                    <LinkIcon/>
+                  </div>
+                </Link>
+                <Link href="/password" target="_blank" rel="noopener noreferrer">
+                  <div className="flex-center">
+                    Password Generator
+                    <LinkIcon/>
+                  </div>
+                </Link>
+                <Link href="/chat" target="_blank" rel="noopener noreferrer">
+                  <div className="flex-center">
+                    AI Chat
+                    <LinkIcon/>
+                  </div>
+                </Link>
+              </div>
+              <div className="mx-16">
+                <MarkdownList/>
+              </div>
+            </ThemeProvider>
+        ) : (
+            <ThemeHandler
+                systemTheme={systemTheme!!}
+                setSystemTheme={setSystemTheme}
+            />
+        )
+      }
+    </>
+  );
+}
+
+export default Index;
