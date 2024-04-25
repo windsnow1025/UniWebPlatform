1466613e6185ae3c7bf5fab2d5ee2d2e88c1dfa6
windsnow1025 <windsnow1025@gmail.com>
2024-04-25 09:43:59 -0700

    refactor!(next): chat api update

--- next/src/model/Message.ts
+++ next/src/model/Message.ts
@@ -1,21 +1,5 @@
-interface ImageURL {
-    url: string;
-}
-
-export type Content = Array<TextContent | ImageContent>
-
-interface TextContent {
-    type: 'text';
-    text: string;
-}
-
-interface ImageContent {
-    type: 'image_url';
-    image_url: ImageURL;
-}
-
 export interface Message {
-    role: string;
-    content: string | Content;
-    files?: string[] | undefined
+    role: string
+    text: string
+    files: string[]
 }
