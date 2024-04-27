40d85b0d2b3369e07e03ab730d2f9feb3725a0bf
windsnow1025 <windsnow1025@gmail.com>
2024-04-26 23:56:27 -0700

    feat: image gen api

--- fastapi/app/api/chat_router.py
+++ fastapi/app/api/chat_router.py
@@ -3,7 +3,6 @@ from pydantic import BaseModel
 
 import app.dao.user_dao as user_dao
 import app.logic.auth as auth
-import app.util.pricing as pricing
 from app.logic.chat.chat_service import handle_chat_interaction
 from app.model.message import Message
 
@@ -18,7 +17,7 @@ class ChatRequest(BaseModel):
     stream: bool
 
 
-@chat_router.post("/")
+@chat_router.post("/chat")
 async def generate(chat_request: ChatRequest, request: Request):
     authorization_header = request.headers.get("Authorization")
     username = auth.get_username_from_token(authorization_header)
@@ -38,6 +37,6 @@ async def generate(chat_request: ChatRequest, request: Request):
     )
 
 
-@chat_router.get("/")
-async def get_models() -> list[dict]:
-    return pricing.model_pricing_data
+@chat_router.get("/chat")
+async def get_models() -> list[str]:
+    return ["dall-e-3"]
