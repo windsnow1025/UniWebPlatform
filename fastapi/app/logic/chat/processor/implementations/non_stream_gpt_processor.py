443635a942a6cc04b781856abd21c273ef2d1d07
windsnow1025 <windsnow1025@gmail.com>
2024-04-27 05:44:54 -0700

    feat(fastapi): openai bad request error

--- fastapi/app/logic/chat/processor/implementations/non_stream_gpt_processor.py
+++ fastapi/app/logic/chat/processor/implementations/non_stream_gpt_processor.py
@@ -2,6 +2,7 @@ import logging
 import traceback
 
 import httpx
+import openai
 from fastapi import HTTPException
 
 from app.logic.chat.processor.interfaces.gpt_processor import GPTProcessor
@@ -23,4 +24,8 @@ class NonStreamGPTProcessor(GPTProcessor):
         except httpx.HTTPStatusError as e:
             status_code = e.response.status_code
             text = e.response.text
+            raise HTTPException(status_code=status_code, detail=text)
+        except openai.BadRequestError as e:
+            status_code = e.status_code
+            text = e.message
             raise HTTPException(status_code=status_code, detail=text)
--- fastapi/app/logic/chat/processor/implementations/stream_gpt_processor.py
+++ fastapi/app/logic/chat/processor/implementations/stream_gpt_processor.py
@@ -2,6 +2,7 @@ import logging
 from typing import Generator
 
 import httpx
+import openai
 from fastapi import HTTPException
 from openai import Stream
 from openai.types.chat import ChatCompletionChunk
@@ -43,4 +44,8 @@ class StreamGPTProcessor(GPTProcessor):
         except httpx.HTTPStatusError as e:
             status_code = e.response.status_code
             text = e.response.text
+            raise HTTPException(status_code=status_code, detail=text)
+        except openai.BadRequestError as e:
+            status_code = e.status_code
+            text = e.message
             raise HTTPException(status_code=status_code, detail=text)
