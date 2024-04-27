1ecaa37cc4d11b9aa66830d53080f100e4898ad5
windsnow1025 <windsnow1025@gmail.com>
2024-04-27 08:04:14 -0700

    feat(next): chat split information

---
+++ next/app/components/chat/ChatInformation.js
@@ -0,0 +1,39 @@
+import React, {useEffect, useState} from 'react';
+import LinkIcon from "@mui/icons-material/Link";
+import UserService from "../../../src/service/UserService";
+
+function ChatInformation({ generate }) {
+  const userService = new UserService();
+
+  const [credit, setCredit] = useState(0);
+
+  useEffect(() => {
+    const fetchCredit = async () => {
+      if (localStorage.getItem('token')) {
+        const credit = await userService.fetchCredit();
+        setCredit(credit);
+      }
+    };
+
+    fetchCredit();
+  }, [generate]);
+
+  return (
+    <div className="flex-around m-2">
+      <a href="/markdown/view/chat-doc.md" target="_blank" rel="noopener noreferrer">
+        <div className="flex-center">
+          <div>Document</div>
+          <LinkIcon/>
+        </div>
+      </a>
+      <a href="/markdown/view/chat-presets.md" target="_blank" rel="noopener noreferrer">
+        <div className="flex-center">
+          <div>Presets</div>
+          <LinkIcon/>
+        </div>
+      </a>
+      <div>Credit: {credit}</div>
+    </div>);
+}
+
+export default ChatInformation;
