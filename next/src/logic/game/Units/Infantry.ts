bee3b77df70ac2fba44a75b7fbc5a6022f53d5a2
windsnow1025 <windsnow1025@gmail.com>
2024-04-28 10:23:55 -0700

    feat(next): game static unit fields

--- next/src/logic/game/Units/Infantry.ts
+++ next/src/logic/game/Units/Infantry.ts
@@ -1,13 +1,13 @@
 import Unit from "@/src/logic/game/Unit";
 
 class Infantry extends Unit {
-    public attack: number = 7;
-    public defend: number = 2;
-    public health: number = 10;
-    public currentHealth: number = this.health;
-    public range: number = 1;
-    public speed: number = 1;
-    public cost: number = 1;
+    public static attack: number = 7;
+    public static defend: number = 2;
+    public static health: number = 10;
+    public static range: number = 1;
+    public static speed: number = 1;
+    public static cost: number = 1;
+    public currentHealth: number = Infantry.health;
 }
 
 export default Infantry;
