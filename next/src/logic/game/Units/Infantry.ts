f4caa9419d1fc0431604e89d661cfaa507989e52
windsnow1025 <windsnow1025@gmail.com>
2024-04-28 01:04:12 -0700

    feat(next): game init

---
+++ next/src/logic/game/Units/Infantry.ts
@@ -0,0 +1,12 @@
+import Unit from "@/src/logic/game/Unit";
+
+class Infantry extends Unit {
+    public attack: number = 7;
+    public defend: number = 2;
+    public health: number = 10;
+    public currentHealth: number = this.health;
+    public range: number = 0;
+    public cost: number = 1;
+}
+
+export default Infantry;
