4b3a360b25cd7e19e68d9de940e21720a6de9dd9
windsnow1025 <windsnow1025@gmail.com>
2024-04-28 01:04:12 -0700

    feat(next): game init

---
+++ next/src/logic/game/Units/Archer.ts
@@ -0,0 +1,12 @@
+import Unit from "@/src/logic/game/Unit";
+
+class Archer extends Unit {
+    public attack: number = 5;
+    public defend: number = 1;
+    public health: number = 10;
+    public currentHealth: number = this.health;
+    public range: number = 1;
+    public cost: number = 1;
+}
+
+export default Archer;
