b26bb43d2e72b4ccbfdf3fcc950e35058d536591
windsnow1025 <windsnow1025@gmail.com>
2024-04-30 05:11:37 -0700

    feat(next): graph attribute

--- next/src/logic/game/Graph.ts
+++ next/src/logic/game/Graph.ts
@@ -1,24 +1,35 @@
 class Graph {
-    nodes: Map<string, Set<string>>;
+    nodes: Map<string, { connections: Set<string>, attribute: number }>;
 
     constructor() {
         this.nodes = new Map();
     }
 
-    addNode(node: string) {
+    addNode(node: string, attribute: number = 0) {
         if (!this.nodes.has(node)) {
-            this.nodes.set(node, new Set());
+            this.nodes.set(node, { connections: new Set(), attribute });
         }
     }
 
-    addEdge(node1: string, node2: string) {
-        this.addNode(node1);
-        this.addNode(node2);
-        this.nodes.get(node1)!.add(node2);
-        this.nodes.get(node2)!.add(node1);
+    setAttribute(node: string, attribute: number) {
+        if (this.nodes.has(node)) {
+            this.nodes.get(node)!.attribute = attribute;
+        }
     }
 
-    getDistance(node1: string, node2: string): number {
+    getAttribute(node: string): number | undefined {
+        return this.nodes.get(node)?.attribute;
+    }
+
+    addEdge(node1: string, node2: string) {
+        if (!this.nodes.has(node1) || !this.nodes.has(node2)) {
+            return;
+        }
+        this.nodes.get(node1)!.connections.add(node2);
+        this.nodes.get(node2)!.connections.add(node1);
+    }
+
+    getDistance(node1: string, node2: string) {
         if (node1 === node2) return 0;
         const visited = new Set<string>();
         const queue: [string, number][] = [[node1, 0]];
@@ -31,7 +42,7 @@ class Graph {
             }
 
             visited.add(currentNode);
-            const neighbors = this.nodes.get(currentNode);
+            const neighbors = this.nodes.get(currentNode)?.connections;
 
             if (neighbors) {
                 for (const neighbor of neighbors) {
