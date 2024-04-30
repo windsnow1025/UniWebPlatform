a30db0d0de05cefce3f0bdff8db8cd49e8d6ca28
windsnow1025 <windsnow1025@gmail.com>
2024-04-30 05:53:48 +0000

    feat(next): game improve types

--- next/src/logic/game/UnitFactory.ts
+++ next/src/logic/game/UnitFactory.ts
@@ -1,12 +1,14 @@
 import Unit from "@/src/logic/game/Unit";
 import {unitClasses} from "@/src/logic/game/data/Unit";
 
-export const unitTypes = unitClasses.reduce((acc, UnitClass) => {
+export const unitTypeMap = unitClasses.reduce((acc, UnitClass) => {
     acc[UnitClass.name] = UnitClass;
     return acc;
 }, {} as { [key: string]: typeof Unit });
 
-export function createUnit(unitType: keyof typeof unitTypes): Unit {
-    const UnitClass = unitTypes[unitType];
+export type UnitTypeNames = keyof typeof unitTypeMap;
+
+export function createUnit(unitType: UnitTypeNames): Unit {
+    const UnitClass = unitTypeMap[unitType];
     return new UnitClass();
 }
