c32f8a9479cd8c28c74969ae32e99969d894d205
windsnow1025 <windsnow1025@gmail.com>
2024-05-15 06:38:52 -0700

    feat(next): spa

--- next/app/components/common/HeaderAppBar.js
+++ next/app/components/common/HeaderAppBar.js
@@ -1,25 +1,46 @@
-import {AppBar, IconButton, Typography} from "@mui/material";
+import { AppBar, IconButton, Typography, Menu, MenuItem } from "@mui/material";
 import AuthDiv from "./AuthDiv";
 import ThemeSelect from "./ThemeSelect";
-import React from "react";
+import React, { useState } from "react";
 import HomeIcon from '@mui/icons-material/Home';
-import {useRouter} from "next/navigation";
+import MenuIcon from '@mui/icons-material/Menu';
+import Link from 'next/link';
 
-const HeaderAppBar = ({title, useAuthDiv = true, systemTheme, setSystemTheme}) => {
-  const router = useRouter();
+const HeaderAppBar = ({ title, useAuthDiv = true, systemTheme, setSystemTheme }) => {
+  const [anchorEl, setAnchorEl] = useState(null);
+
+  const handleMenuOpen = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleMenuClose = () => {
+    setAnchorEl(null);
+  };
 
   return (
     <AppBar position="static" color="secondary">
       <div className="flex-around p-2">
-        <IconButton aria-label="home" onClick={() => router.push("/")}>
-          <HomeIcon fontSize="large"/>
+        <IconButton aria-label="menu" onClick={handleMenuOpen}>
+          <MenuIcon fontSize="large" />
         </IconButton>
+        <Menu
+          anchorEl={anchorEl}
+          open={Boolean(anchorEl)}
+          onClose={handleMenuClose}
+        >
+          <MenuItem component={Link} href="/markdown" onClick={handleMenuClose}>Markdown Blogs</MenuItem>
+          <MenuItem component={Link} href="/bookmark" onClick={handleMenuClose}>Bookmarks</MenuItem>
+          <MenuItem component={Link} href="/message" onClick={handleMenuClose}>Message Transmitter</MenuItem>
+          <MenuItem component={Link} href="/password" onClick={handleMenuClose}>Password Generator</MenuItem>
+          <MenuItem component={Link} href="/image" onClick={handleMenuClose}>Image Generate</MenuItem>
+          <MenuItem component={Link} href="/chat" onClick={handleMenuClose}>AI Chat</MenuItem>
+        </Menu>
         <Typography variant="h4" className="grow">
           {title}
         </Typography>
         {useAuthDiv &&
           <div className="m-1">
-            <AuthDiv/>
+            <AuthDiv />
           </div>
         }
         <div className="m-1">
