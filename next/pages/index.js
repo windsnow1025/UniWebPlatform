8a0a4ebbf9a9ed290ee8a67f4b6802c8db887d4f
windsnow1025 <windsnow1025@gmail.com>
2024-05-15 06:38:52 -0700

    feat(next): spa

--- next/pages/index.js
+++ next/pages/index.js
@@ -3,11 +3,9 @@
 import React, {useEffect} from "react";
 import {ThemeProvider} from "@mui/material/styles";
 import {CssBaseline, Link} from "@mui/material";
-import LinkIcon from "@mui/icons-material/Link";
 import GitHubIcon from "@mui/icons-material/GitHub";
 import useThemeHandler from "../app/hooks/useThemeHandler";
 import HeaderAppBar from "../app/components/common/HeaderAppBar";
-import MarkdownList from '../app/components/markdown/MarkdownList';
 
 function Index() {
   const {systemTheme, setSystemTheme, muiTheme} = useThemeHandler();
@@ -24,41 +22,6 @@ function Index() {
         systemTheme={systemTheme}
         setSystemTheme={setSystemTheme}
       />
-      <div className="flex-around m-4">
-        <Link href="/bookmark" target="_blank" rel="noopener noreferrer">
-          <div className="flex-center">
-            Bookmarks
-            <LinkIcon/>
-          </div>
-        </Link>
-        <Link href="/message" target="_blank" rel="noopener noreferrer">
-          <div className="flex-center">
-            Message Transmitter
-            <LinkIcon/>
-          </div>
-        </Link>
-        <Link href="/password" target="_blank" rel="noopener noreferrer">
-          <div className="flex-center">
-            Password Generator
-            <LinkIcon/>
-          </div>
-        </Link>
-        <Link href="/image" target="_blank" rel="noopener noreferrer">
-          <div className="flex-center">
-            Image Generate
-            <LinkIcon/>
-          </div>
-        </Link>
-        <Link href="/chat" target="_blank" rel="noopener noreferrer">
-          <div className="flex-center">
-            AI Chat
-            <LinkIcon/>
-          </div>
-        </Link>
-      </div>
-      <div className="m-8">
-        <MarkdownList/>
-      </div>
       <div className="flex-around m-2">
         <span>My Email: windsnow1024@gmail.com</span>
         <Link
