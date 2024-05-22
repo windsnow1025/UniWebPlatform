e46dcd37af469fada222d3f0a11d790dc62f50ca
windsnow1025 <windsnow1025@gmail.com>
2024-05-22 08:19:26 -0700

    chore(nest): eslint prettier

--- nest/src/main.ts
+++ nest/src/main.ts
@@ -1,6 +1,6 @@
 import {NestFactory} from '@nestjs/core';
-import {AppModule} from './app.module';
-import {ConfigService} from "@nestjs/config";
+import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
