a4f5419872f4ff24792b8ac1a0f921e2e275b417
windsnow1025 <windsnow1025@gmail.com>
2024-05-22 13:35:34 +0000

    build(nest): config init

---
+++ nest/config/configuration.ts
@@ -0,0 +1,16 @@
+export default () => {
+  const isProduction = process.env.NODE_ENV !== "development"
+
+  return {
+    port: isProduction ? 3000 : 3001,
+    baseUrl: isProduction ? '/api' : '',
+    mysql: {
+      host: isProduction ? 'mysql' : 'localhost',
+      rootPassword: process.env.MYSQL_ROOT_PASSWORD,
+      user: process.env.MYSQL_USER,
+      password: process.env.MYSQL_PASSWORD,
+      database: process.env.MYSQL_DATABASE,
+    },
+    jwtSecret: process.env.JWT_SECRET,
+  };
+};
