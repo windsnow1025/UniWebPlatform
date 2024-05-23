1087e24f9277a1222dacb5eaaaf50f76d64a053d
windsnow1025 <windsnow1025@gmail.com>
2024-05-22 21:46:10 -0700

    feat(nest): typeorm init

--- nest/src/app.module.ts
+++ nest/src/app.module.ts
@@ -3,12 +3,23 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import configuration from '../config/configuration';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       load: [configuration],
       isGlobal: true,
+    }),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'root',
+      database: 'test',
+      entities: [],
+      synchronize: true,
     }),
   ],
   controllers: [AppController],
