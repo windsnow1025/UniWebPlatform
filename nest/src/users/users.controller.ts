2576c427f958504b026711796018f6871853cf36
windsnow1025 <windsnow1025@gmail.com>
2024-05-23 06:30:59 -0700

    feat(nest): role authorization init

--- nest/src/users/users.controller.ts
+++ nest/src/users/users.controller.ts
@@ -1,17 +1,21 @@
 import { Controller, Get, Param } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { User } from './user.entity';
+import { Roles } from '../common/decorators/roles.decorator';
+import { Role } from '../common/enums/role.enum';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Get()
+  @Roles(Role.Admin)
   findAll(): Promise<User[]> {
     return this.usersService.findAll();
   }
 
   @Get('/:id')
+  @Roles(Role.Admin)
   findOneById(@Param('id') id: number): Promise<User | null> {
     return this.usersService.findOneById(id);
   }
