aa910df8b09b04761f30eb64b4ba3d4a91dbb020
windsnow1025 <windsnow1025@gmail.com>
2024-05-22 23:45:26 -0700

    feat(nest): user domain init

---
+++ nest/src/users/users.controller.ts
@@ -0,0 +1,13 @@
+import { Controller, Get } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { User } from './user.entity';
+
+@Controller('users')
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Get()
+  findAll(): Promise<User[]> {
+    return this.usersService.findAll();
+  }
+}
