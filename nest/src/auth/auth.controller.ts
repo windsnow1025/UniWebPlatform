fc2149461f36b733b6dadda151408686cbafeb3e
windsnow1025 <windsnow1025@gmail.com>
2024-05-23 04:17:27 -0700

    feat(nest): auth init

---
+++ nest/src/auth/auth.controller.ts
@@ -0,0 +1,14 @@
+import { Body, Controller, Post, HttpCode, HttpStatus } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { SignInDto } from './dto/sign-in.dto';
+
+@Controller('auth')
+export class AuthController {
+  constructor(private authService: AuthService) {}
+
+  @HttpCode(HttpStatus.OK)
+  @Post('login')
+  signIn(@Body() signInDto: SignInDto) {
+    return this.authService.signIn(signInDto.username, signInDto.password);
+  }
+}
