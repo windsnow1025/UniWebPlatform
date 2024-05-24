51552c65108b16ad387c683e01c217e2ab884805
windsnow1025 <windsnow1025@gmail.com>
2024-05-24 10:27:10 -0700

    feat: user & auth

--- nest/src/users/users.controller.ts
+++ nest/src/users/users.controller.ts
@@ -1,33 +1,32 @@
-import { Controller, Get, Param, Request, UnauthorizedException } from '@nestjs/common';
+import { Body, Controller, Delete, Get, NotFoundException, Post, Request } from "@nestjs/common";
 import { UsersService } from './users.service';
-import { User } from './user.entity';
-import { Roles } from '../common/decorators/roles.decorator';
-import { Role } from '../common/enums/role.enum';
 import { RequestWithUser } from '../auth/interfaces/request-with-user.interface';
+import { Public } from '../common/decorators/public.decorator';
+import { AuthDto } from '../auth/dto/auth.dto';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Get('user')
-  async findOne(@Request() req: RequestWithUser): Promise<User> {
+  async findOne(@Request() req: RequestWithUser) {
     const id = req.user.sub;
     const user = await this.usersService.findOneById(id);
     if (!user) {
-      throw new UnauthorizedException();
+      throw new NotFoundException();
     }
     return user;
   }
 
-  @Get()
-  @Roles(Role.Admin)
-  findAll(): Promise<User[]> {
-    return this.usersService.findAll();
+  @Public()
+  @Post('user')
+  create(@Body() signUpDto: AuthDto) {
+    return this.usersService.create(signUpDto.username, signUpDto.password);
   }
 
-  @Get(':id')
-  @Roles(Role.Admin)
-  findOneById(@Param('id') id: number): Promise<User | null> {
-    return this.usersService.findOneById(id);
+  @Delete('user')
+  delete(@Request() req: RequestWithUser) {
+    const id = req.user.sub;
+    return this.usersService.remove(id);
   }
 }
