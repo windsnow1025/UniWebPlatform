b504facb1d310e930b045b07c53572be8da76d34
windsnow1025 <windsnow1025@gmail.com>
2024-05-24 10:37:49 +0000

    feat(nest): enable cors

--- nest/src/main.ts
+++ nest/src/main.ts
@@ -1,9 +1,10 @@
-import {NestFactory} from '@nestjs/core';
+import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
 
   const configService = app.get(ConfigService);
   const port = configService.get<number>('port')!;
