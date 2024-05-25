48af99b35a3742c0cee1fbc0c71c3e48955d66d5
windsnow1025 <windsnow1025@gmail.com>
2024-05-25 05:02:57 -0700

    feat(nest): file upload api

--- nest/src/files/files.controller.ts
+++ nest/src/files/files.controller.ts
@@ -1,16 +1,30 @@
 import {
   Controller,
   Post,
+  Request,
   UploadedFile,
   UseInterceptors,
 } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
+import { RequestWithUser } from '../auth/interfaces/request-with-user.interface';
+import { ConfigService } from '@nestjs/config';
+import { multerOptions } from './multer.config';
 
 @Controller()
 export class FilesController {
+  constructor(private readonly configService: ConfigService) {}
+
   @Post('upload')
-  @UseInterceptors(FileInterceptor('file'))
-  uploadFile(@UploadedFile() file: Express.Multer.File) {
-    console.log(file);
+  @UseInterceptors(FileInterceptor('file', multerOptions))
+  uploadFile(
+    @Request() req: RequestWithUser,
+    @UploadedFile() file: Express.Multer.File,
+  ) {
+    const protocol = req.headers['x-forwarded-proto'] || req.protocol;
+    const host = req.headers['host'] || req.get('host');
+    const baseUrl = this.configService.get<string>('baseUrl')!;
+    const fileUrl = `${protocol}://${host}${baseUrl}/uploads/${file.filename}`;
+
+    return { url: fileUrl };
   }
 }
