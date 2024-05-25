95b8038a97ded28326f6a171296eabb353034dea
windsnow1025 <windsnow1025@gmail.com>
2024-05-25 01:21:52 -0700

    feat(nest): use validation pipe

--- nest/src/main.ts
+++ nest/src/main.ts
@@ -1,10 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   app.enableCors();
+  app.useGlobalPipes(new ValidationPipe());
 
   const configService = app.get(ConfigService);
   const port = configService.get<number>('port')!;
