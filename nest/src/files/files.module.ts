7214fc4dd2d9e6667ce9968b26cee88f4730b18e
windsnow1025 <windsnow1025@gmail.com>
2024-05-25 05:02:57 -0700

    feat(nest): file upload api

--- nest/src/files/files.module.ts
+++ nest/src/files/files.module.ts
@@ -1,9 +1,9 @@
 import { Module } from '@nestjs/common';
 import { FilesController } from './files.controller';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule],
   controllers: [FilesController],
-  exports: [],
 })
 export class FilesModule {}
