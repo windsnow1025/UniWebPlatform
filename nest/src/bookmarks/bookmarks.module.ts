7756baac3bd38227a7bed95e9c483a88182b30ac
windsnow1025 <windsnow1025@gmail.com>
2024-05-25 03:35:52 -0700

    feat(nest): conversation api

--- nest/src/bookmarks/bookmarks.module.ts
+++ nest/src/bookmarks/bookmarks.module.ts
@@ -1,14 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { JwtModule } from '@nestjs/jwt';
 import { Bookmark } from './bookmark.entity';
 import { BookmarksService } from './bookmarks.service';
 import { BookmarksController } from './bookmarks.controller';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Bookmark]), JwtModule],
+  imports: [TypeOrmModule.forFeature([Bookmark])],
   providers: [BookmarksService],
   controllers: [BookmarksController],
-  exports: [BookmarksService],
+  exports: [],
 })
 export class BookmarksModule {}
