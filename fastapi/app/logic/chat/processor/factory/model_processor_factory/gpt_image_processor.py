dd0df194725569ce53967f6594ff361668a7435c
windsnow1025 <windsnow1025@gmail.com>
2024-05-27 18:29:46 +0000

    feat(fastapi): gpt support document

--- fastapi/app/logic/chat/processor/factory/model_processor_factory/gpt_image_processor.py
+++ fastapi/app/logic/chat/processor/factory/model_processor_factory/gpt_image_processor.py
@@ -1,12 +1,13 @@
 import base64
 
-from app.logic.chat.processor.factory.model_processor_factory.image_processor import fetch_img_data
+from app.logic.chat.processor.factory.model_processor_factory.file_processor.image_processor import fetch_img_data
 from app.model.gpt_message import ImageURL, ImageContent
 
 
-async def get_image_contents_from_files(files: list[str]) -> list[ImageContent]:
-    image_urls = [await get_image_url_from_file(file) for file in files]
-    return [ImageContent(type="image_url", image_url=image_url) for image_url in image_urls]
+async def get_image_content_from_file(file: str) -> ImageContent:
+    base64_image = await encode_image_from_url(file)
+    image_url = ImageURL(url=f"data:image/jpeg;base64,{base64_image}")
+    return ImageContent(type="image_url", image_url=image_url)
 
 
 async def get_image_url_from_file(file: str) -> ImageURL:
@@ -17,6 +18,3 @@ async def get_image_url_from_file(file: str) -> ImageURL:
 async def encode_image_from_url(img_url: str) -> str:
     img_data = await fetch_img_data(img_url)
     return base64.b64encode(img_data.getvalue()).decode('utf-8')
-
-
-
