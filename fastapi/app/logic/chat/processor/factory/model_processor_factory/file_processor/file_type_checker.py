e388acb83bad45cb691da317a8b13f453f203ad7
windsnow1025 <windsnow1025@gmail.com>
2024-05-29 09:24:43 -0700

    feat(fastapi): improve file type checking

--- fastapi/app/logic/chat/processor/factory/model_processor_factory/file_processor/file_type_checker.py
+++ fastapi/app/logic/chat/processor/factory/model_processor_factory/file_processor/file_type_checker.py
@@ -1,3 +1,20 @@
-def is_image_file(file: str) -> bool:
-    image_extensions = ('.jpg', '.jpeg', '.png', '.gif', '.bmp', '.tiff', '.webp')
-    return file.lower().endswith(image_extensions)
+import mimetypes
+
+from app.logic.chat.processor.factory.model_processor_factory.file_processor.code_file_extensions import \
+    code_file_extensions
+
+
+def get_file_type(file_name: str) -> str:
+    if file_name.endswith(('.docx', '.doc')):
+        return 'word'
+    if file_name.endswith('.pdf'):
+        return 'pdf'
+    if file_name.endswith(code_file_extensions):
+        return 'code'
+    mime_type, _ = mimetypes.guess_type(file_name)
+    if mime_type:
+        if mime_type.startswith('application'):
+            return mime_type.split('/')[1]
+        else:
+            return mime_type.split('/')[0]
+    return 'unknown'
