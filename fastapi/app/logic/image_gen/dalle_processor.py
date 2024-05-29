d7c0c9a9f40c56967bf770bf16866d81b0304dc4
windsnow1025 <windsnow1025@gmail.com>
2024-05-29 10:12:52 -0700

    feat(fastapi): message preprocessor

--- fastapi/app/logic/image_gen/dalle_processor.py
+++ fastapi/app/logic/image_gen/dalle_processor.py
@@ -1,8 +1,3 @@
-from app.logic.chat.handler.response_handler import NonStreamResponseHandler, StreamResponseHandler
-from app.logic.chat.processor.interfaces.chat_processor import ChatProcessor
-from app.model.gpt_message import GptMessage
-
-
 class DalleProcessor:
     def __init__(
             self,
