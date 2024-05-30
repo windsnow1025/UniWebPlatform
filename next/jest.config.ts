b37aff7a0a574cb9a78d00a51fc8642ad681cd90
windsnow1025 <windsnow1025@gmail.com>
2024-05-30 06:23:42 -0700

    refactor(next): next 15 rc

--- next/jest.config.ts
+++ next/jest.config.ts
@@ -14,5 +14,4 @@ const config: Config = {
   // setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
 }
 
-// createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
 export default createJestConfig(config)
