523c7aefec628a33cab3bfff08075dda0a522ac2
windsnow1025 <windsnow1025@gmail.com>
2024-05-30 01:38:19 -0700

    feat(next): update file div ui

---
+++ next/app/components/message/FileDiv.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import {Typography, IconButton, Paper} from '@mui/material';
+import RemoveCircleOutlineIcon from '@mui/icons-material/RemoveCircleOutline';
+import mime from 'mime';
+
+const FileDiv = ({fileUrl, handleFileDelete}) => {
+  const mimeType = mime.getType(fileUrl);
+  const fileName = fileUrl.split('/').pop().split('-').pop();
+
+  if (mimeType && mimeType.startsWith('image/')) {
+    return (
+      <Paper key={fileUrl} className="flex p-2 m-2">
+        <div className="inflex-fill">
+          <img src={fileUrl} alt={fileName} className="max-w-full"/>
+        </div>
+        <div className="inflex-end">
+          <IconButton aria-label="delete-file" onClick={() => handleFileDelete(fileUrl)}>
+            <RemoveCircleOutlineIcon fontSize="small"/>
+          </IconButton>
+        </div>
+      </Paper>
+    )
+  } else {
+    return (
+      <Paper key={fileUrl} className="flex-center p-2 m-2">
+        <Typography variant="body2">
+          {fileName}
+        </Typography>
+        <IconButton aria-label="delete-file" onClick={() => handleFileDelete(fileUrl)}>
+          <RemoveCircleOutlineIcon fontSize="small"/>
+        </IconButton>
+      </Paper>
+    );
+  }
+};
+
+export default FileDiv;
