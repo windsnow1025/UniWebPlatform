b2f99d4d2d0648db87d352af279a25d2ef405d05
windsnow1025 <windsnow1025@gmail.com>
2024-06-03 16:15:44 +0000

    feat(fastapi): upload file filename

--- fastapi/app/logic/chat/processor/file_processor/file_preprocessor.py
+++ fastapi/app/logic/chat/processor/file_processor/file_preprocessor.py
@@ -1,5 +1,4 @@
-from app.logic.chat.processor.file_processor.document_processor import \
-    extract_text_from_file
+from app.logic.chat.processor.file_processor.document_processor import extract_text_from_file
 from app.logic.chat.processor.file_processor.file_type_checker import get_file_type
 from app.model.message import Message
 
@@ -9,8 +8,9 @@ async def preprocess_message(message: Message) -> None:
     for i, file in enumerate(message.files[::-1]):
         if get_file_type(file) == "image":
             continue
+        filename = file.split('-', 1)[1]
         file_text = await extract_text_from_file(file)
-        message.text = f"{file}: \n{file_text}\n{message.text}"
+        message.text = f"{filename}: \n{file_text}\n{message.text}"
         indices_to_delete.append(len(message.files) - 1 - i)
 
     for index in sorted(indices_to_delete, reverse=True):
