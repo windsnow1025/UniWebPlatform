9b7d392a7486f3d0ce7ea7de12cfcb02546dc215
windsnow1025 <windsnow1025@gmail.com>
2024-06-20 21:34:00 +0000

    style(next): credit div loading state

--- next/app/components/common/user/CreditDiv.js
+++ next/app/components/common/user/CreditDiv.js
@@ -1,17 +1,20 @@
-import React, {useEffect, useState} from 'react';
-import {UserLogic} from "../../../../src/logic/UserLogic";
-import {Typography} from "@mui/material";
+import React, { useEffect, useState } from 'react';
+import { Typography, CircularProgress } from "@mui/material";
+import { UserLogic } from "../../../../src/logic/UserLogic";
 
 function CreditDiv({ refreshKey }) {
   const userLogic = new UserLogic();
 
-  const [credit, setCredit] = useState(0);
+  const [credit, setCredit] = useState(null);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const fetchCredit = async () => {
       if (localStorage.getItem('token')) {
+        setLoading(true);
         const credit = await userLogic.fetchCredit();
         setCredit(credit);
+        setLoading(false);
       }
     };
 
@@ -19,7 +22,13 @@ function CreditDiv({ refreshKey }) {
   }, [refreshKey]);
 
   return (
-    <Typography>Credit: {credit}</Typography>
+    <>
+      {loading ? (
+        <CircularProgress />
+      ) : (
+        <Typography>Credit: {credit}</Typography>
+      )}
+    </>
   );
 }
 
