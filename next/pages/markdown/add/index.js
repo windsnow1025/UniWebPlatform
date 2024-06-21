3656148954b45c1c53014021bf44646aa69de46a
windsnow1025 <windsnow1025@gmail.com>
2024-06-21 07:59:51 -0700

    style(next): more local scroll

--- next/pages/markdown/add/index.js
+++ next/pages/markdown/add/index.js
@@ -53,23 +53,27 @@ function Index() {
   return (
     <ThemeProvider theme={muiTheme}>
       <CssBaseline enableColorScheme/>
-      <HeaderAppBar
-        title="Markdown Add"
-        systemTheme={systemTheme}
-        setSystemTheme={setSystemTheme}
-      />
-      <div
-        className="markdown-body p-2 min-h-16"
-        ref={markdownRef}
-        contentEditable={isEditing ? "plaintext-only" : "false"}
-      />
-      <div className="flex-center">
-        {!isEditing &&
-          <div className="m-1"><Button variant="contained" color="primary" onClick={handleEdit}>Edit</Button></div>}
-        {isEditing &&
-          <div className="m-1"><Button variant="contained" color="primary" onClick={handleConfirm}>Confirm</Button>
-          </div>}
-        <div className="m-1"><Button variant="contained" color="secondary" onClick={handleAdd}>Add</Button></div>
+      <div className="local-scroll-root">
+        <HeaderAppBar
+          title="Markdown Add"
+          systemTheme={systemTheme}
+          setSystemTheme={setSystemTheme}
+        />
+        <div className="local-scroll-scrollable m-2">
+          <div
+            className="markdown-body p-2 min-h-16"
+            ref={markdownRef}
+            contentEditable={isEditing ? "plaintext-only" : "false"}
+          />
+        </div>
+        <div className="flex-center">
+          {!isEditing &&
+            <div className="m-1"><Button variant="contained" color="primary" onClick={handleEdit}>Edit</Button></div>}
+          {isEditing &&
+            <div className="m-1"><Button variant="contained" color="primary" onClick={handleConfirm}>Confirm</Button>
+            </div>}
+          <div className="m-1"><Button variant="contained" color="secondary" onClick={handleAdd}>Add</Button></div>
+        </div>
       </div>
       <Snackbar
         open={alertOpen}
