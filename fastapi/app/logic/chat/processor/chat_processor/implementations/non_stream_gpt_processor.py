e4e964f6e97aed99fad1ddfde271ebf953e3cf1d
windsnow1025 <windsnow1025@gmail.com>
2024-06-21 11:58:56 -0700

    fix(fastapi): generate exception

--- fastapi/app/logic/chat/processor/chat_processor/implementations/non_stream_gpt_processor.py
+++ fastapi/app/logic/chat/processor/chat_processor/implementations/non_stream_gpt_processor.py
@@ -1,4 +1,5 @@
 import logging
+import re
 
 import httpx
 import openai
@@ -28,3 +29,11 @@ class NonStreamGPTProcessor(GPTProcessor):
             status_code = e.status_code
             text = e.message
             raise HTTPException(status_code=status_code, detail=text)
+        except Exception as e:
+            match = re.search(r'\d{3}', str(e))
+            if match:
+                error_code = int(match.group(0))
+            else:
+                error_code = 500
+
+            raise HTTPException(status_code=error_code, detail=str(e))
