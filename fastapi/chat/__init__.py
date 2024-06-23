b4c4886f886f50c8918ae505e3132d366579e017
windsnow1025 <windsnow1025@gmail.com>
2024-06-23 00:14:16 -0700

    chore(fastapi): optimize init py

--- fastapi/chat/__init__.py
+++ fastapi/chat/__init__.py
@@ -1,4 +1,4 @@
 from .factory.chat_processor_factory import create_chat_processor
-from chat.logic.file_preprocessor import preprocess_message
+from .logic.file_preprocessor import preprocess_message
 from .model.message import Message
 from .model.handler import ChunkGenerator
