4a4b6e3b93e570db2accf267cf79fe63a0c5d8c7
windsnow1025 <windsnow1025@gmail.com>
2024-07-02 07:46:07 +0000

    feat!: minio

--- nest/src/files/files.controller.ts
+++ nest/src/files/files.controller.ts
@@ -8,27 +8,27 @@ import {
 } from '@nestjs/common';
 import { Public } from '../common/decorators/public.decorator';
 import { FileInterceptor } from '@nestjs/platform-express';
-import { ConfigService } from '@nestjs/config';
 import { Request } from 'express';
-import { multerOptions } from './multer.config';
+import { MinioService } from './minio.service';
 
 @Controller('files')
 export class FilesController {
-  constructor(private readonly configService: ConfigService) {}
+  constructor(private readonly minioService: MinioService) {}
 
   @Public()
   @Post('file')
-  @UseInterceptors(FileInterceptor('file', multerOptions))
-  uploadFile(
+  @UseInterceptors(FileInterceptor('file'))
+  async uploadFile(
     @Headers('x-forwarded-proto') forwardedProto: string,
     @Headers('host') forwardedHost: string,
     @Req() req: Request,
     @UploadedFile() file: Express.Multer.File,
   ) {
     const protocol = forwardedProto || req.protocol;
-    const host = forwardedHost || req.get('host');
-    const baseUrl = this.configService.get<string>('baseUrl')!;
-    const fileUrl = `${protocol}://${host}${baseUrl}/uploads/${file.filename}`;
+    const host = forwardedHost || req.get('host')!;
+
+    const fileName = await this.minioService.uploadFile(file);
+    const fileUrl = this.minioService.getFileUrl(protocol, host, fileName);
 
     return { url: fileUrl };
   }
--- nest/src/files/files.module.ts
+++ nest/src/files/files.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { FilesController } from './files.controller';
 import { ConfigModule } from '@nestjs/config';
+import { MinioService } from './minio.service';
 
 @Module({
   imports: [ConfigModule],
   controllers: [FilesController],
+  providers: [MinioService],
 })
 export class FilesModule {}
