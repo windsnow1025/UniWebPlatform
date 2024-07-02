0d5279ce4d22bf7f77fc8735c5215cc54bd47bd1
windsnow1025 <windsnow1025@gmail.com>
2024-07-02 06:53:37 +0000

    feat(nest): optimize mysql config

--- nest/src/app.module.ts
+++ nest/src/app.module.ts
@@ -35,7 +35,7 @@ import { MessagesModule } from './messages/messages.module';
       useFactory: (configService: ConfigService) => ({
         type: 'mysql',
         host: configService.get<string>('mysql.host'),
-        port: 3306,
+        port: configService.get<number>('mysql.port'),
         username: configService.get<string>('mysql.user'),
         password: configService.get<string>('mysql.password'),
         database: configService.get<string>('mysql.database'),
