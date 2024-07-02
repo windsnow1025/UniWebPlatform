833f8c52f545b993025021297f40d05e6e8b6ef0
windsnow1025 <windsnow1025@gmail.com>
2024-07-02 01:04:19 +0000

    feat(nest): salt hash password

--- nest/src/auth/auth.service.ts
+++ nest/src/auth/auth.service.ts
@@ -1,3 +1,4 @@
+import * as bcrypt from 'bcrypt';
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { UsersService } from '../users/users.service';
 import { JwtService } from '@nestjs/jwt';
@@ -12,9 +13,17 @@ export class AuthService {
 
   async getToken(username: string, password: string) {
     const user = await this.usersService.findOneByUsername(username);
-    if (user?.password !== password) {
+
+    if (!user) {
       throw new UnauthorizedException();
     }
+
+    const hash = user.password;
+    const isMatch = await bcrypt.compare(password, hash);
+    if (!isMatch) {
+      throw new UnauthorizedException();
+    }
+
     const payload: JwtPayload = {
       sub: user.id.toString(),
       username: user.username,
