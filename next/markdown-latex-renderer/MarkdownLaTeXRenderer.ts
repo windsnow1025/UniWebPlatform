d28aec6acf71a1923fc5f19f29313352797f2002
windsnow1025 <windsnow1025@gmail.com>
2024-07-04 03:48:59 +0000

    feat!(next): use imported markdown latex renderer

--- next/markdown-latex-renderer/MarkdownLaTeXRenderer.ts
+++
@@ -1,7 +0,0 @@
-import {parseMarkdown} from "@/markdown-latex-renderer/MarkdownParser";
-import {renderLaTeX} from "@/markdown-latex-renderer/LaTeXRenderer";
-
-export async function parseMarkdownLaTeX(content_div: HTMLElement, content: string, sanitize = true) {
-  content_div.innerHTML = await parseMarkdown(content, sanitize);
-  renderLaTeX(content_div);
-}
--- next/markdown-latex-renderer/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './MarkdownParser';
-export * from './LaTeXRenderer';
-export * from './MarkdownLaTeXRenderer';
