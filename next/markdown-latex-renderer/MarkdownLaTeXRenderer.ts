8a2363ee2c58198107b62c1ea1576c037187b66f
windsnow1025 <windsnow1025@gmail.com>
2024-07-04 03:26:09 +0000

    fix(next): markdown latex renderer folder name

--- next/markdown-latex-renderer/MarkdownLaTeXRenderer.ts
+++ next/markdown-latex-renderer/MarkdownLaTeXRenderer.ts
@@ -1,5 +1,5 @@
-import {parseMarkdown} from "@/markdown-latext-renderer/MarkdownParser";
-import {renderLaTeX} from "@/markdown-latext-renderer/LaTeXRenderer";
+import {parseMarkdown} from "@/markdown-latex-renderer/MarkdownParser";
+import {renderLaTeX} from "@/markdown-latex-renderer/LaTeXRenderer";
 
 export async function parseMarkdownLaTeX(content_div: HTMLElement, content: string, sanitize = true) {
   content_div.innerHTML = await parseMarkdown(content, sanitize);
