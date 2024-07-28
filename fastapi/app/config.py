073a8ab0e99d586d998c37b2427eb9f0d3bb828b
windsnow1025 <windsnow1025@gmail.com>
2024-07-28 06:07:56 +0000

    Revert "feat(fastapi): nest api base url env"
    This reverts commit d57f77b975638f1580d7d2dfc4de143413015bc8.

--- fastapi/app/config.py
+++ fastapi/app/config.py
@@ -8,6 +8,5 @@ def init_env():
     is_production = os.environ.get('ENV') != 'development'
 
     os.environ["MYSQL_HOST"] = "mysql" if is_production else "localhost"
-    os.environ["NEST_API_BASE_URL"] = "/api/nest" if is_production else "http://localhost:3001"
 
     logging.info(f"Using {'production' if is_production else 'development'} setting.")
