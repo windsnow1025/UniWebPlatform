db6247e3a5a7d73de6ec4137e5717a02563ae5a7
windsnow1025 <windsnow1025@gmail.com>
2024-07-29 06:30:59 -0700

    feat(fastapi): system messages

--- fastapi/chat/__init__.py
+++ fastapi/chat/__init__.py
@@ -1,4 +1,4 @@
 from .factory.chat_processor_factory import create_chat_processor
-from .logic.file_preprocessor import preprocess_message
+from .logic.message_preprocessor import preprocess_messages, extract_system_messages
 from .model.message import Message
 from .model.handler import ChunkGenerator
