f6ee49a2aed66b156c485f4daa45206aaab3e506
windsnow1025 <windsnow1025@gmail.com>
2024-07-29 06:30:59 -0700

    feat(fastapi): system messages

--- fastapi/chat/factory/model_processor_factory/claude_processor_factory.py
+++ fastapi/chat/factory/model_processor_factory/claude_processor_factory.py
@@ -5,6 +5,7 @@ import anthropic
 from chat.factory.message_converter import convert_messages_to_gpt, convert_messages_to_claude
 from chat.implementations.non_stream_claude_processor import NonStreamClaudeProcessor
 from chat.implementations.stream_claude_processor import StreamClaudeProcessor
+from chat.logic.message_preprocessor import extract_system_messages
 from chat.model.handler import NonStreamResponseHandler, StreamResponseHandler
 from chat.model.message import Message
 
@@ -21,6 +22,8 @@ async def create_claude_processor(
         api_key=os.environ["ANTHROPIC_API_KEY"],
     )
 
+    system = extract_system_messages(messages)
+
     claude_messages = await convert_messages_to_claude(messages)
 
     if stream:
@@ -28,7 +31,7 @@ async def create_claude_processor(
             model=model,
             messages=claude_messages,
             temperature=temperature,
-            system="",
+            system=system,
             anthropic=client,
             response_handler=stream_response_handler
         )
