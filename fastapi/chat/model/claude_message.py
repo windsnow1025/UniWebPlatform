8acb86d6fdc092253abc4f9422fee4298a5bbdac
windsnow1025 <windsnow1025@gmail.com>
2024-08-01 03:49:24 -0700

    feat(fastapi): claude vision support

--- fastapi/chat/model/claude_message.py
+++ fastapi/chat/model/claude_message.py
@@ -1,7 +1,29 @@
 from dataclasses import dataclass
 
 
+# type: "base64"
+@dataclass
+class ImageSource:
+    type: str
+    media_type: str
+    data: str
+
+
+# type: "text"
+@dataclass
+class TextContent:
+    type: str
+    text: str
+
+
+# type: "image"
+@dataclass
+class ImageContent:
+    type: str
+    source: ImageSource
+
+
 @dataclass
 class ClaudeMessage:
     role: str
-    content: str
+    content: str | list[TextContent | ImageContent]
