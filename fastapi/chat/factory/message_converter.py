1b93f6173e233af8cad76e93ce68b77a75fb7463
windsnow1025 <windsnow1025@gmail.com>
2024-08-01 03:49:24 -0700

    feat(fastapi): claude vision support

--- fastapi/chat/factory/message_converter.py
+++ fastapi/chat/factory/message_converter.py
@@ -1,9 +1,9 @@
-from chat.factory.model_processor_factory.gemini_image_processor import get_image_part_from_file
-from chat.factory.model_processor_factory.gpt_image_processor import get_image_content_from_file
+from chat.factory.image_processor import *
 from chat.logic.file_type_checker import get_file_type
+from chat.model import gpt_message, claude_message
 from chat.model.claude_message import ClaudeMessage
 from chat.model.gemini_message import GeminiMessage
-from chat.model.gpt_message import GptMessage, TextContent
+from chat.model.gpt_message import GptMessage
 from chat.model.message import Message
 
 
@@ -17,6 +17,25 @@ async def convert_messages_to_gpt(messages: list[Message]) -> list[GptMessage]:
 
 async def convert_messages_to_claude(messages: list[Message]) -> list[ClaudeMessage]:
     return [await convert_message_to_claude(message) for message in messages]
+
+
+async def convert_message_to_gpt(message: Message) -> GptMessage:
+    role = message.role
+    text = message.text
+    files = message.files
+
+    content = []
+
+    if text:
+        text_content = gpt_message.TextContent(type="text", text=text)
+        content.append(text_content)
+
+    for file in files:
+        if get_file_type(file) == "image":
+            image_contents = await get_gpt_image_content_from_url(file)
+            content.append(image_contents)
+
+    return GptMessage(role=role, content=content)
 
 
 async def convert_message_to_gemini(message: Message) -> GeminiMessage:
@@ -39,29 +58,10 @@ async def convert_message_to_gemini(message: Message) -> GeminiMessage:
 
     for file in files:
         if get_file_type(file) == "image":
-            image_contents = await get_image_part_from_file(file)
+            image_contents = await get_gemini_image_part_from_url(file)
             parts.append(image_contents)
 
     return GeminiMessage(role=gemini_role, parts=parts)
-
-
-async def convert_message_to_gpt(message: Message) -> GptMessage:
-    role = message.role
-    text = message.text
-    files = message.files
-
-    content = []
-
-    if text:
-        text_content = TextContent(type="text", text=text)
-        content.append(text_content)
-
-    for file in files:
-        if get_file_type(file) == "image":
-            image_contents = await get_image_content_from_file(file)
-            content.append(image_contents)
-
-    return GptMessage(role=role, content=content)
 
 
 async def convert_message_to_claude(message: Message) -> ClaudeMessage:
@@ -75,4 +75,15 @@ async def convert_message_to_claude(message: Message) -> ClaudeMessage:
     elif role == "assistant":
         claude_role = "assistant"
 
-    return ClaudeMessage(role=claude_role, content=text)
+    content = []
+
+    if text:
+        text_content = claude_message.TextContent(type="text", text=text)
+        content.append(text_content)
+
+    for file in files:
+        if get_file_type(file) == "image":
+            image_contents = await get_claude_image_content_from_url(file)
+            content.append(image_contents)
+
+    return ClaudeMessage(role=claude_role, content=content)
