94e309fb751c105a704f74336bfe7d4edb79885d
windsnow1025 <windsnow1025@gmail.com>
2024-08-07 02:14:25 -0700

    chore(next): remove redundant comments

--- next/pages/markdown/add/index.js
+++ next/pages/markdown/add/index.js
@@ -34,7 +34,7 @@ function MarkdownAdd() {
 
   const [alertOpen, setAlertOpen] = useState(false);
   const [alertMessage, setAlertMessage] = useState('');
-  const [alertSeverity, setAlertSeverity] = useState('info'); // 'success', 'error', 'warning', 'info'
+  const [alertSeverity, setAlertSeverity] = useState('info');
 
   const handleAdd = async () => {
     const title = markdownLogic.getTitleFromContent(content);
