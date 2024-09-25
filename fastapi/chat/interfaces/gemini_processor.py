f1f3bef4f175c06f82d48d8ca955a1bf1572c2be
windsnow1025 <windsnow1025@gmail.com>
2024-09-24 21:32:11 -0700

    refactor(fastapi): chat handler

--- fastapi/chat/interfaces/gemini_processor.py
+++ fastapi/chat/interfaces/gemini_processor.py
@@ -11,12 +11,10 @@ class GeminiProcessor(ChatProcessor):
             model: GenerativeModel,
             messages: list[GeminiMessage],
             temperature: float,
-            response_handler: NonStreamResponseHandler | StreamResponseHandler
     ):
         self.model = model
         self.messages = messages
         self.temperature = temperature
-        self.response_handler = response_handler
 
     def process_request(self):
         raise NotImplementedError
