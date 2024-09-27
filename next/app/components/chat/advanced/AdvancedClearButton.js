1ba079fd987c87f63f09f484dfc92731254dcc3e
windsnow1025 <windsnow1025@gmail.com>
2024-09-27 07:51:14 -0700

    feat(next): stop generation on clear/delete/add

--- next/app/components/chat/advanced/AdvancedClearButton.js
+++ next/app/components/chat/advanced/AdvancedClearButton.js
@@ -3,10 +3,16 @@ import {Button} from "@mui/material";
 import RefreshIcon from '@mui/icons-material/Refresh';
 import ChatLogic from "../../../../src/conversation/chat/ChatLogic";
 
-function AdvancedClearButton({setMessages}) {
+function AdvancedClearButton({
+                               setMessages,
+                               setIsGenerating,
+                               isGeneratingRef,
+}) {
   const chatLogic = new ChatLogic();
 
   const handleClear = () => {
+    setIsGenerating(false);
+    isGeneratingRef.current = false;
     setMessages(chatLogic.initMessages);
   };
 
