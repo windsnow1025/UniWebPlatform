6d3a5ef7db877baa2f88e9c34a3ce7874f80dcf7
windsnow1025 <windsnow1025@gmail.com>
2024-10-13 07:25:02 -0700

    chore(fastapi): optimize code

--- fastapi/chat/__init__.py
+++ fastapi/chat/__init__.py
@@ -1,3 +1,3 @@
 from .logic.chat_generate.chat_client_factory import create_chat_client
-from .logic.message_preprocess.message_preprocessor import preprocess_messages, extract_system_messages
+from .logic.message_preprocess.message_preprocessor import preprocess_messages
 from .model.message import Message
