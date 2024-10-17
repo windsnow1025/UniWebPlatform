dcf47716c09c46791c106a08757d54f304bd2799
windsnow1025 <windsnow1025@gmail.com>
2024-10-17 01:20:38 -0700

    feat(fastapi): disable sqlmodel logging

--- fastapi/app/repository/db_connection.py
+++ fastapi/app/repository/db_connection.py
@@ -8,7 +8,7 @@ from sqlmodel import create_engine, Session
 
 def get_session():
     db_url = f"mysql+mysqlconnector://{os.getenv('MYSQL_USER')}:{os.getenv('MYSQL_PASSWORD')}@{os.getenv('MYSQL_HOST')}/{os.getenv('MYSQL_DATABASE')}"
-    engine = create_engine(db_url, echo=True)
+    engine = create_engine(db_url)
 
     with Session(engine) as session:
         yield session
