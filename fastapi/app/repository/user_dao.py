43e44076d5bc2b11494374cf9341ce52373f55ce
windsnow1025 <windsnow1025@gmail.com>
2024-10-17 01:02:34 -0700

    feat(fastapi): session dep

--- fastapi/app/repository/user_dao.py
+++ fastapi/app/repository/user_dao.py
@@ -1,23 +1,20 @@
-from sqlmodel import select
-
-from app.repository.db_connection import DatabaseConnection
+from fastapi import HTTPException
+from sqlmodel import select, Session
 from app.repository.user import User
 
+def select_credit(username: str, session: Session) -> float:
+    statement = select(User).where(User.username == username)
+    user = session.exec(statement).first()
+    if not user:
+        raise HTTPException(status_code=404, detail="User not found")
+    return user.credit
 
-def select_credit(username: str) -> float:
-    database_connection = DatabaseConnection.get_instance()
-    with database_connection.get_session() as session:
-        statement = select(User).where(User.username == username)
-        user = session.exec(statement).first()
-        return user.credit
-
-
-def reduce_credit(username: str, cost: float) -> float:
-    database_connection = DatabaseConnection.get_instance()
-    with database_connection.get_session() as session:
-        statement = select(User).where(User.username == username)
-        user = session.exec(statement).first()
-        user.credit -= cost
-        session.add(user)
-        session.commit()
-        return user.credit
+def reduce_credit(username: str, cost: float, session: Session) -> float:
+    statement = select(User).where(User.username == username)
+    user = session.exec(statement).first()
+    if not user:
+        raise HTTPException(status_code=404, detail="User not found")
+    user.credit -= cost
+    session.add(user)
+    session.commit()
+    return user.credit
