c72aa899f2ac50e4d246f59f58fb73d249b3da46
windsnow1025 <windsnow1025@gmail.com>
2024-10-20 06:02:20 -0700

    feat(fastapi): message dto

---
+++ fastapi/app/api/message_dto.py
@@ -0,0 +1,38 @@
+import os
+import re
+from dataclasses import dataclass
+from typing import List
+
+from chat.model.message import Message, FileUrl
+
+
+# role: "user" || "assistant" || "system"
+@dataclass
+class MessageDto:
+    role: str
+    text: str
+    files: List[str]
+
+
+def convert_message_dtos_to_messages(message_dtos: List[MessageDto]) -> List[Message]:
+    messages = []
+    for message_dto in message_dtos:
+        file_urls = []
+        for file in message_dto.files:
+            external_url = file
+            internal_url = get_internal_url(file)
+            file_urls.append(FileUrl(external_url, internal_url))
+        messages.append(Message(
+            message_dto.role,
+            message_dto.text,
+            file_urls,
+        ))
+    return messages
+
+
+def get_internal_url(external_url):
+    return re.sub(
+        r'https?://[^/]+',
+        f'http://{os.environ["MINIO_HOST"]}',
+        external_url
+    )
--- fastapi/chat/logic/chat_generate/image_processor.py
+++ fastapi/chat/logic/chat_generate/image_processor.py
@@ -8,20 +8,20 @@ from fastapi import HTTPException
 from chat.model import gpt_message, claude_message
 
 
-async def get_gpt_image_content_from_url(file: str) -> gpt_message.ImageContent:
-    img_data, media_type = await fetch_img_data(file)
+async def get_gpt_image_content_from_url(req_img_url: str) -> gpt_message.ImageContent:
+    img_data, media_type = await fetch_img_data(req_img_url)
     base64_image = base64.b64encode(img_data.getvalue()).decode('utf-8')
-    image_url = gpt_message.ImageURL(url=f"data:{media_type};base64,{base64_image}")
-    return gpt_message.ImageContent(type="image_url", image_url=image_url)
+    res_img_url = gpt_message.ImageURL(url=f"data:{media_type};base64,{base64_image}")
+    return gpt_message.ImageContent(type="image_url", image_url=res_img_url)
 
 
-async def get_gemini_image_part_from_url(file: str):
-    image_data, _ = await fetch_img_data(file)
+async def get_gemini_image_part_from_url(req_img_url: str) -> PIL.Image:
+    image_data, _ = await fetch_img_data(req_img_url)
     return PIL.Image.open(image_data)
 
 
-async def get_claude_image_content_from_url(file: str) -> claude_message.ImageContent:
-    img_data, media_type = await fetch_img_data(file)
+async def get_claude_image_content_from_url(req_img_url: str) -> claude_message.ImageContent:
+    img_data, media_type = await fetch_img_data(req_img_url)
     base64_image = base64.b64encode(img_data.getvalue()).decode('utf-8')
     image_source = claude_message.ImageSource(type="base64", media_type=media_type, data=base64_image)
     return claude_message.ImageContent(type="image", source=image_source)
