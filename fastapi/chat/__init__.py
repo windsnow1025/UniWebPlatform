f7e2c347535fcb0ef7a5a9526cefc61b52d845cd
windsnow1025 <windsnow1025@gmail.com>
2024-10-20 04:50:12 -0700

    fix: fastapi minio host

--- fastapi/chat/__init__.py
+++ fastapi/chat/__init__.py
@@ -1,3 +1,4 @@
 from .logic.chat_generate.chat_client_factory import create_chat_client
 from .logic.message_preprocess.message_preprocessor import preprocess_messages
+from .logic.message_preprocess.file_type_checker import get_file_type
 from .model.message import Message
