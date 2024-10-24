6db66d2daee5053a38d0900721180d0500c569a4
windsnow1025 <windsnow1025@gmail.com>
2024-10-24 03:18:32 -0700

    chore(fastapi): optimize

--- fastapi/app/api/chat_router.py
+++ fastapi/app/api/chat_router.py
@@ -6,8 +6,8 @@ from pydantic import BaseModel
 import app.logic.auth as auth
 import app.logic.chat.util.model_pricing as pricing
 import app.repository.user_dao as user_dao
-from app.logic.chat.chat_service import handle_chat_interaction
 from app.api.message_dto import MessageDto
+from app.logic.chat.chat_service import handle_chat_interaction
 from app.repository.db_connection import SessionDep
 
 chat_router = APIRouter()
