3895bf96b612f3367029fd9574adb708baffd896
windsnow1025 <windsnow1025@gmail.com>
2024-12-17 01:35:34 -0800

    feat: improve api models

--- fastapi/app/api/messages_convert_router.py
+++ fastapi/app/api/messages_convert_router.py
@@ -16,9 +16,9 @@ async def convert_messages(chat_request: MessagesConvertRequest):
     messages = chat_request.messages
     api_type = chat_request.api_type
 
-    if api_type == "open_ai":
+    if api_type == "OpenAI":
         return await convert_messages_to_gpt(messages)
-    elif api_type == "gemini":
+    elif api_type == "Gemini":
         return await convert_messages_to_gemini(messages)
-    elif api_type == "claude":
+    elif api_type == "Claude":
         return await convert_messages_to_claude(messages)
