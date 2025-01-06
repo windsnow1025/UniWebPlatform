9176462f98a31d5048f199158e74812212a7ea15
windsnow1025 <windsnow1025@gmail.com>
2025-01-07 00:03:06 +0800

    fix(fastapi): cors

--- fastapi/app/main.py
+++ fastapi/app/main.py
@@ -1,7 +1,7 @@
 import logging
 
 from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
+from starlette.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
 
 from app.api.chat_router import chat_router
