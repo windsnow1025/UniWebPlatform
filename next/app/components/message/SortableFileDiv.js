a3b807dbeebf97e99099821a0f8b6778b9a96508
windsnow1025 <windsnow1025@gmail.com>
2025-01-15 20:38:16 -0800

    feat(next): draggable filediv

---
+++ next/app/components/message/SortableFileDiv.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { useSortable } from "@dnd-kit/sortable";
+import { CSS } from "@dnd-kit/utilities";
+import FileDiv from './FileDiv';
+
+const SortableFileDiv = ({ fileUrl, files, setFiles }) => {
+  const {
+    attributes,
+    listeners,
+    setNodeRef,
+    transform,
+    transition,
+  } = useSortable({ id: fileUrl });
+
+  const style = {
+    transform: CSS.Translate.toString(transform),
+    transition,
+  };
+
+  return (
+    <div ref={setNodeRef} style={style} {...attributes} {...listeners}>
+      <FileDiv fileUrl={fileUrl} files={files} setFiles={setFiles} />
+    </div>
+  );
+};
+
+export default SortableFileDiv;
