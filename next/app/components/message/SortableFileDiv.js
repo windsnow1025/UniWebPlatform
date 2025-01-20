3f8d0280443ee751d801098aab543bcf8c52ef08
windsnow1025 <windsnow1025@gmail.com>
2025-01-20 07:30:19 -0800

    feat(next): drag indicator icon

--- next/app/components/message/SortableFileDiv.js
+++ next/app/components/message/SortableFileDiv.js
@@ -1,27 +1,32 @@
 import React from 'react';
-import { useSortable } from "@dnd-kit/sortable";
-import { CSS } from "@dnd-kit/utilities";
+import {useSortable} from "@dnd-kit/sortable";
+import {CSS} from "@dnd-kit/utilities";
 import FileDiv from './FileDiv';
+import DragIndicatorIcon from '@mui/icons-material/DragIndicator';
 
-const SortableFileDiv = ({ fileUrl, files, setFiles }) => {
+const SortableFileDiv = ({fileUrl, files, setFiles}) => {
   const {
     attributes,
     listeners,
     setNodeRef,
     transform,
     transition,
-  } = useSortable({ id: fileUrl });
+  } = useSortable({id: fileUrl});
 
   const style = {
     transform: CSS.Translate.toString(transform),
     transition,
-    cursor: 'grab',
     touchAction: 'none',
   };
 
   return (
-    <div ref={setNodeRef} style={style} {...attributes} {...listeners}>
-      <FileDiv fileUrl={fileUrl} files={files} setFiles={setFiles} />
+    <div ref={setNodeRef} style={style}>
+      <div className="flex-center">
+        <div {...attributes} {...listeners} style={{cursor: 'grab'}}>
+          <DragIndicatorIcon/>
+        </div>
+        <FileDiv fileUrl={fileUrl} files={files} setFiles={setFiles}/>
+      </div>
     </div>
   );
 };
