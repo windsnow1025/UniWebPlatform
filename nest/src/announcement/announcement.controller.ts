6e118a70ff25c3b2d8e125e6b9d10b0fb4a252e9
windsnow1025 <windsnow1025@gmail.com>
2025-01-22 08:05:34 -0800

    refactor(nest): remove announcement dto

--- nest/src/announcement/announcement.controller.ts
+++ nest/src/announcement/announcement.controller.ts
@@ -1,10 +1,9 @@
 import { Body, Controller, Get, Put } from '@nestjs/common';
 import { AnnouncementService } from './announcement.service';
-import { AnnouncementReqDto } from './dto/announcement.req.dto';
-import { AnnouncementResDto } from './dto/announcement.res.dto';
 import { Public } from '../common/decorators/public.decorator';
 import { Roles } from '../common/decorators/roles.decorator';
 import { Role } from '../common/enums/role.enum';
+import { Announcement } from './announcement.entity';
 
 @Controller('announcement')
 export class AnnouncementController {
@@ -12,15 +11,13 @@ export class AnnouncementController {
 
   @Public()
   @Get()
-  async find(): Promise<AnnouncementResDto> {
+  async find(): Promise<Announcement> {
     return this.announcementService.find();
   }
 
   @Put()
   @Roles(Role.Admin)
-  async update(
-    @Body() announcementReqDto: AnnouncementReqDto,
-  ): Promise<AnnouncementResDto> {
-    return this.announcementService.update(announcementReqDto.content);
+  async update(@Body() announcement: Announcement): Promise<Announcement> {
+    return this.announcementService.update(announcement.content);
   }
 }
