8ee2eb80faef17574c417b90b03e9250875ce768
windsnow1025 <windsnow1025@gmail.com>
2025-01-25 07:04:15 -0800

    refactor(fastapi)!: google genai

---
+++ fastapi/chat/logic/chat_generate/model_message_converter/gpt_message_converter.py
@@ -0,0 +1,24 @@
+from chat.logic.chat_generate import image_processor
+from chat.logic.message_preprocess.file_type_checker import get_file_type
+from chat.model import gpt_message
+from chat.model.gpt_message import GptMessage
+from chat.model.message import Message
+
+
+async def convert_message_to_gpt(message: Message) -> GptMessage:
+    role = message.role
+    text = message.text
+    file_urls = message.file_urls
+
+    content = []
+
+    if text:
+        text_content = gpt_message.TextContent(type="text", text=text)
+        content.append(text_content)
+
+    for file_url in file_urls:
+        if get_file_type(file_url) == "image":
+            image_contents = await image_processor.get_gpt_image_content_from_url(file_url)
+            content.append(image_contents)
+
+    return GptMessage(role=role, content=content)
