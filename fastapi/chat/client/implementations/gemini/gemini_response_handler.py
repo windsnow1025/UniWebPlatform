2e3d08a822723ad5c0aba50938f6ef23d7c04af5
windsnow1025 <windsnow1025@gmail.com>
2025-01-31 01:48:50 -0800

    refactor(fastapi): gemini response handler

---
+++ fastapi/chat/client/implementations/gemini/gemini_response_handler.py
@@ -0,0 +1,30 @@
+from enum import Enum
+
+from google.genai import types
+
+
+class PrintingStatus(Enum):
+    Start = "start"
+    Thought = "thought"
+    Response = "response"
+
+class GeminiResponseHandler:
+    def __init__(self):
+        self.printing_status = PrintingStatus.Start
+
+    def process_gemini_response(self, response: types.GenerateContentResponse):
+        output = ""
+
+        for part in response.candidates[0].content.parts:
+            print("debug", part.thought, self.printing_status)
+            if part.thought and self.printing_status == PrintingStatus.Start:
+                output += "# Model Thought:\n\n"
+                self.printing_status = PrintingStatus.Thought
+            elif not part.thought and self.printing_status == PrintingStatus.Thought:
+                output += f"\n\n# Model Response:\n\n"
+                self.printing_status = PrintingStatus.Response
+            output += part.text
+        if grounding_metadata := response.candidates[0].grounding_metadata:
+            output += "\n" + grounding_metadata.search_entry_point.rendered_content
+        return output
+
