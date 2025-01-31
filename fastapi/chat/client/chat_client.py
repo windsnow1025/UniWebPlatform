3e66612af538c06f810d17f4434588fd1da12853
windsnow1025 <windsnow1025@gmail.com>
2025-01-31 05:22:04 -0800

    refactor(fastapi): enum role

--- fastapi/chat/client/chat_client.py
+++ fastapi/chat/client/chat_client.py
@@ -1,9 +1,22 @@
 from dataclasses import asdict, is_dataclass
+from enum import Enum
 
 
 class ChatClient:
     def _to_dict(self, data_list):
-        return [asdict(item) for item in data_list if is_dataclass(item)]
+        def serialize(obj):
+            if isinstance(obj, Enum):
+                return obj.value
+            elif is_dataclass(obj):
+                return {key: serialize(value) for key, value in asdict(obj).items()}
+            elif isinstance(obj, list):
+                return [serialize(item) for item in obj]
+            elif isinstance(obj, dict):
+                return {key: serialize(value) for key, value in obj.items()}
+            else:
+                return obj
+
+        return [serialize(item) for item in data_list]
 
     def generate_response(self):
         raise NotImplementedError
