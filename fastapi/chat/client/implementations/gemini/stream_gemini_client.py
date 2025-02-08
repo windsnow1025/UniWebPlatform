a9d6cbe6380cc096d4dacfda705412b0cb867be5
windsnow1025 <windsnow1025@gmail.com>
2025-02-08 04:54:18 -0800

    fix(fastapi): yield in generate response

--- fastapi/chat/client/implementations/gemini/stream_gemini_client.py
+++ fastapi/chat/client/implementations/gemini/stream_gemini_client.py
@@ -17,27 +17,6 @@ def process_delta(completion_delta: types.GenerateContentResponse) -> str:
     return output
 
 
-async def generate_chunk(
-        response: AsyncIterator[types.GenerateContentResponse]
-) -> AsyncGenerator[str, None]:
-    try:
-        async for response_delta in response:
-            content_delta = process_delta(response_delta)
-            yield content_delta
-    except httpx.HTTPStatusError as e:
-        status_code = e.response.status_code
-        text = e.response.text
-        raise HTTPException(status_code=status_code, detail=text)
-    except Exception as e:
-        logging.exception(e)
-        match = re.search(r'\d{3}', str(e))
-        if match:
-            error_code = int(match.group(0))
-        else:
-            error_code = 500
-        raise HTTPException(status_code=error_code, detail=str(e))
-
-
 class StreamGeminiClient(GeminiClient):
     async def generate_response(self) -> AsyncGenerator[str, None]:
         try:
@@ -49,7 +28,9 @@ class StreamGeminiClient(GeminiClient):
                 config=self.config,
             )
 
-            return generate_chunk(response)
+            async for response_delta in response:
+                content_delta = process_delta(response_delta)
+                yield content_delta
 
         except httpx.HTTPStatusError as e:
             status_code = e.response.status_code
