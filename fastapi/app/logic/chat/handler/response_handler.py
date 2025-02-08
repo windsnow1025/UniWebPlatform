4fa01889e54042c3c5cef98a790ac6a452c479f5
windsnow1025 <windsnow1025@gmail.com>
2025-02-07 22:50:48 -0800

    Revert "feat(fastapi): chat response"
    This reverts commit 9c3468f95bc36af001247107ce30fe0e353d9383.

--- fastapi/app/logic/chat/handler/response_handler.py
+++ fastapi/app/logic/chat/handler/response_handler.py
@@ -1,4 +1,3 @@
-import json
 import logging
 from collections.abc import AsyncGenerator
 from typing import Callable, Awaitable
@@ -6,17 +5,16 @@ from typing import Callable, Awaitable
 from fastapi.responses import StreamingResponse
 
 from app.logic.chat.util.token_counter import num_tokens_from_text
-from chat import *
 
-ChunkGenerator = AsyncGenerator[ChatResponse, None]
+ChunkGenerator = AsyncGenerator[str, None]
 ReduceCredit = Callable[[int], Awaitable[float]]
 
 
 async def non_stream_handler(
-        content: ChatResponse,
+        content: str,
         reduce_credit: ReduceCredit
-) -> ChatResponse:
-    completion_tokens = num_tokens_from_text(content.text)
+) -> str:
+    completion_tokens = num_tokens_from_text(content)
     await reduce_credit(completion_tokens)
     logging.info(f"content: {content}")
     return content
@@ -27,11 +25,11 @@ async def stream_handler(
         reduce_credit: ReduceCredit
 ) -> StreamingResponse:
 
-    async def wrapper_generator() -> AsyncGenerator[str, None]:
+    async def wrapper_generator():
         content = ""
         async for chunk in generator:
-            content += chunk.text
-            yield json.dumps(serialize(chunk))
+            content += chunk
+            yield chunk
         completion_tokens = num_tokens_from_text(content)
         await reduce_credit(completion_tokens)
         logging.info(f"content: {content}")
