791ca27267a85fe8a47e267582449d2fd0b60632
windsnow1025 <windsnow1025@gmail.com>
2025-02-08 05:32:57 -0800

    fix(fastapi): generator exception handle

--- fastapi/chat/client/implementations/gemini/stream_gemini_client.py
+++ fastapi/chat/client/implementations/gemini/stream_gemini_client.py
@@ -24,18 +24,9 @@ async def generate_chunk(
         async for response_delta in response:
             content_delta = process_delta(response_delta)
             yield content_delta
-    except httpx.HTTPStatusError as e:
-        status_code = e.response.status_code
-        text = e.response.text
-        raise HTTPException(status_code=status_code, detail=text)
     except Exception as e:
         logging.exception(e)
-        match = re.search(r'\d{3}', str(e))
-        if match:
-            error_code = int(match.group(0))
-        else:
-            error_code = 500
-        raise HTTPException(status_code=error_code, detail=str(e))
+        yield str(e)
 
 
 class StreamGeminiClient(GeminiClient):
