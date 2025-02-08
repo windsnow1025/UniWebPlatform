338fd5b2ac938a1ff433a63eea76b01f990a9f22
windsnow1025 <windsnow1025@gmail.com>
2025-02-08 04:11:37 -0800

    fix(fastapi): gemini error

--- fastapi/chat/client/implementations/gemini/stream_gemini_client.py
+++ fastapi/chat/client/implementations/gemini/stream_gemini_client.py
@@ -20,9 +20,22 @@ def process_delta(completion_delta: types.GenerateContentResponse) -> str:
 async def generate_chunk(
         response: AsyncIterator[types.GenerateContentResponse]
 ) -> AsyncGenerator[str, None]:
-    async for response_delta in response:
-        content_delta = process_delta(response_delta)
-        yield content_delta
+    try:
+        async for response_delta in response:
+            content_delta = process_delta(response_delta)
+            yield content_delta
+    except httpx.HTTPStatusError as e:
+        status_code = e.response.status_code
+        text = e.response.text
+        raise HTTPException(status_code=status_code, detail=text)
+    except Exception as e:
+        logging.exception(e)
+        match = re.search(r'\d{3}', str(e))
+        if match:
+            error_code = int(match.group(0))
+        else:
+            error_code = 500
+        raise HTTPException(status_code=error_code, detail=str(e))
 
 
 class StreamGeminiClient(GeminiClient):
