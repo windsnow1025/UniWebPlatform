e483dee39b92ef32731dba58b536071456373cbc
windsnow1025 <windsnow1025@gmail.com>
2025-02-09 01:58:22 -0800

    feat(fastapi): citation dataclass

--- fastapi/chat/type/chat_response.py
+++ fastapi/chat/type/chat_response.py
@@ -3,7 +3,14 @@ from typing import Optional
 
 
 @dataclass
+class Citation:
+    text: str
+    indices: list[int]
+
+
+@dataclass
 class ChatResponse:
     text: Optional[str] = None
     display: Optional[str] = None
+    citations: Optional[list[Citation]] = None
     error: Optional[str] = None
