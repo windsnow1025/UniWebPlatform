2833df79e5bc837d74c9b611c5d080aca86af52a
windsnow1025 <windsnow1025@gmail.com>
2025-02-15 07:08:19 -0800

    style(next): add message divider

--- next/app/components/chat/AddMessageDivider.js
+++ next/app/components/chat/AddMessageDivider.js
@@ -1,6 +1,6 @@
-import {Divider, IconButton, Tooltip} from "@mui/material";
+import { Divider, IconButton, Tooltip, Box } from "@mui/material";
 import AddCircleIcon from "@mui/icons-material/AddCircle";
-import React from "react";
+import React, { useState } from "react";
 import ChatLogic from "../../../src/conversation/chat/ChatLogic";
 
 function AddMessageDivider({
@@ -12,6 +12,7 @@ function AddMessageDivider({
                              setConversationUpdateTrigger
                            }) {
   const chatLogic = new ChatLogic();
+  const [isHovered, setIsHovered] = useState(false);
 
   const handleMessageAdd = (index) => {
     if (index === messages.length - 1) {
@@ -27,14 +28,28 @@ function AddMessageDivider({
   };
 
   return (
-    <Divider>
-      <Tooltip title="Add Message">
-        <IconButton aria-label="add" onClick={() => handleMessageAdd(index)}>
-          <AddCircleIcon fontSize="small"/>
-        </IconButton>
-      </Tooltip>
-    </Divider>
-  )
+    <div
+      className="flex-center-nowrap h-4"
+      style={{
+        cursor: "pointer",
+      }}
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
+    >
+      <Divider sx={{ flexGrow: 1 }} />
+      {isHovered && (
+        <Tooltip title="Add Message">
+          <IconButton
+            aria-label="add"
+            onClick={() => handleMessageAdd(index)}
+          >
+            <AddCircleIcon fontSize="small" />
+          </IconButton>
+        </Tooltip>
+      )}
+      <Divider sx={{ flexGrow: 1 }} />
+    </div>
+  );
 }
 
 export default AddMessageDivider;
