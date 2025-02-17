cbf7cab289c3d8dab12c9c706225393d011bdf6d
windsnow1025 <windsnow1025@gmail.com>
2025-02-17 06:45:40 -0800

    chore(fastapi): remove redundant env load

--- fastapi/app/config.py
+++ fastapi/app/config.py
@@ -8,6 +8,4 @@ def init_env():
     load_dotenv()
     is_production = os.environ.get('ENV') != 'development'
 
-    os.environ["MYSQL_HOST"] = os.environ.get('MYSQL_HOST')
-
     logging.info(f"Using {'production' if is_production else 'development'} setting.")
