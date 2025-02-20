62267d930768acadea0d89c5c7f57513535a7541
windsnow1025 <windsnow1025@gmail.com>
2025-02-20 03:49:47 -0800

    refactor(nest): roles

--- nest/src/common/decorators/roles.decorator.ts
+++ nest/src/common/decorators/roles.decorator.ts
@@ -1,5 +1,4 @@
-import { SetMetadata } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
 import { Role } from '../enums/role.enum';
 
-export const ROLES_KEY = 'roles';
-export const Roles = (...roles: Role[]) => SetMetadata(ROLES_KEY, roles);
+export const Roles = Reflector.createDecorator<Role[]>();
