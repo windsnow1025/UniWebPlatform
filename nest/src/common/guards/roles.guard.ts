22f13ebcfb132dbe857c026f6fb1bb22cbef9894
windsnow1025 <windsnow1025@gmail.com>
2025-02-20 03:49:47 -0800

    refactor(nest): roles

--- nest/src/common/guards/roles.guard.ts
+++ nest/src/common/guards/roles.guard.ts
@@ -1,21 +1,26 @@
 import { Injectable, CanActivate, ExecutionContext } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
 import { Role } from '../enums/role.enum';
-import { ROLES_KEY } from '../decorators/roles.decorator';
+import { Roles } from '../decorators/roles.decorator';
 
 @Injectable()
 export class RolesGuard implements CanActivate {
   constructor(private reflector: Reflector) {}
 
   canActivate(context: ExecutionContext): boolean {
-    const requiredRoles = this.reflector.getAllAndOverride<Role[]>(ROLES_KEY, [
-      context.getHandler(),
-      context.getClass(),
-    ]);
-    if (!requiredRoles) {
+    const roles = this.reflector.get(Roles, context.getHandler());
+    if (!roles) {
       return true;
     }
-    const { user } = context.switchToHttp().getRequest();
-    return requiredRoles.some((role) => user.roles?.includes(role));
+    const request = context.switchToHttp().getRequest();
+    const user = request.user;
+    return matchRoles(roles, user.roles);
   }
+}
+
+function matchRoles(requiredRoles: Role[], userRoles: Role[]): boolean {
+  if (!userRoles) {
+    return false;
+  }
+  return requiredRoles.some((role) => userRoles.includes(role));
 }
