3cdae4d3e6b70e44f729e7e78b12d5787756ace1
windsnow1025 <windsnow1025@gmail.com>
2025-02-26 06:13:07 -0800

    feat(nest): verify email

--- nest/src/users/users.module.ts
+++ nest/src/users/users.module.ts
@@ -3,10 +3,12 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { User } from './user.entity';
+import { ConfigModule } from '@nestjs/config';
+import { FirebaseService } from './firebase.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
-  providers: [UsersService],
+  imports: [TypeOrmModule.forFeature([User]), ConfigModule],
+  providers: [UsersService, FirebaseService],
   controllers: [UsersController],
   exports: [UsersService],
 })
