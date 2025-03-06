33bfbf18865b22d69ac8b2e4dd9f4f995a517753
windsnow1025 <windsnow1025@gmail.com>
2025-03-06 11:59:36 -0800

    feat(next): openapi api config

--- next/src/common/APIConfig.ts
+++ next/src/common/APIConfig.ts
@@ -1,13 +1,14 @@
 import axios, {AxiosInstance} from 'axios';
+import {Configuration} from "@/client";
 
 export interface APIBaseURLs {
   nest: string;
   fastAPI: string;
 }
 
-export const defaultAPIBaseURLs = {
-  nest: process.env.NEXT_PUBLIC_NEST_API_BASE_URL,
-  fastAPI: process.env.NEXT_PUBLIC_FASTAPI_API_BASE_URL,
+export const defaultAPIBaseURLs: APIBaseURLs = {
+  nest: process.env.NEXT_PUBLIC_NEST_API_BASE_URL!,
+  fastAPI: process.env.NEXT_PUBLIC_FASTAPI_API_BASE_URL!,
 };
 
 let apiBaseURLs: APIBaseURLs;
@@ -22,7 +23,7 @@ export function getAPIBaseURLs(): APIBaseURLs {
   return apiBaseURLs;
 }
 
-export function getNestAxiosInstance() {
+export function getNestAxiosInstance(): AxiosInstance {
   if (!nestAxiosInstance) {
     nestAxiosInstance = axios.create({
       baseURL: getAPIBaseURLs().nest
@@ -31,11 +32,23 @@ export function getNestAxiosInstance() {
   return nestAxiosInstance;
 }
 
-export function getFastAPIAxiosInstance() {
+export function getFastAPIAxiosInstance(): AxiosInstance {
   if (!fastAPIAxiosInstance) {
     fastAPIAxiosInstance = axios.create({
       baseURL: getAPIBaseURLs().fastAPI
     });
   }
   return fastAPIAxiosInstance;
+}
+
+export function getOpenAPIConfiguration(): Configuration {
+  const token = localStorage.getItem("token");
+  return new Configuration({
+    basePath: getAPIBaseURLs().nest,
+    baseOptions: {
+      headers: {
+        Authorization: token ? `Bearer ${token}` : "",
+      },
+    },
+  });
 }
