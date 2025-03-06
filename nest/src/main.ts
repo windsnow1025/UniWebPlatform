cf4cb08bb075d4f9d75476654eb962e72930bcf3
windsnow1025 <windsnow1025@gmail.com>
2025-03-06 03:28:28 -0800

    feat(nest): openapi swagger

--- nest/src/main.ts
+++ nest/src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 import { ValidationPipe } from '@nestjs/common';
 import * as bodyParser from 'body-parser';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -13,9 +14,17 @@ async function bootstrap() {
   app.use(bodyParser.json({ limit: 'Infinity' }));
   app.use(bodyParser.urlencoded({ limit: 'Infinity', extended: true }));
 
+  const config = new DocumentBuilder()
+    .setTitle('Nest JS')
+    .setDescription('Nest JS API description')
+    .setVersion('1.0')
+    .addTag('NestJS')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+
   const configService = app.get(ConfigService);
   const port = configService.get<number>('port')!;
-
   await app.listen(port);
   console.log(`Application is running on: ${await app.getUrl()}`);
 }
