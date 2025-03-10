860a1c43dd726feb138ea847e3a1718ffd00dcce
windsnow1025 <windsnow1025@gmail.com>
2025-03-10 10:59:43 -0700

    feat(next): root theme WIP

--- next/pages/_app.js
+++ next/pages/_app.js
@@ -1,5 +1,15 @@
 import '../src/global.css';
+import { ThemeProvider } from "@mui/material/styles";
+import { CssBaseline } from "@mui/material";
+import useThemeHandler from "../app/hooks/useThemeHandler";
 
 export default function App({Component, pageProps}) {
-  return <Component {...pageProps} />;
+  const { muiTheme } = useThemeHandler();
+
+  return (
+    <ThemeProvider theme={muiTheme}>
+      <CssBaseline enableColorScheme />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
--- next/pages/image/index.js
+++ next/pages/image/index.js
@@ -5,7 +5,6 @@ import {
   Alert,
   Button,
   CircularProgress,
-  CssBaseline,
   FormControl,
   InputLabel,
   MenuItem,
@@ -19,7 +18,6 @@ import ContentDiv from "../../app/components/message/ContentDiv";
 import ImageClient from "../../src/image/ImageClient";
 
 function ImageGenerate() {
-  const {themeType, setThemeType, muiTheme} = useThemeHandler();
   const [alertOpen, setAlertOpen] = useState(false);
   const [alertMessage, setAlertMessage] = useState('');
   const [alertSeverity, setAlertSeverity] = useState('info');
@@ -56,101 +54,98 @@ function ImageGenerate() {
   }
 
   return (
-    <ThemeProvider theme={muiTheme}>
-      <CssBaseline enableColorScheme/>
-      <div className="local-scroll-root">
-        <HeaderAppBar title={title}/>
-        <div className="local-scroll-scrollable">
-          <div className="flex-around m-2">
-            <div className="m-1">
-              <FormControl fullWidth className="mt-2">
-                <InputLabel id="model-select-label">Model</InputLabel>
-                <Select
-                  labelId="model-select-label"
-                  id="model-select"
-                  value={model}
-                  label="Model"
-                  onChange={e => setModel(e.target.value)}
-                >
-                  <MenuItem value="dall-e-3">DALL·E 3</MenuItem>
-                </Select>
-              </FormControl>
-            </div>
-            <div className="m-1">
-              <FormControl fullWidth className="mt-2">
-                <InputLabel id="size-select-label">Size</InputLabel>
-                <Select
-                  labelId="size-select-label"
-                  id="size-select"
-                  value={size}
-                  label="Size"
-                  onChange={e => setSize(e.target.value)}
-                >
-                  <MenuItem value="1024x1024">1024x1024</MenuItem>
-                  <MenuItem value="1792x1024">1792x1024</MenuItem>
-                  <MenuItem value="1024x1792">1024x1792</MenuItem>
-                </Select>
-              </FormControl>
-            </div>
-            <div className="m-1">
-              <FormControl fullWidth className="mt-2">
-                <InputLabel id="quality-select-label">Quality</InputLabel>
-                <Select
-                  labelId="quality-select-label"
-                  id="quality-select"
-                  value={quality}
-                  label="Quality"
-                  onChange={e => setQuality(e.target.value)}
-                >
-                  <MenuItem value="standard">standard</MenuItem>
-                  <MenuItem value="hd">hd</MenuItem>
-                </Select>
-              </FormControl>
-            </div>
-            <div className="m-1">
-              <InputLabel htmlFor="temperature">Amount: {n}</InputLabel>
-              <Slider
-                aria-label="Amount"
-                value={n}
-                onChange={(e, newValue) => setN(newValue)}
-                valueLabelDisplay="auto"
-                step={1}
-                marks
-                min={1}
-                max={1}
-              />
-            </div>
+    <div className="local-scroll-root">
+      <HeaderAppBar title={title}/>
+      <div className="local-scroll-scrollable">
+        <div className="flex-around m-2">
+          <div className="m-1">
+            <FormControl fullWidth className="mt-2">
+              <InputLabel id="model-select-label">Model</InputLabel>
+              <Select
+                labelId="model-select-label"
+                id="model-select"
+                value={model}
+                label="Model"
+                onChange={e => setModel(e.target.value)}
+              >
+                <MenuItem value="dall-e-3">DALL·E 3</MenuItem>
+              </Select>
+            </FormControl>
           </div>
-          <Paper elevation={1} className="m-2 p-4 rounded-lg">
-            <ContentDiv
-              content={prompt}
-              setContent={setPrompt}
+          <div className="m-1">
+            <FormControl fullWidth className="mt-2">
+              <InputLabel id="size-select-label">Size</InputLabel>
+              <Select
+                labelId="size-select-label"
+                id="size-select"
+                value={size}
+                label="Size"
+                onChange={e => setSize(e.target.value)}
+              >
+                <MenuItem value="1024x1024">1024x1024</MenuItem>
+                <MenuItem value="1792x1024">1792x1024</MenuItem>
+                <MenuItem value="1024x1792">1024x1792</MenuItem>
+              </Select>
+            </FormControl>
+          </div>
+          <div className="m-1">
+            <FormControl fullWidth className="mt-2">
+              <InputLabel id="quality-select-label">Quality</InputLabel>
+              <Select
+                labelId="quality-select-label"
+                id="quality-select"
+                value={quality}
+                label="Quality"
+                onChange={e => setQuality(e.target.value)}
+              >
+                <MenuItem value="standard">standard</MenuItem>
+                <MenuItem value="hd">hd</MenuItem>
+              </Select>
+            </FormControl>
+          </div>
+          <div className="m-1">
+            <InputLabel htmlFor="temperature">Amount: {n}</InputLabel>
+            <Slider
+              aria-label="Amount"
+              value={n}
+              onChange={(e, newValue) => setN(newValue)}
+              valueLabelDisplay="auto"
+              step={1}
+              marks
+              min={1}
+              max={1}
             />
-          </Paper>
-          <div className="flex-center">
-            <div className="m-2">
-              <Button id="generate" variant="contained" onClick={handleGenerate}>Generate</Button>
-            </div>
-            <div>
-              {loading && (
-                <CircularProgress size={24}/>
-              )}
-            </div>
           </div>
-          <div className="flex-around">
-            {imageUrls.map((imageUrl, index) => (
-              <div key={index}>
-                <picture>
-                  <img
-                    key={index}
-                    src={imageUrl}
-                    alt="imageUrl"
-                    className="max-w-full"
-                  />
-                </picture>
-              </div>
-            ))}
+        </div>
+        <Paper elevation={1} className="m-2 p-4 rounded-lg">
+          <ContentDiv
+            content={prompt}
+            setContent={setPrompt}
+          />
+        </Paper>
+        <div className="flex-center">
+          <div className="m-2">
+            <Button id="generate" variant="contained" onClick={handleGenerate}>Generate</Button>
           </div>
+          <div>
+            {loading && (
+              <CircularProgress size={24}/>
+            )}
+          </div>
+        </div>
+        <div className="flex-around">
+          {imageUrls.map((imageUrl, index) => (
+            <div key={index}>
+              <picture>
+                <img
+                  key={index}
+                  src={imageUrl}
+                  alt="imageUrl"
+                  className="max-w-full"
+                />
+              </picture>
+            </div>
+          ))}
         </div>
       </div>
       <Snackbar
@@ -162,7 +157,7 @@ function ImageGenerate() {
           {alertMessage}
         </Alert>
       </Snackbar>
-    </ThemeProvider>
+    </div>
   );
 }
 
