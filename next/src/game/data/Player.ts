5ac58247a03832245689b96cd3f9a3922cb1a02e
windsnow1025 <windsnow1025@gmail.com>
2025-03-11 12:53:33 -0700

    chore(next): remove game

--- next/src/game/data/Player.ts
+++
@@ -1,5 +0,0 @@
-import Player from "@/src/game/Player";
-
-const initPlayers = [new Player(100, "Blue"), new Player(100, "Red"), new Player(100, "Green")]
-
-export default initPlayers
--- next/src/game/data/Unit.ts
+++
@@ -1,8 +0,0 @@
-import Infantry from "@/src/game/data/units/Infantry";
-import Archer from "@/src/game/data/units/Archer";
-import Cavalry from "@/src/game/data/units/Cavalry";
-import Unit from "@/src/game/Unit";
-
-const unitClasses: typeof Unit[] = [Infantry, Archer, Cavalry];
-
-export default unitClasses;
