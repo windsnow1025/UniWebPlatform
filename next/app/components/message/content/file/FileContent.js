14bd74b47d7999353e9a54eaa039425b3e0042ae
windsnow1025 <windsnow1025@gmail.com>
2025-03-17 08:20:03 -0700

    temp

--- next/app/components/message/content/file/FileContent.js
+++ next/app/components/message/content/file/FileContent.js
@@ -1,14 +1,10 @@
 import React, { useState } from 'react';
 import { Alert, Button, CircularProgress, Snackbar, Tooltip } from "@mui/material";
-import MicIcon from '@mui/icons-material/Mic';
-import StopIcon from '@mui/icons-material/Stop';
 import UploadFileIcon from '@mui/icons-material/UploadFile';
 import FolderIcon from '@mui/icons-material/Folder';
 import FileLogic from "../../../../../src/common/file/FileLogic";
 
 function FileContent({ file, setFile, setUploadProgress }) {
-  const [isRecording, setIsRecording] = useState(false);
-  const [mediaRecorder, setMediaRecorder] = useState(null);
   const [isUploading, setIsUploading] = useState(false);
   const fileLogic = new FileLogic();
 
@@ -18,43 +14,6 @@ function FileContent({ file, setFile, setUploadProgress }) {
 
   const fileInputRef = React.useRef(null);
   const folderInputRef = React.useRef(null);
-
-  // Audio recording functions
-  const startRecording = async () => {
-    try {
-      const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
-      const recorder = new MediaRecorder(stream);
-      let chunks = [];
-
-      recorder.ondataavailable = (event) => {
-        chunks.push(event.data);
-      };
-
-      recorder.onstop = async () => {
-        const audioBlob = new Blob(chunks, { type: 'audio/webm' });
-        const audioFile = new File([audioBlob], `recording-${Date.now()}.webm`, { type: 'audio/webm' });
-
-        await uploadFile(audioFile);
-      };
-
-      recorder.start();
-      setMediaRecorder(recorder);
-      setIsRecording(true);
-    } catch (error) {
-      console.error("Error starting recording:", error);
-      setAlertMessage("Failed to start recording");
-      setAlertSeverity('error');
-      setAlertOpen(true);
-    }
-  };
-
-  const stopRecording = () => {
-    if (mediaRecorder) {
-      mediaRecorder.stop();
-      setIsRecording(false);
-      mediaRecorder.stream.getTracks().forEach(track => track.stop());
-    }
-  };
 
   // File upload functions
   const uploadFile = async (file) => {
@@ -196,17 +155,6 @@ function FileContent({ file, setFile, setUploadProgress }) {
             >
               Upload Folder
             </Button>
-
-            <Tooltip title={isRecording ? "Stop Recording" : "Record Audio"}>
-              <Button
-                variant="outlined"
-                color={isRecording ? "error" : "primary"}
-                startIcon={isRecording ? <StopIcon /> : <MicIcon />}
-                onClick={isRecording ? stopRecording : startRecording}
-              >
-                {isRecording ? "Stop" : "Record"}
-              </Button>
-            </Tooltip>
           </div>
         </div>
       );
