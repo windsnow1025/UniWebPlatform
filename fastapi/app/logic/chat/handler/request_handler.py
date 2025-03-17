a6e0a2d13a3dc78a13e8c6d2da44b14e249f81f0
windsnow1025 <windsnow1025@gmail.com>
2025-03-17 09:05:52 -0700

    feat(fastapi): upgrade token count

--- fastapi/app/logic/chat/handler/request_handler.py
+++ fastapi/app/logic/chat/handler/request_handler.py
@@ -1,15 +1,26 @@
 from typing import Callable, Awaitable
 
 from app.logic.chat.util.token_counter import num_tokens_from_text
-from llm_bridge import Message
+from llm_bridge import Message, ContentType
 
 
 async def handle_request(
         messages: list[Message],
         reduce_credit: Callable[[int], Awaitable[float]]
 ) -> float:
-    text = ''.join(message.text for message in messages)
-    file_length = sum(len(message.files) for message in messages)
-    prompt_tokens = num_tokens_from_text(text) + file_length * 1000
-    cost = await reduce_credit(prompt_tokens)
-    return cost
+    prompt_tokens = calculate_token_count(messages)
+    return await reduce_credit(prompt_tokens)
+
+
+def calculate_token_count(messages: list[Message]) -> int:
+    text = ''
+    file_count = 0
+
+    for message in messages:
+        for content in message.contents:
+            if content.type == ContentType.Text:
+                text += content.data
+            elif content.type == ContentType.File:
+                file_count += 1
+
+    return num_tokens_from_text(text) + file_count * 1000
