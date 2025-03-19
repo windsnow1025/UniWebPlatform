98fe1b4cc95105cfd3306546b9e0367b543254e0
windsnow1025 <windsnow1025@gmail.com>
2025-03-19 03:00:05 -0700

    style-wip(next): sortable file div

--- next/app/components/message/content/file/SortableFile.js
+++ next/app/components/message/content/file/SortableFile.js
@@ -3,6 +3,7 @@ import {useSortable} from "@dnd-kit/sortable";
 import {CSS} from "@dnd-kit/utilities";
 import FileDiv from './FileDiv';
 import DragIndicatorIcon from '@mui/icons-material/DragIndicator';
+import {useTheme} from "@mui/material";
 
 const SortableFile = ({fileUrl, files, setFiles}) => {
   const {
@@ -19,10 +20,12 @@ const SortableFile = ({fileUrl, files, setFiles}) => {
     touchAction: 'none',
   };
 
+  const theme = useTheme();
+
   return (
     <div ref={setNodeRef} style={style}>
       <div className="flex-center-nowrap">
-        <div {...attributes} {...listeners} style={{cursor: 'grab'}}>
+        <div {...attributes} {...listeners} style={{cursor: 'grab', color: theme.palette.text.secondary}}>
           <DragIndicatorIcon fontSize="small"/>
         </div>
         <FileDiv fileUrl={fileUrl} files={files} setFiles={setFiles}/>
