2286e67e9dd77e5a4b762b44620de02ffc88ad84
windsnow1025 <windsnow1025@gmail.com>
2025-03-24 13:25:30 -0700

    feat: remove image gen

--- fastapi/app/main.py
+++ fastapi/app/main.py
@@ -4,7 +4,6 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
 from app.api.chat_router import chat_router
-from app.api.image_gen_router import image_gen_router
 from app.api.messages_convert_router import messages_convert_router
 from app.config import init_env
 
@@ -23,5 +22,4 @@ app.add_middleware(
 )
 
 app.include_router(chat_router)
-app.include_router(image_gen_router)
 app.include_router(messages_convert_router)
