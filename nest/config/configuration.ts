53d2d582194e851dbc23d249742624a0a1e5f57e
windsnow1025 <windsnow1025@gmail.com>
2025-04-12 21:02:51 -0700

    build: nest firebase json

--- nest/config/configuration.ts
+++ nest/config/configuration.ts
@@ -1,8 +1,18 @@
 import * as process from 'node:process';
+import * as path from 'node:path';
+import * as fs from 'node:fs';
 
 export default () => {
   const isProduction = process.env.ENV !== 'development';
   console.log(`Using ${isProduction ? 'production' : 'development'} setting.`);
+
+  const firebaseConfigPath = isProduction
+    ? path.resolve('/app/config', 'firebaseConfig.json')
+    : path.resolve(process.cwd(), 'firebaseConfig.json');
+
+  const firebaseConfig = JSON.parse(
+    fs.readFileSync(firebaseConfigPath, 'utf8'),
+  );
 
   return {
     port: isProduction ? 3000 : 3001,
@@ -23,14 +33,6 @@ export default () => {
       bucketName: process.env.MINIO_BUCKET_NAME,
       webUrl: process.env.MINIO_WEB_URL,
     },
-    firebase: {
-      apiKey: process.env.FIREBASE_API_KEY,
-      authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-      projectId: process.env.FIREBASE_PROJECT_ID,
-      storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-      messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-      appId: process.env.FIREBASE_APP_ID,
-      measurementId: process.env.FIREBASE_MEASUREMENT_ID,
-    },
+    firebaseConfig: firebaseConfig,
   };
 };
