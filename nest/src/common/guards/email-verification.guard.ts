a4d9d71ca63d43a1bebe80d39b0ed467c442c204
windsnow1025 <windsnow1025@gmail.com>
2025-04-13 09:13:39 -0700

    fix(nest): email verification bypass public

--- nest/src/common/guards/email-verification.guard.ts
+++ nest/src/common/guards/email-verification.guard.ts
@@ -7,17 +7,25 @@ import {
 } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
 import { ALLOW_UNVERIFIED_EMAIL_KEY } from '../decorators/allow-unverified-email.decorator';
+import { IS_PUBLIC_KEY } from '../decorators/public.decorator';
 
 @Injectable()
 export class EmailVerificationGuard implements CanActivate {
   constructor(private reflector: Reflector) {}
 
   canActivate(context: ExecutionContext): boolean {
+    const isPublic = this.reflector.getAllAndOverride<boolean>(IS_PUBLIC_KEY, [
+      context.getHandler(),
+      context.getClass(),
+    ]);
+    if (isPublic) {
+      return true;
+    }
+
     const allowUnverifiedEmail = this.reflector.getAllAndOverride<boolean>(
       ALLOW_UNVERIFIED_EMAIL_KEY,
       [context.getHandler(), context.getClass()],
     );
-
     if (allowUnverifiedEmail) {
       return true;
     }
