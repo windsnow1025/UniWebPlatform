fbcb6bcb607283a5a5c9aaa5a04660746fa3ff0e
windsnow1025 <windsnow1025@gmail.com>
2025-05-02 12:45:43 -0700

    refactor(next): split add text button

---
+++ next/components/message/content/create/AddTextButton.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Button } from "@mui/material";
+import TextSnippetIcon from '@mui/icons-material/TextSnippet';
+import { ContentTypeEnum } from "../../../../client";
+
+function AddTextButton({ setContent, disabled }) {
+  const handleAddTextContent = () => {
+    setContent({ type: ContentTypeEnum.Text, data: '' });
+  };
+
+  return (
+    <Button
+      variant="outlined"
+      size="small"
+      startIcon={<TextSnippetIcon />}
+      onClick={handleAddTextContent}
+      sx={{ mr: 1 }}
+      disabled={disabled}
+    >
+      Add Text
+    </Button>
+  );
+}
+
+export default AddTextButton;
