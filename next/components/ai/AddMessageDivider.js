c7ef50cc3129a339a29f0ecefed84e583d07361d
windsnow1025 <windsnow1025@gmail.com>
2025-05-27 09:12:00 -0700

    style(next): add message divider

--- next/components/ai/AddMessageDivider.js
+++ next/components/ai/AddMessageDivider.js
@@ -3,6 +3,16 @@ import AddCircleIcon from "@mui/icons-material/AddCircle";
 import React, {useState} from "react";
 import ChatLogic from "../../lib/chat/ChatLogic";
 
+const FlexDivider = ({isHovered}) => (
+  <Divider
+    sx={{
+      flexGrow: 1,
+      opacity: isHovered ? 1 : 0.5,
+      transition: 'opacity 0.2s ease-in-out'
+    }}
+  />
+);
+
 function AddMessageDivider({
                              messages,
                              setMessages,
@@ -29,34 +39,25 @@ function AddMessageDivider({
 
   return (
     <div
-      className="flex items-center my-1 group relative"
+      className="flex-center"
       onMouseEnter={() => setIsHovered(true)}
       onMouseLeave={() => setIsHovered(false)}
     >
-      <Divider
-        sx={{
-          flexGrow: 1,
-          opacity: isHovered ? 1 : 0.5,
-          transition: 'opacity 0.2s ease-in-out'
-        }}
-      />
-      <div
-        className={`absolute left-1/2 transform -translate-x-1/2`}>
-        <Tooltip title="Add Message" placement="top">
-          <IconButton
-            size="small"
-            onClick={() => handleMessageAdd(index)}
-            sx={{
-              opacity: isHovered ? 1 : 0.2,
-              transition: 'opacity 0.2s ease-in-out'
-            }}
-          >
-            <AddCircleIcon
-              fontSize="small"
-            />
-          </IconButton>
-        </Tooltip>
-      </div>
+      <FlexDivider isHovered={isHovered}/>
+      <Tooltip title="Add Message">
+        <IconButton
+          size="small"
+          onClick={() => handleMessageAdd(index)}
+          sx={{
+            py: 0,
+            opacity: isHovered ? 1 : 0.2,
+            transition: 'opacity 0.2s ease-in-out',
+          }}
+        >
+          <AddCircleIcon fontSize="small"/>
+        </IconButton>
+      </Tooltip>
+      <FlexDivider isHovered={isHovered}/>
     </div>
   );
 }
