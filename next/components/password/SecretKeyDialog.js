ab4e17a46030ab43b5453a1add30aa94050360cc
windsnow1025 <windsnow1025@gmail.com>
2025-09-05 22:27:20 -0400

    feat(next): secret key hide

--- next/components/password/SecretKeyDialog.js
+++ next/components/password/SecretKeyDialog.js
@@ -5,16 +5,20 @@ import {
   DialogContent,
   DialogActions,
   Button,
-  TextField,
+  FormControl,
+  InputLabel,
+  OutlinedInput,
   InputAdornment,
+  IconButton,
   FormControlLabel,
   Checkbox
 } from "@mui/material";
-import {Key as KeyIcon} from "@mui/icons-material";
+import {Key as KeyIcon, Visibility, VisibilityOff} from "@mui/icons-material";
 
 function SecretKeyDialog({open, onClose, keyValue, setKeyValue}) {
   const [inputValue, setInputValue] = useState(keyValue);
   const [rememberKey, setRememberKey] = useState(true);
+  const [showKey, setShowKey] = useState(false);
 
   useEffect(() => {
     if (open) setInputValue(keyValue);
@@ -25,19 +29,53 @@ function SecretKeyDialog({open, onClose, keyValue, setKeyValue}) {
     onClose();
   };
 
+  const handleClickShowKey = () => {
+    setShowKey(!showKey);
+  };
+
+  const handleMouseDownKey = (event) => {
+    event.preventDefault();
+  };
+
+  const handleMouseUpKey = (event) => {
+    event.preventDefault();
+  };
+
   return (
     <Dialog open={open} onClose={onClose}>
       <DialogTitle>Set Secret Key</DialogTitle>
       <DialogContent>
-        <TextField
-          autoFocus
-          margin="dense"
-          label="Secret Key"
-          type="number"
-          fullWidth
-          value={inputValue}
-          onChange={e => setInputValue(e.target.value)}
-        />
+        <FormControl fullWidth margin="dense" variant="outlined">
+          <InputLabel htmlFor="outlined-adornment-secret-key">Secret Key</InputLabel>
+          <OutlinedInput
+            id="outlined-adornment-secret-key"
+            type={showKey ? 'text' : 'password'}
+            value={inputValue}
+            onChange={(e) => setInputValue(e.target.value)}
+            startAdornment={
+              <InputAdornment position="start">
+                <KeyIcon />
+              </InputAdornment>
+            }
+            endAdornment={
+              <InputAdornment position="end">
+                <IconButton
+                  aria-label={
+                    showKey ? 'hide the secret key' : 'display the secret key'
+                  }
+                  onClick={handleClickShowKey}
+                  onMouseDown={handleMouseDownKey}
+                  onMouseUp={handleMouseUpKey}
+                  edge="end"
+                >
+                  {showKey ? <VisibilityOff /> : <Visibility />}
+                </IconButton>
+              </InputAdornment>
+            }
+            label="Secret Key"
+            autoFocus
+          />
+        </FormControl>
         <FormControlLabel
           control={
             <Checkbox
