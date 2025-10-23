4a366ef0cbdd95bc92b94e261caea8f2cc4a0010
windsnow1025 <windsnow1025@gmail.com>
2025-10-22 20:20:53 -0400

    fix(fastapi): local remote nest api support

--- fastapi/app/config.py
+++ fastapi/app/config.py
@@ -12,6 +12,6 @@ def init_env():
         nest_host = os.environ.get('NEST_HOST')
         os.environ['NEST_API_BASE_URL'] = f'http://{nest_host}:3000'
     else:
-        os.environ['NEST_API_BASE_URL'] = 'http://localhost:3001'
+        os.environ['NEST_API_BASE_URL'] = os.environ.get('NEST_API_BASE_URL') or 'http://localhost:3001'
 
     logging.info(f"Using {'production' if is_production else 'development'} setting.")
