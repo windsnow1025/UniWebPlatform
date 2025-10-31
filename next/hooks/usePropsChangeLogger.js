609ff4f5db5b445f3ac4f6f1f30424f825f0dcb7
windsnow1025 <windsnow1025@gmail.com>
2025-10-31 14:33:06 -0400

    perf(next): use props change logger

---
+++ next/hooks/usePropsChangeLogger.js
@@ -0,0 +1,29 @@
+import { useEffect, useRef } from 'react';
+
+function usePropsChangeLogger(name, props) {
+  const previousProps = useRef();
+
+  useEffect(() => {
+    if (previousProps.current) {
+      const allKeys = Object.keys({ ...previousProps.current, ...props });
+      const changesObj = {};
+
+      allKeys.forEach(key => {
+        if (previousProps.current[key] !== props[key]) {
+          changesObj[key] = {
+            from: previousProps.current[key],
+            to: props[key],
+          };
+        }
+      });
+
+      if (Object.keys(changesObj).length) {
+        console.log('[Props Changed]\n', name, changesObj);
+      }
+    }
+
+    previousProps.current = props;
+  });
+}
+
+export default usePropsChangeLogger;
