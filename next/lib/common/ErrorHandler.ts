44955c39338743415faa007d1bec283529829777
windsnow1025 <windsnow1025@gmail.com>
2025-11-22 19:21:28 -0500

    feat: precondition failed final message

--- next/lib/common/ErrorHandler.ts
+++ next/lib/common/ErrorHandler.ts
@@ -2,7 +2,17 @@ import axios from "axios";
 
 export function handleError(error: unknown, fallbackMessage: string): never {
   if (axios.isAxiosError(error)) {
-    throw new Error(`Error ${error.response?.status}: ${error.response?.data.message}`);
+    const response = error.response;
+    if (response) {
+      const status = response.status;
+      const statusText = response.statusText;
+      const message = response.data.message;
+      let finalMessage = "";
+      if (status === 412) {
+        finalMessage = "Please reload the resource."
+      }
+      throw new Error(`Error ${status} ${statusText}: ${message}. ${finalMessage}`)
+    }
   }
   console.error(error);
   throw new Error(fallbackMessage);
