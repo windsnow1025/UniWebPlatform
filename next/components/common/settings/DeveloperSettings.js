961057f52df18fceefe9f99f0381a6525e581407
windsnow1025 <windsnow1025@gmail.com>
2025-11-22 23:30:44 -0500

    feat(next): developer mode

--- next/components/common/settings/DeveloperSettings.js
+++ next/components/common/settings/DeveloperSettings.js
@@ -1,17 +1,36 @@
-import React from "react";
-import {Divider, Typography} from "@mui/material";
+import React, {useEffect, useState} from "react";
+import {Switch, FormControlLabel, Typography, Divider} from "@mui/material";
 import APIBaseURLSelect from "./APIBaseURLSelect";
+import {StorageKeys} from "../../../lib/common/Constants";
 
 const DeveloperSettings = () => {
+  const [developerMode, setDeveloperMode] = useState(false);
+
+  useEffect(() => {
+    const stored = localStorage.getItem(StorageKeys.DeveloperMode);
+    if (stored !== null) {
+      setDeveloperMode(stored === 'true');
+    }
+  }, []);
+
+  const handleToggle = (event) => {
+    const checked = !!event.target.checked;
+    setDeveloperMode(checked);
+    localStorage.setItem(StorageKeys.DeveloperMode, String(checked));
+  };
+
   return (
     <div>
       <Typography variant="h4">Developer Settings</Typography>
-      {/*<br></br>*/}
-      {/*<Typography variant="h5">Developer Mode</Typography>*/}
-      <br></br>
+      <Divider sx={{my: 2}}/>
+      <Typography variant="h5">Developer Mode</Typography>
+      <FormControlLabel
+        control={<Switch checked={developerMode} onChange={handleToggle} />}
+        label="Developer Mode"
+      />
+      <Divider sx={{my: 2}}/>
       <Typography variant="h5">API Base URL</Typography>
       <APIBaseURLSelect apiType="nest" label="Nest API Base URL"/>
-      {/*<Divider/>*/}
       <APIBaseURLSelect apiType="fastAPI" label="FastAPI API Base URL"/>
     </div>
   );
--- next/lib/common/Constants.ts
+++ next/lib/common/Constants.ts
@@ -3,6 +3,7 @@ export const StorageKeys = {
   APIBaseURLs: "apiBaseURLs",
   APIBaseURLsOptions: "apiBaseURLsOptions",
   SecretKey: "secretKey",
+  DeveloperMode: "developerMode",
 }
 
 export const AuthorEmail = "windsnow1025@gmail.com";
