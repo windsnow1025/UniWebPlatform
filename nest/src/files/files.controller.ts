c490971c7631d3a6e9a8e829710150bc8e4a7721
windsnow1025 <windsnow1025@gmail.com>
2025-11-28 17:31:08 -0500

    feat(nest): allow 32 mb single file

--- nest/src/files/files.controller.ts
+++ nest/src/files/files.controller.ts
@@ -18,7 +18,7 @@ import { MulterOptions } from '@nestjs/platform-express/multer/interfaces/multer
 
 const multerOptions: MulterOptions = {
   limits: {
-    fileSize: 16 * 1024 * 1024, // 16MB
+    fileSize: 32 * 1024 * 1024, // 32MB
   },
 };
 
