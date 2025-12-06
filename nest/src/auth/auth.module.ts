5c6598587a25705fe431ec87b4a14634e97f70da
windsnow1025 <windsnow1025@gmail.com>
2025-12-05 22:08:37 -0500

    feat(nest): unlimited time jwt

--- nest/src/auth/auth.module.ts
+++ nest/src/auth/auth.module.ts
@@ -14,7 +14,7 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
       useFactory: async (configService: ConfigService) => ({
         global: true,
         secret: configService.get<string>('jwtSecret'),
-        signOptions: { expiresIn: '30d' },
+        // signOptions: { expiresIn: '30d' },
       }),
     }),
   ],
