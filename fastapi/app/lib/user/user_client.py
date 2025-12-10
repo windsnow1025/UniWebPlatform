ee4305e226c245bdb1b51936f1c2b5682258a874
windsnow1025 <windsnow1025@gmail.com>
2025-12-10 03:37:53 -0500

    feat(fastapi): user logic error handling

--- fastapi/app/lib/user/user_client.py
+++ fastapi/app/lib/user/user_client.py
@@ -1,5 +1,8 @@
+from fastapi import HTTPException
+
 from ...client.nest_js_client.api.users.users_controller_find_one import asyncio as find_one_async
 from ...client.nest_js_client.api.users.users_controller_reduce_credit import asyncio as reduce_credit_async
+from ...client.nest_js_client.errors import UnexpectedStatus
 from ...client.nest_js_client.models import UserResDto
 from ...client.nest_js_client.models.reduce_credit_req_dto import ReduceCreditReqDto
 from ...config import get_client
@@ -7,11 +10,17 @@ from ...config import get_client
 
 async def get_user(token: str = None) -> UserResDto:
     client = get_client(token)
-    async with client as client:
-        return await find_one_async(client=client)
+    try:
+        async with client as client:
+            return await find_one_async(client=client)
+    except UnexpectedStatus as e:
+        raise HTTPException(status_code=e.status_code, detail=e.content.decode(errors='ignore'))
 
 
 async def reduce_user_credit(amount: float, token: str = None) -> UserResDto:
     client = get_client(token)
-    async with client as client:
-        return await reduce_credit_async(client=client, body=ReduceCreditReqDto(amount=amount))
+    try:
+        async with client as client:
+            return await reduce_credit_async(client=client, body=ReduceCreditReqDto(amount=amount))
+    except UnexpectedStatus as e:
+        raise HTTPException(status_code=e.status_code, detail=e.content.decode(errors='ignore'))
--- fastapi/app/lib/user/user_logic.py
+++ fastapi/app/lib/user/user_logic.py
@@ -1,11 +1,40 @@
+import logging
+
+import httpcore
+import httpx
+from fastapi import HTTPException
+
 from app.client.nest_js_client.models import UserResDto
 from app.lib.user import user_client
 
 
 async def get_user(token: str = None) -> UserResDto:
-    return await user_client.get_user(token)
+    try:
+        return await user_client.get_user(token)
+    except HTTPException as e:
+        raise e
+    except (httpcore.ConnectError, httpx.ConnectError) as e:
+        detail = f"ConnectError while fetching users: {e}"
+        logging.exception(detail)
+        raise HTTPException(status_code=500, detail=detail)
+    except Exception as e:
+        detail = f"Unknown error while fetching users: {e}"
+        logging.exception(detail)
+        raise HTTPException(status_code=500, detail=detail)
 
 
 async def reduce_credit(cost: float, token: str = None) -> float:
-    user_res_dto = await user_client.reduce_user_credit(cost, token)
-    return user_res_dto.credit
+    try:
+        user_res_dto = await user_client.reduce_user_credit(cost, token)
+        return user_res_dto.credit
+    except HTTPException as e:
+        raise e
+    except (httpcore.ConnectError, httpx.ConnectError) as e:
+        detail = f"ConnectError while fetching users: {e}"
+        logging.exception(detail)
+        raise HTTPException(status_code=500, detail=detail)
+    except Exception as e:
+        detail = f"Unknown error while fetching users: {e}"
+        logging.exception(detail)
+        raise HTTPException(status_code=500, detail=detail)
+
