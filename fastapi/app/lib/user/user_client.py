7eabec7cfe7acb57f8b4c2e69d71162aaabeb419
windsnow1025 <windsnow1025@gmail.com>
2026-01-05 05:37:09 -0500

    feat(fastapi): update user client

--- fastapi/app/lib/user/user_client.py
+++ fastapi/app/lib/user/user_client.py
@@ -1,6 +1,6 @@
 from fastapi import HTTPException
 
-from ...client.nest_js_client.api.users.users_controller_find_one import asyncio as find_one_async
+from ...client.nest_js_client.api.users.users_controller_find import asyncio as find_async
 from ...client.nest_js_client.api.users.users_controller_reduce_credit import asyncio as reduce_credit_async
 from ...client.nest_js_client.errors import UnexpectedStatus
 from ...client.nest_js_client.models import UserResDto
@@ -12,7 +12,7 @@ async def get_user(token: str = None) -> UserResDto:
     client = get_client(token)
     try:
         async with client as client:
-            return await find_one_async(client=client)
+            return await find_async(client=client)
     except UnexpectedStatus as e:
         raise HTTPException(status_code=e.status_code, detail=e.content.decode(errors='ignore'))
 
