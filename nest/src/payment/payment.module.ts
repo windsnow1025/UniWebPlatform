83abf42d19f7e184d15393af51a1a05dfc59d689
windsnow1025 <windsnow1025@gmail.com>
2026-01-10 00:48:37 -0500

    feat(nest): module updates

--- nest/src/payment/payment.module.ts
+++ nest/src/payment/payment.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { PaymentController } from './payment.controller';
 import { PaymentService } from './payment.service';
 import { CoreModule } from '../core/core.module';
+import { ConfigModule } from '@nestjs/config';
+import { CreemService } from './creem.service';
 
 @Module({
-  imports: [CoreModule],
+  imports: [ConfigModule, CoreModule],
+  providers: [PaymentService, CreemService],
   controllers: [PaymentController],
-  providers: [PaymentService],
 })
 export class PaymentModule {}
