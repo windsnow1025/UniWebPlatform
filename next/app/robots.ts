636bb6dd9ffed54fdab0c2305d79bbb8ec695f84
windsnow1025 <windsnow1025@gmail.com>
2026-01-28 22:08:18 -0500

    fix(next): dynamic routes

--- next/app/robots.ts
+++ next/app/robots.ts
@@ -1,6 +1,8 @@
 import type {MetadataRoute} from "next";
 import {getBaseUrl} from "@/lib/common/Constants";
 
+export const dynamic = 'force-dynamic';
+
 export default function robots(): MetadataRoute.Robots {
   const baseUrl = getBaseUrl();
   return {
--- next/app/sitemap.ts
+++ next/app/sitemap.ts
@@ -1,6 +1,8 @@
 import type {MetadataRoute} from "next";
 import {getBaseUrl} from "@/lib/common/Constants";
 
+export const dynamic = 'force-dynamic';
+
 export default function sitemap(): MetadataRoute.Sitemap {
   const baseUrl = getBaseUrl();
   return [
