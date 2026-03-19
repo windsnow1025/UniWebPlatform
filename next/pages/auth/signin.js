236412aa30247aa51be19ba72653c7e3b7e9cd0e
windsnow1025 <windsnow1025@gmail.com>
2026-03-19 08:02:34 -0400

    refactor(next): sign in

--- next/pages/auth/signin.js
+++ next/pages/auth/signin.js
@@ -1,5 +1,11 @@
 import SignIn from '@/components/common/sign-in/SignIn';
 
 export default function SignInPage() {
-  return <SignIn />;
+  return (
+    <div className="local-scroll-container">
+      <div className="local-scroll-scrollable">
+        <SignIn />
+      </div>
+    </div>
+  );
 }
