a21754650e4e4941230b740f4c052bb5d7785ac5
windsnow1025 <windsnow1025@gmail.com>
2026-03-19 10:27:32 -0400

    fix(next): layout

--- next/components/common/dashboard/Dashboard.tsx
+++ next/components/common/dashboard/Dashboard.tsx
@@ -11,32 +11,44 @@ export default function Dashboard({ children }: { children?: React.ReactNode }) 
   return (
     <>
       <CssBaseline enableColorScheme />
-      <Box sx={{ display: 'flex' }}>
+      <Box sx={{
+        position: 'relative',
+        display: 'flex',
+        overflow: 'hidden',
+        height: '100vh',
+        width: '100vw',
+      }}>
         <SideMenu />
         <AppNavbar />
         {/* Main content */}
-        <Box
-          component="main"
-          sx={(theme) => ({
-            flexGrow: 1,
-            backgroundColor: theme.vars
-              ? `rgba(${theme.vars.palette.background.defaultChannel} / 1)`
-              : alpha(theme.palette.background.default, 1),
-            overflow: 'auto',
-          })}
-        >
+        <Box sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          flex: 1,
+          minWidth: 0
+        }}>
           <Stack
             spacing={2}
             sx={{
               alignItems: 'center',
               mx: 3,
-              pb: 5,
               mt: { xs: 8, md: 0 },
             }}
           >
             <Header />
-            {children}
           </Stack>
+          <Box
+            component="main"
+            sx={(theme) => ({
+              flexGrow: 1,
+              backgroundColor: theme.vars
+                ? `rgba(${theme.vars.palette.background.defaultChannel} / 1)`
+                : alpha(theme.palette.background.default, 1),
+              overflow: 'auto',
+            })}
+          >
+            {children}
+          </Box>
         </Box>
       </Box>
     </>
