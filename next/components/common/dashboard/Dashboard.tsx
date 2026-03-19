dbe7724dafdd875546c578ece28650dd5cacb26c
windsnow1025 <windsnow1025@gmail.com>
2026-03-19 08:49:08 -0400

    refactor(next): dashboard

--- next/components/common/dashboard/Dashboard.tsx
+++ next/components/common/dashboard/Dashboard.tsx
@@ -1,33 +1,15 @@
-import type {} from '@mui/x-date-pickers/themeAugmentation';
-import type {} from '@mui/x-charts/themeAugmentation';
-import type {} from '@mui/x-data-grid-pro/themeAugmentation';
-import type {} from '@mui/x-tree-view/themeAugmentation';
+import * as React from 'react';
 import { alpha } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
 import Box from '@mui/material/Box';
 import Stack from '@mui/material/Stack';
 import AppNavbar from './components/AppNavbar';
 import Header from './components/Header';
-import MainGrid from './components/MainGrid';
 import SideMenu from './components/SideMenu';
-import AppTheme from '../shared-theme/AppTheme';
-import {
-  chartsCustomizations,
-  dataGridCustomizations,
-  datePickersCustomizations,
-  treeViewCustomizations,
-} from './theme/customizations';
 
-const xThemeComponents = {
-  ...chartsCustomizations,
-  ...dataGridCustomizations,
-  ...datePickersCustomizations,
-  ...treeViewCustomizations,
-};
-
-export default function Dashboard(props: { disableCustomTheme?: boolean }) {
+export default function Dashboard({ children }: { children?: React.ReactNode }) {
   return (
-    <AppTheme {...props} themeComponents={xThemeComponents}>
+    <>
       <CssBaseline enableColorScheme />
       <Box sx={{ display: 'flex' }}>
         <SideMenu />
@@ -53,10 +35,10 @@ export default function Dashboard(props: { disableCustomTheme?: boolean }) {
             }}
           >
             <Header />
-            <MainGrid />
+            {children}
           </Stack>
         </Box>
       </Box>
-    </AppTheme>
+    </>
   );
 }
