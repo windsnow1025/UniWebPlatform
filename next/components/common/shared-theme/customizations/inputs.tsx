65cf0612a30be00c83a9e6e2963a52fd1d68d8e8
windsnow1025 <windsnow1025@gmail.com>
2026-03-20 04:24:56 -0400

    style(next): icon button gap

--- next/components/common/shared-theme/customizations/inputs.tsx
+++ next/components/common/shared-theme/customizations/inputs.tsx
@@ -235,27 +235,27 @@ export const inputsCustomizations: Components<Theme> = {
         fontWeight: theme.typography.fontWeightMedium,
         letterSpacing: 0,
         color: (theme.vars || theme).palette.text.primary,
-        border: '1px solid ',
-        borderColor: gray[200],
-        backgroundColor: alpha(gray[50], 0.3),
-        '&:hover': {
-          backgroundColor: gray[100],
-          borderColor: gray[300],
-        },
-        '&:active': {
-          backgroundColor: gray[200],
-        },
-        ...theme.applyStyles('dark', {
-          backgroundColor: gray[800],
-          borderColor: gray[700],
-          '&:hover': {
-            backgroundColor: gray[900],
-            borderColor: gray[600],
-          },
-          '&:active': {
-            backgroundColor: gray[900],
-          },
-        }),
+        // border: '1px solid ',
+        // borderColor: gray[200],
+        // backgroundColor: alpha(gray[50], 0.3),
+        // '&:hover': {
+        //   backgroundColor: gray[100],
+        //   borderColor: gray[300],
+        // },
+        // '&:active': {
+        //   backgroundColor: gray[200],
+        // },
+        // ...theme.applyStyles('dark', {
+        //   backgroundColor: gray[800],
+        //   borderColor: gray[700],
+        //   '&:hover': {
+        //     backgroundColor: gray[900],
+        //     borderColor: gray[600],
+        //   },
+        //   '&:active': {
+        //     backgroundColor: gray[900],
+        //   },
+        // }),
         variants: [
           {
             props: {
