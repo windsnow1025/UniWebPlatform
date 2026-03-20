8a86d50f44771f7f83364269103395991f244da4
windsnow1025 <windsnow1025@gmail.com>
2026-03-20 02:35:51 -0400

    fix(next): mui alert

--- next/components/common/shared-theme/customizations/feedback.tsx
+++ next/components/common/shared-theme/customizations/feedback.tsx
@@ -7,16 +7,16 @@ export const feedbackCustomizations: Components<Theme> = {
     styleOverrides: {
       root: ({ theme }) => ({
         borderRadius: 10,
-        backgroundColor: orange[100],
-        color: (theme.vars || theme).palette.text.primary,
-        border: `1px solid ${alpha(orange[300], 0.5)}`,
-        '& .MuiAlert-icon': {
-          color: orange[500],
-        },
-        ...theme.applyStyles('dark', {
-          backgroundColor: `${alpha(orange[900], 0.5)}`,
-          border: `1px solid ${alpha(orange[800], 0.5)}`,
-        }),
+        // backgroundColor: orange[100],
+        // color: (theme.vars || theme).palette.text.primary,
+        // border: `1px solid ${alpha(orange[300], 0.5)}`,
+        // '& .MuiAlert-icon': {
+        //   color: orange[500],
+        // },
+        // ...theme.applyStyles('dark', {
+        //   backgroundColor: `${alpha(orange[900], 0.5)}`,
+        //   border: `1px solid ${alpha(orange[800], 0.5)}`,
+        // }),
       }),
     },
   },
