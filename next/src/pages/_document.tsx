170b18ff409b2167cc4be7bfdb7bdc64c50f44cd
windsnow1025 <windsnow1025@gmail.com>
2026-04-01 02:33:56 -0400

    feat(next): svg

--- next/src/pages/_document.tsx
+++ next/src/pages/_document.tsx
@@ -3,7 +3,9 @@ import {Html, Head, Main, NextScript} from 'next/document';
 export default function Document() {
   return (
     <Html lang="en" data-mui-color-scheme="light">
-      <Head/>
+      <Head>
+        <link rel="icon" href="/favicon.svg" type="image/svg+xml"/>
+      </Head>
       <body>
       <Main/>
       <NextScript/>
